1a829045fbee6fbc2612cba379cb1fb41bdec01c
cat <cat@0edadee9-8154-4c59-a661-b5f45207fcbd>
2008-08-21 14:12:19 +0000

    removed unused code and commented on source of CRS
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@277 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.awe.catalog.json/src/org/amanzi/awe/catalog/json/JSONGeoResourceInfo.java
+++ org.amanzi.awe.catalog.json/src/org/amanzi/awe/catalog/json/JSONGeoResourceInfo.java
@@ -6,8 +6,6 @@ import net.refractions.udig.catalog.IGeoResourceInfo;
 import net.sf.json.JSONObject;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.geotools.referencing.crs.DefaultGeographicCRS;
-import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 /**
  * This class produces information about the CSV data stream, including
@@ -24,10 +22,9 @@ public class JSONGeoResourceInfo extends IGeoResourceInfo {
         JSONReader jsonReader = handle.getJSONReader(monitor);
         JSONObject json = jsonReader.jsonObject();
         try {
-            CoordinateReferenceSystem crs = jsonReader.getCRS(DefaultGeographicCRS.WGS84);  // default WGS84 if crs cannot be found in JSON
             this.name = json.has("name") ? json.getString("name") : "GeoJSON Data";
             this.description = json.has("description") ? json.getString("description") : "GeoJSON Data";
-            this.bounds = jsonReader.getBounds();
+            this.bounds = jsonReader.getBounds();   // This also provides the CRS
         } catch (Exception e) {
             System.err.println("Failed to determine GeoResourceInfo: "+e.getMessage());
             e.printStackTrace(System.err);
