89a4a351f9d6aeb22ba0a050120559a7e8e61245
cat <cat@0edadee9-8154-4c59-a661-b5f45207fcbd>
2008-09-18 21:59:24 +0000

    moved generic code from org.amanzi.awe.script to org.amanzi.scripting
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@312 0edadee9-8154-4c59-a661-b5f45207fcbd

---
+++ org.amanzi.scripting.jirb/src/org/amanzi/scripting/jirb/IRBConfigData.java
@@ -0,0 +1,55 @@
+package org.amanzi.scripting.jirb;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * A simple container for configuration information relevant to starting
+ * the IRB console. This is used to customize the startup with various
+ * options:
+ * <ul>
+ *   <li>title - a String to use as the title of the IRB session</li>
+ *   <li>extraLoadPath - a String[] of additional paths to add to $: for require to search</li>
+ *   <li>extraRequire - a String[] of additional files to require after irb, but before IRB.start, allowing for customization of the IRB session</li>
+ * </ul>
+ * @author craig
+ *
+ */
+public class IRBConfigData {
+	private String title = "JRuby IRB Console";
+	private String[] extraLoadPath = null;
+	private String[] extraRequire = null;
+	private HashMap<String,Object> extraGlobals = new HashMap<String,Object>();
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String[] getExtraLoadPath() {
+		return extraLoadPath==null ? (new String[0]) : extraLoadPath;
+	}
+	public void setExtraLoadPath(String[] extraLoadPath) {
+		this.extraLoadPath = extraLoadPath;
+	}
+	public String[] getExtraRequire() {
+		return extraRequire==null ? (new String[0]) : extraRequire;
+	}
+	public void setExtraRequire(String[] extraRequire) {
+		this.extraRequire = extraRequire;
+	}
+	public String[] getStartScriptlets(){
+		ArrayList<String> scriptlets = new ArrayList<String>();
+		scriptlets.add("require 'irb'");
+		scriptlets.add("require 'irb/completion'");
+        for(String require:getExtraRequire()) scriptlets.add("require '"+require+"'");
+		scriptlets.add("IRB.start");
+		return scriptlets.toArray(new String[0]);
+	}
+    public HashMap<String, Object> getExtraGlobals() {
+        return extraGlobals;
+    }
+    public void addExtraGlobal(String key, Object value) {
+        this.extraGlobals.put(key,value);
+    }
+}
