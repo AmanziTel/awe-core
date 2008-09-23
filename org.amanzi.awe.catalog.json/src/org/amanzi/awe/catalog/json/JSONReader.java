230528e65e956be5d9077a06c62d40d4326a6a19
cat <cat@0edadee9-8154-4c59-a661-b5f45207fcbd>
2008-09-23 14:54:16 +0000

    comment new iterator usage
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@324 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.awe.catalog.json/src/org/amanzi/awe/catalog/json/JSONReader.java
+++ org.amanzi.awe.catalog.json/src/org/amanzi/awe/catalog/json/JSONReader.java
@@ -324,8 +324,10 @@ public class JSONReader {
     }
 
 	/**
-	 * Interface for various anonymous inner classes that can
-	 * produce a stream of JSON features based on the internal JSON data.
+	 * This class provides the API for various anonymous inner classes that
+	 * can produce a stream of JSON features. We implement both the Iterator
+	 * and the Enumeration interfaces to be friendly to both the java5-style
+	 * 'for loop' and the JRuby 'each' method.
 	 * This allows, for example, the JSON to contain the features directly
 	 * as well as reference another data source, like a file or
 	 * another URL, that will provide the necessary data.
