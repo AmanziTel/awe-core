12cab7f1bac5e1bfb4d1422088523d6da5537f3c
amabdelsalam <amabdelsalam@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-06-13 08:08:05 +0000

    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@1295 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.splash/jruby.rb
+++ org.amanzi.splash/jruby.rb
@@ -6,14 +6,31 @@ class Cells
   	def method_missing(meth, *args)
       s = "#{meth}"
       s = s.gsub("=","")
+      puts "s = " + s
+      a = args.to_s
+      puts "a = " + a
+      if (a =~ /\d+/ and a.include?".")
+      	puts "FLOAT DETECTED"
+      	b = a.to_f
+      elsif (a =~ /\d+/)
+      	puts "INTEGER DETECTED"
+      	b = a.to_i
+      else
+      	b = a
+      end
+      
       if (args.to_s.eql?"")
         #puts "value of "+ s + "= " + @cells[s]
         @cells[s]
       else
-        @cells[s] = args.to_s
+        @cells[s] = b #args[0].to_i #.to_s
 	  end
 	end
+	
+	
 end
+
+	
 
 class Spreadsheet
   def initialize
@@ -24,6 +41,8 @@ class Spreadsheet
   def cells
     @cells
   end
+  
+  
 end
 
 
