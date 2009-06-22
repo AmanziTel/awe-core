c7f49f4b6bd782a7b70c3cccfe9ed764325f60d2
cat <cat@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-06-22 14:06:01 +0000

    Move unused plugins to prototypes folder
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@1369 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.awe.catalog.json/src/org/amanzi/awe/catalog/json/beans/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains classes that related to parsing ext network json file. 
- * Those are beans that correspond to structure in ext network json file. ExtJSON is the root of the structure.
- *
- * @author Milan Dinic
- */
-package org.amanzi.awe.catalog.json.beans;
