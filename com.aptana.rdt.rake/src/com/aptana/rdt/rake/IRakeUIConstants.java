ee000fa717abe2b83fc76b3c70803217a1bb2229
lagutko <lagutko@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-06-29 10:08:33 +0000

    Ruby Gems doesn't supported
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@1474 0edadee9-8154-4c59-a661-b5f45207fcbd

--- com.aptana.rdt.rake/src/com/aptana/rdt/rake/IRakeUIConstants.java
+++
@@ -1,7 +0,0 @@
-package com.aptana.rdt.rake;
-
-public interface IRakeUIConstants {
-
-	String ID_RAKE_VIEW = "com.aptana.rdt.ViewRakeTasks";
-
-}
--- com.aptana.rdt.rake/src/com/aptana/rdt/rake/PreferenceConstants.java
+++
@@ -1,7 +0,0 @@
-package com.aptana.rdt.rake;
-
-public class PreferenceConstants {
-	
-	public static final String PREF_RAKE_PATH = "rails.core.path.rake";
-
-}
