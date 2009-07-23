58c279325649bf8a17f3156edde311ba93b850b9
lagutko <lagutko@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-07-23 14:37:29 +0000

    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@1681 0edadee9-8154-4c59-a661-b5f45207fcbd

---
+++ org.amanzi.neo.loader/src/org/amanzi/neo/loader/internal/NeoLoaderPluginMessages.java
@@ -0,0 +1,36 @@
+package org.amanzi.neo.loader.internal;
+
+import java.util.ResourceBundle;
+
+import org.eclipse.osgi.util.NLS;
+
+public class NeoLoaderPluginMessages extends NLS {
+    
+    private static final String BUNDLE_NAME = NeoLoaderPluginMessages.class.getName();
+    private static ResourceBundle resourceBundle = ResourceBundle.getBundle(BUNDLE_NAME);
+    
+    public static String TEMSDialog_RemoveButtonText;
+    public static String TEMSDialog_AddButtonText;
+    public static String TEMSDialog_BrowseButtonText;
+    public static String TEMSDialog_LoadButtonText;
+    public static String TEMSDialog_CancelButtonText;
+    public static String TEMSDialog_FilesToLoadListLabel;
+    public static String TEMSDialog_FilesToChooseListLabel;
+    public static String TEMSDialog_DialogTitle;
+    public static String TEMSDialog_FileDialogTitle;
+    public static String TEMSDialog_MonitorName;
+    public static String Console_ErrorOnClose;
+    public static String NetworkDialog_DialogTitle;
+    
+    private NeoLoaderPluginMessages() {
+    }
+
+    public static ResourceBundle getResourceBundle() {
+        return resourceBundle;
+    }
+
+    static {
+        NLS.initializeMessages(BUNDLE_NAME, NeoLoaderPluginMessages.class);
+    }
+
+}
