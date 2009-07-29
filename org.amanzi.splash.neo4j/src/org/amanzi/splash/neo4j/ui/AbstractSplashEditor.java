719be882465b92585c47118f7770ccc0b3494e43
lagutko <lagutko@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-07-29 14:48:09 +0000

    Additional correctings in import.
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@1703 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.splash.neo4j/src/org/amanzi/splash/neo4j/ui/AbstractSplashEditor.java
+++ org.amanzi.splash.neo4j/src/org/amanzi/splash/neo4j/ui/AbstractSplashEditor.java
@@ -67,7 +67,6 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
-import org.eclipse.ui.IStorageEditorInput;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.EditorPart;
 import org.rubypeople.rdt.core.RubyModelException;
