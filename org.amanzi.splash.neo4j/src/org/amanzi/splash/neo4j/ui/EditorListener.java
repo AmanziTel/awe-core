5812d0374e481ce6d341d80a3ad2936cbd54ba65
artemij <artemij@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-08-10 10:16:00 +0000

    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@1742 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.splash.neo4j/src/org/amanzi/splash/neo4j/ui/EditorListener.java
+++ org.amanzi.splash.neo4j/src/org/amanzi/splash/neo4j/ui/EditorListener.java
@@ -5,20 +5,15 @@ package org.amanzi.splash.neo4j.ui;
 
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
-import org.eclipse.core.resources.IResourceDelta;
 
 /**
- * @author Cinkel_A
- *
+ * @author Cinkel_A Listener for change ruby script
  */
-public class EditorListener implements IResourceChangeListener{
+public class EditorListener implements IResourceChangeListener {
 
 	@Override
 	public void resourceChanged(IResourceChangeEvent event) {
-//		IResourceDelta delta = event.getDelta().getAffectedChildren()[0].getClass();
-//		delta.
-//		System.out.println(event.getDelta().getAffectedChildren()[0].getFullPath()
-//				);
+		// TODO check script change
 	}
 
 }
