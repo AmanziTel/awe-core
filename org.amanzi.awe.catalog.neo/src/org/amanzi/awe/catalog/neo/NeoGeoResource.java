ce2912572f2f9270ea966db52907718c0eb27393
artemij <artemij@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-08-12 14:48:55 +0000

    Realize Feature #552
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@1770 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.awe.catalog.neo/src/org/amanzi/awe/catalog/neo/NeoGeoResource.java
+++ org.amanzi.awe.catalog.neo/src/org/amanzi/awe/catalog/neo/NeoGeoResource.java
@@ -5,88 +5,106 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import net.refractions.udig.catalog.IGeoResource;
+import net.refractions.udig.catalog.ui.CatalogUIPlugin;
 
 import org.amanzi.neo.core.INeoConstants;
+import org.amanzi.neo.core.icons.IconManager;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.neo4j.api.core.Node;
 
 /**
- * This class represents a single collection of geographic objects in the Neo4j database.
- * In other words this class represents something that can be turned into a single layer
- * in the map.
+ * This class represents a single collection of geographic objects in the Neo4j
+ * database. In other words this class represents something that can be turned
+ * into a single layer in the map.
+ * 
  * @author craig
  * @since 1.0.0
  */
 public class NeoGeoResource extends IGeoResource {
-    private URL identifierUrl;
-    private NeoService service;
-    private Node gisNode;
-    private GeoNeo geoNeo;
-    
-    public NeoGeoResource( NeoService service, org.neo4j.api.core.NeoService neo, Node gisNode ) {
-        this.service = service;
-        this.gisNode = gisNode;
-        this.geoNeo = new GeoNeo(neo, this.gisNode);
-        try {
-            URL serviceUrl = service.getIdentifier();
-            identifierUrl = new URL( serviceUrl+"#"+this.gisNode.getProperty(INeoConstants.PROPERTY_NAME_NAME) );
-        } catch (MalformedURLException e) {            
-        }
-    }
+	private URL identifierUrl;
+	private NeoService service;
+	private Node gisNode;
+	private GeoNeo geoNeo;
 
-    @Override
-    public URL getIdentifier() {
-        return identifierUrl;
-    }
+	public NeoGeoResource(NeoService service,
+			org.neo4j.api.core.NeoService neo, Node gisNode) {
+		this.service = service;
+		this.gisNode = gisNode;
+		this.geoNeo = new GeoNeo(neo, this.gisNode);
+		try {
+			URL serviceUrl = service.getIdentifier();
+			identifierUrl = new URL(serviceUrl
+					+ "#"
+					+ this.gisNode
+							.getProperty(INeoConstants.PROPERTY_NAME_NAME));
+			CatalogUIPlugin.getDefault().getImageRegistry().put(
+					this.getIdentifier().toString(),
+					IconManager.getIconManager().getImage(
+							IconManager.NETWORK_ICON));
+		} catch (MalformedURLException e) {
+		}
+	}
 
-    private NeoGeoResourceInfo info;
-    public NeoGeoResourceInfo getInfo( IProgressMonitor monitor ) throws IOException {
-        if (info == null) {
-            synchronized (this) {
-                if (info == null) {
-                    info = new NeoGeoResourceInfo( this, gisNode, monitor );
-                }
-            }
-        }
-        return info;
-    }
+	@Override
+	public URL getIdentifier() {
+		return identifierUrl;
+	}
 
-    @Override
-    public NeoService service( IProgressMonitor monitor ) throws IOException {
-        return service;
-    }
+	private NeoGeoResourceInfo info;
 
-    /**
-     * Return a utility object used to read the GIS data. This is used in two places:
-     * <ul>
-     * <li>the NeoGeoResourceInfo class uses this to determine the envelope for the map</li>
-     * <li>the resolve method returns this to the map for use in drawing the points</li>
-     * </ul>
-     * 
-     * @param monitor
-     * @return
-     * @throws IOException
-     */
-    public GeoNeo getGeoNeo( IProgressMonitor monitor ) throws IOException {
-        return geoNeo;
-    }
+	public NeoGeoResourceInfo getInfo(IProgressMonitor monitor)
+			throws IOException {
+		if (info == null) {
+			synchronized (this) {
+				if (info == null) {
+					info = new NeoGeoResourceInfo(this, gisNode, monitor);
+				}
+			}
+		}
+		return info;
+	}
 
-    public <T> boolean canResolve( Class<T> adaptee ) {
-        return adaptee.isAssignableFrom(GeoNeo.class) || super.canResolve(adaptee);
-    }
+	@Override
+	public NeoService service(IProgressMonitor monitor) throws IOException {
+		return service;
+	}
 
-    public <T> T resolve( Class<T> adaptee, IProgressMonitor monitor ) throws IOException {
-        if (adaptee.isAssignableFrom(GeoNeo.class)) {
-            return adaptee.cast(getGeoNeo(monitor));
-        }
-        return super.resolve(adaptee, monitor);
-    }
+	/**
+	 * Return a utility object used to read the GIS data. This is used in two
+	 * places:
+	 * <ul>
+	 * <li>the NeoGeoResourceInfo class uses this to determine the envelope for
+	 * the map</li>
+	 * <li>the resolve method returns this to the map for use in drawing the
+	 * points</li>
+	 * </ul>
+	 * 
+	 * @param monitor
+	 * @return
+	 * @throws IOException
+	 */
+	public GeoNeo getGeoNeo(IProgressMonitor monitor) throws IOException {
+		return geoNeo;
+	}
 
-    public Throwable getMessage() {
-        return service.getMessage();
-    }
+	public <T> boolean canResolve(Class<T> adaptee) {
+		return adaptee.isAssignableFrom(GeoNeo.class)
+				|| super.canResolve(adaptee);
+	}
 
-    public Status getStatus() {
-        return service.getStatus();
-    }
+	public <T> T resolve(Class<T> adaptee, IProgressMonitor monitor)
+			throws IOException {
+		if (adaptee.isAssignableFrom(GeoNeo.class)) {
+			return adaptee.cast(getGeoNeo(monitor));
+		}
+		return super.resolve(adaptee, monitor);
+	}
+
+	public Throwable getMessage() {
+		return service.getMessage();
+	}
+
+	public Status getStatus() {
+		return service.getStatus();
+	}
 }
--- org.amanzi.awe.catalog.neo/src/org/amanzi/awe/catalog/neo/NeoGeoResourceInfo.java
+++ org.amanzi.awe.catalog.neo/src/org/amanzi/awe/catalog/neo/NeoGeoResourceInfo.java
@@ -5,31 +5,49 @@ import java.io.IOException;
 import net.refractions.udig.catalog.IGeoResourceInfo;
 
 import org.amanzi.neo.core.INeoConstants;
+import org.amanzi.neo.core.icons.IconManager;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.ImageData;
 import org.neo4j.api.core.Node;
 
 /**
- * This class produces information about the CSV data stream, including
- * the bounds of the data. Currently this is done by reading the stream
- * and expanding an envelope to include all points. A better approach would
- * be to provide meta-data with the stream.
+ * This class produces information about the CSV data stream, including the
+ * bounds of the data. Currently this is done by reading the stream and
+ * expanding an envelope to include all points. A better approach would be to
+ * provide meta-data with the stream.
  */
 public class NeoGeoResourceInfo extends IGeoResourceInfo {
-    NeoGeoResource handle;
-    Node gisNode;
-    public NeoGeoResourceInfo( NeoGeoResource resource, Node gisNode, IProgressMonitor monitor )
-            throws IOException {
-        this.handle = resource;
-        this.gisNode = gisNode;
-        this.title = handle.getIdentifier().toString();
-        GeoNeo geoNeo = handle.getGeoNeo(monitor);
-        try {
-            this.name = gisNode.getProperty(INeoConstants.PROPERTY_NAME_NAME).toString();
-            this.description = gisNode.hasProperty(INeoConstants.PROPERTY_DESCRIPTION_NAME) ? gisNode.getProperty(INeoConstants.PROPERTY_DESCRIPTION_NAME).toString() : "GeoNeo Data";
-            this.bounds = geoNeo.getBounds();
-        } catch (Exception e) {
-            System.err.println("Failed to determine GeoResourceInfo: "+e.getMessage());
-            e.printStackTrace(System.err);
-        }
-    }
+	NeoGeoResource handle;
+	Node gisNode;
+
+	public NeoGeoResourceInfo(NeoGeoResource resource, Node gisNode,
+			IProgressMonitor monitor) throws IOException {
+		this.handle = resource;
+		this.gisNode = gisNode;
+		this.title = handle.getIdentifier().toString();
+		GeoNeo geoNeo = handle.getGeoNeo(monitor);
+		try {
+			this.name = gisNode.getProperty(INeoConstants.PROPERTY_NAME_NAME)
+					.toString();
+			this.description = gisNode
+					.hasProperty(INeoConstants.PROPERTY_DESCRIPTION_NAME) ? gisNode
+					.getProperty(INeoConstants.PROPERTY_DESCRIPTION_NAME)
+					.toString()
+					: "GeoNeo Data";
+			this.bounds = geoNeo.getBounds();
+		} catch (Exception e) {
+			System.err.println("Failed to determine GeoResourceInfo: "
+					+ e.getMessage());
+			e.printStackTrace(System.err);
+		}
+		final ImageData imageData = IconManager.getIconManager().getImage(
+				IconManager.NETWORK_ICON).getImageData();
+		this.icon = new ImageDescriptor() {
+			@Override
+			public ImageData getImageData() {
+				return imageData;
+			}
+		};
+	}
 }
