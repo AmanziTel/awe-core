132115caafb00e2c2940574c5237543b5c336a28
artemij <artemij@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-08-12 10:19:27 +0000

    Realize Feature #568
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@1766 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.splash.neo4j/src/org/amanzi/splash/neo4j/console/NeoSplashManager.java
+++ org.amanzi.splash.neo4j/src/org/amanzi/splash/neo4j/console/NeoSplashManager.java
@@ -9,123 +9,154 @@ import org.amanzi.splash.neo4j.ui.SplashPlugin;
 import org.amanzi.splash.neo4j.utilities.ActionUtil;
 import org.amanzi.splash.neo4j.utilities.ActionUtil.RunnableWithResult;
 
-
 /**
  * Spreadsheet Manager class
  * 
  * Provides access to Spreadsheets
  * 
  * @author Lagutko_N
- *
+ * 
  */
 
 public class NeoSplashManager {
-	
+
 	private SplashTableModel activeModel;
-	
-	private Spreadsheet currentSpreadsheet;	
-	
+
+	private Spreadsheet currentSpreadsheet;
+
 	private static NeoSplashManager instance = null;
-	
+
 	/**
 	 * Returns the instance of SpreadsheetManager
 	 * 
 	 * @return
 	 */
-	
+
 	public static NeoSplashManager getInstance() {
 		if (instance == null) {
 			instance = new NeoSplashManager();
-		}	
-				
-		return instance;		
+		}
+
+		return instance;
 	}
-	
+
 	/**
-	 * Search for spreadsheet by it's name, name of Ruby Project and name of AWE project
+	 * Search for spreadsheet by it's name, name of Ruby Project and name of AWE
+	 * project
 	 * 
-	 * @param name name of spreadsheet
-	 * @param rdtName name of Ruby Project
-	 * @param udigName name of uDIG project
+	 * @param name
+	 *            name of spreadsheet
+	 * @param rdtName
+	 *            name of Ruby Project
+	 * @param udigName
+	 *            name of uDIG project
 	 * @return Spreadsheet
 	 */
-	
-	public Spreadsheet getSpreadsheet(final String name,  String rdtName, String udigName) throws SpreadsheetManagerException {
+
+	public Spreadsheet getSpreadsheet(final String name, String rdtName,
+			String udigName) throws SpreadsheetManagerException {
 		final String realUdigName = resolveUDIGProjectName(udigName);
 		final String realRdtName = resolveRDTProjectName(realUdigName, rdtName);
-		
-		final SpreadsheetNode spreadsheet = (SpreadsheetNode)ActionUtil.getInstance().runTaskWithResult(new RunnableWithResult() {
-		    
-		    private SpreadsheetNode result;
+		final RubyProjectNode rootNode = (RubyProjectNode) ActionUtil
+				.getInstance().runTaskWithResult(new RunnableWithResult() {
 
-            public Object getValue() {
-                return result;
-            }
+					private RubyProjectNode result;
 
-            public void run() {
-                RubyProjectNode rootNode = NeoCorePlugin.getDefault().getProjectService().findRubyProject(realUdigName);//getSpreadsheetRoot(realUdigName, realRdtName);
-                result = SplashPlugin.getDefault().getSpreadsheetService().findSpreadsheet(rootNode, name);
-            }		    
-		});
-		
+					public Object getValue() {
+						return result;
+					}
+
+					public void run() {
+						result = NeoCorePlugin.getDefault().getProjectService()
+								.findRubyProject(realUdigName);
+					}
+				});
+		final SpreadsheetNode spreadsheet = (SpreadsheetNode) ActionUtil
+				.getInstance().runTaskWithResult(new RunnableWithResult() {
+
+					private SpreadsheetNode result;
+
+					public Object getValue() {
+						return result;
+					}
+
+					public void run() {
+						result = SplashPlugin.getDefault()
+								.getSpreadsheetService().findSpreadsheet(
+										rootNode, name);
+					}
+				});
+
 		if (spreadsheet == null) {
-		    throw new SpreadsheetManagerException(realRdtName, realUdigName, name);
+			throw new SpreadsheetManagerException(realRdtName, realUdigName,
+					name);
 		}
-		
-		SplashTableModel model = new SplashTableModel(spreadsheet);
-		
+
+		SplashTableModel model = new SplashTableModel(spreadsheet, rootNode);
+
 		currentSpreadsheet = new Spreadsheet(model, realRdtName, name);
-		
+
 		return currentSpreadsheet;
 	}
-	
+
 	/**
-	 * Resolves name of AWE project. 
+	 * Resolves name of AWE project.
 	 * 
-	 * @param projectName name of AWE project, if null than method computes name of default project
-	 * @return name of AWE project 
-	 * @throws SpreadsheetManagerException if project doesn't found 
+	 * @param projectName
+	 *            name of AWE project, if null than method computes name of
+	 *            default project
+	 * @return name of AWE project
+	 * @throws SpreadsheetManagerException
+	 *             if project doesn't found
 	 */
-	
-	private String resolveUDIGProjectName(String projectName) throws SpreadsheetManagerException {
+
+	private String resolveUDIGProjectName(String projectName)
+			throws SpreadsheetManagerException {
 		String realName = AWEProjectManager.findAWEProjectName(projectName);
-		
+
 		if (realName == null) {
 			throw new SpreadsheetManagerException(projectName);
 		}
-		
+
 		return realName;
 	}
-	
+
 	/**
 	 * Resolves name of Ruby project
 	 * 
-	 * @param udigName name of AWE project
-	 * @param rdtName name of Ruby project, if null than method computes name of default project
+	 * @param udigName
+	 *            name of AWE project
+	 * @param rdtName
+	 *            name of Ruby project, if null than method computes name of
+	 *            default project
 	 * @return name of Ruby project
-	 * @throws SpreadsheetManagerException if Ruby project doesnt' found or AWE project doesn't contain Ruby projects
+	 * @throws SpreadsheetManagerException
+	 *             if Ruby project doesnt' found or AWE project doesn't contain
+	 *             Ruby projects
 	 */
-	
-	private String resolveRDTProjectName(String udigName, String rdtName) throws SpreadsheetManagerException {
-		String realName = AWEProjectManager.findRubyProjectName(udigName, rdtName);
-		
+
+	private String resolveRDTProjectName(String udigName, String rdtName)
+			throws SpreadsheetManagerException {
+		String realName = AWEProjectManager.findRubyProjectName(udigName,
+				rdtName);
+
 		if (realName == null) {
 			throw new SpreadsheetManagerException(udigName, rdtName);
 		}
-		
+
 		return realName;
 	}
-	
+
 	/**
 	 * Returns Spreadsheet that is currently opened
 	 * 
 	 * @return
 	 */
-	
-	public Spreadsheet getActiveSpreadsheet() {		
+
+	public Spreadsheet getActiveSpreadsheet() {
 		return new Spreadsheet(activeModel, null, null);
 	}
-	
+
 	public void setActiveModel(SplashTableModel model) {
 		activeModel = model;
 	}
