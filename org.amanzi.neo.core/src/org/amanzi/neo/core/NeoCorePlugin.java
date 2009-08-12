9b12f5fef9b867b43e4e2fa5d9e297611e4c1165
artemij <artemij@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-08-12 08:44:05 +0000

    Realize Feature #568
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@1762 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.neo.core/src/org/amanzi/neo/core/NeoCorePlugin.java
+++ org.amanzi.neo.core/src/org/amanzi/neo/core/NeoCorePlugin.java
@@ -1,6 +1,7 @@
 package org.amanzi.neo.core;
 
 import org.amanzi.neo.core.database.services.AweProjectService;
+import org.amanzi.neo.core.database.services.UpdateBDManager;
 import org.amanzi.neo.core.preferences.NeoPreferencesInitializer;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Plugin;
@@ -8,101 +9,121 @@ import org.eclipse.core.runtime.Status;
 import org.osgi.framework.BundleContext;
 
 /**
- * Plugin class for org.amanzi.neo.core 
+ * Plugin class for org.amanzi.neo.core
  * 
  * @author Lagutko_N
  * @since 1.1.0
  */
 
 public class NeoCorePlugin extends Plugin {
-    
-    /*
-     * Plugin's ID 
-     */
-    
-    private static final String ID = "org.amanzi.neo.core";
-    
-    /*
-     * Plugin variable
-     */
 
-    static private NeoCorePlugin plugin;
-    
-    /*
-     * Initializer for AWE-specific Neo Preferences
-     */
-    
-    private NeoPreferencesInitializer initializer = new NeoPreferencesInitializer();
+	/*
+	 * Plugin's ID
+	 */
+
+	private static final String ID = "org.amanzi.neo.core";
+
+	/*
+	 * Plugin variable
+	 */
+
+	static private NeoCorePlugin plugin;
+
+	/*
+	 * Initializer for AWE-specific Neo Preferences
+	 */
+
+	private NeoPreferencesInitializer initializer = new NeoPreferencesInitializer();
 
 	private AweProjectService aweProjectService;
-    
-    /**
-     * Constructor for SplashPlugin.
-     */
-    public NeoCorePlugin() {
-        super();
-        plugin = this;  
-    }
-    
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-        plugin = this;
-    }
+	private UpdateBDManager updateBDManager;
 
-    /*
-     * (non-Javadoc)
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-     */
-    public void stop(BundleContext context) throws Exception {
-        plugin = null;
-        super.stop(context);
-    }
-    
-    /**
-     * Returns the shared instance.
-     */
-    public static NeoCorePlugin getDefault() {
-        return plugin;
-    }
-    
-    /**
-     * Returns initializer of NeoPreferences
-     *
-     * @return initializer of Neo Preferences
-     */
-    
-    public NeoPreferencesInitializer getInitializer() {
-        return initializer;
-    }
-    /**
-     * 
-     * @return awe project service
-     */
+	/**
+	 * Constructor for SplashPlugin.
+	 */
+	public NeoCorePlugin() {
+		super();
+		plugin = this;
+	}
+
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		updateBDManager = new UpdateBDManager();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static NeoCorePlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns initializer of NeoPreferences
+	 * 
+	 * @return initializer of Neo Preferences
+	 */
+
+	public NeoPreferencesInitializer getInitializer() {
+		return initializer;
+	}
+
+	/**
+	 * 
+	 * @return awe project service
+	 */
 	public AweProjectService getProjectService() {
-		if (aweProjectService==null){
-	        aweProjectService = new AweProjectService();
+		if (aweProjectService == null) {
+			aweProjectService = new AweProjectService();
 		}
-	    return aweProjectService;
-	}   
-    /**
-     * Sets initializer of NeoPreferences
-     *
-     * @param initializer new initializer for NeoPreferences
-     */
+		return aweProjectService;
+	}
 
-    public void setInitializer(NeoPreferencesInitializer initializer) {
-        this.initializer = initializer;
-    }
-    
-    /**
-     * Print a message and information about exception to Log
-     *
-     * @param message message
-     * @param e exception
-     */
+	/**
+	 * 
+	 * @return UpdateBDManager
+	 */
+	public UpdateBDManager getUpdateBDManager() {
+		return updateBDManager;
+	}
 
-    public static void error(String message, Throwable e) {
-        getDefault().getLog().log(new Status(IStatus.ERROR, ID, 0, message == null ? "" : message, e)); //$NON-NLS-1$
-    }
+	/**
+	 * Sets initializer of NeoPreferences
+	 * 
+	 * @param initializer
+	 *            new initializer for NeoPreferences
+	 */
+
+	public void setInitializer(NeoPreferencesInitializer initializer) {
+		this.initializer = initializer;
+	}
+
+	/**
+	 * Print a message and information about exception to Log
+	 * 
+	 * @param message
+	 *            message
+	 * @param e
+	 *            exception
+	 */
+
+	public static void error(String message, Throwable e) {
+		getDefault().getLog().log(
+				new Status(IStatus.ERROR, ID, 0,
+						message == null ? "" : message, e)); //$NON-NLS-1$
+	}
 
 }
