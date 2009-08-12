d1e52bb600090fff8733f4cffb3ac14029c26f45
artemij <artemij@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-08-12 10:18:51 +0000

    Realize Feature #568
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@1765 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.splash.neo4j/src/org/amanzi/splash/ui/neo4j/wizards/ExportScriptWizard.java
+++ org.amanzi.splash.neo4j/src/org/amanzi/splash/ui/neo4j/wizards/ExportScriptWizard.java
@@ -1,6 +1,15 @@
 package org.amanzi.splash.ui.neo4j.wizards;
 
+import java.net.URI;
+
+import org.amanzi.neo.core.NeoCorePlugin;
+import org.amanzi.neo.core.database.nodes.CellNode;
+import org.amanzi.neo.core.database.nodes.SpreadsheetNode;
+import org.amanzi.splash.neo4j.database.services.SpreadsheetService;
 import org.amanzi.splash.neo4j.swing.Cell;
+import org.amanzi.splash.neo4j.ui.SplashPlugin;
+import org.amanzi.splash.neo4j.utilities.ActionUtil;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.ui.PlatformUI;
 import org.rubypeople.rdt.internal.ui.wizards.NewFileCreationWizard;
 
@@ -8,37 +17,52 @@ import org.rubypeople.rdt.internal.ui.wizards.NewFileCreationWizard;
  * Wizard for exporting script that is base on New Ruby Script creation
  * 
  * @author Lagutko_N
- *
+ * 
  */
 
 public class ExportScriptWizard extends NewFileCreationWizard {
-	
-	//Cell for export
-	private Cell cell;
-	
-	public ExportScriptWizard(Cell cell) {
+
+	// Cell for export
+	private final Cell cell;
+	private final SpreadsheetNode spreadsheetNode;
+
+	public ExportScriptWizard(SpreadsheetNode spreadsheetNode, Cell cell) {
 		super();
+		this.spreadsheetNode = spreadsheetNode;
 		this.cell = cell;
 		init(PlatformUI.getWorkbench(), null);
 	}
-	
-	
 
-	public void addPages() {		
+	public void addPages() {
 		if (fPage == null) {
-			fPage= new ExportScriptWizardPage(cell);	
+			fPage = new ExportScriptWizardPage(cell);
 			fPage.init(getSelection());
-			
+
 		}
 		addPage(fPage);
 	}
-	
+
 	public boolean performFinish() {
-		//add ScriptURI to exported script
-		cell.setScriptURI(((ExportScriptWizardPage)super.getPages()[0]).getModifiedResource().getLocationURI());
+		final IResource modifiedResource = ((ExportScriptWizardPage) super
+				.getPages()[0]).getModifiedResource();
+		// add ScriptURI to exported script
+		URI uri = modifiedResource.getLocationURI();
+		ActionUtil.getInstance().runTask(new Runnable() {
+			@Override
+			public void run() {
+				SpreadsheetService spreadsheetService = SplashPlugin
+						.getDefault().getSpreadsheetService();
+				CellNode cellNode = spreadsheetService.getCellNode(
+						spreadsheetNode, cell.getCellID());
+				if (cellNode == null) {
+					cellNode = spreadsheetService.createCell(spreadsheetNode,
+							cell.getCellID());
+				}
+				NeoCorePlugin.getDefault().getProjectService().createScript(
+						cellNode, modifiedResource.getName());
+			}
+		}, false);
+		cell.setScriptURI(uri);
 		return super.performFinish();
 	}
-	
-	
-	
 }
