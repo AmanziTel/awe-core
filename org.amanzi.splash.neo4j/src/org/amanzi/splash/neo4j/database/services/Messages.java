4dcb6cdf0736a572b172051fd31b2414e2a7dfdc
lagutko <lagutko@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-08-14 15:32:32 +0000

    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@1784 0edadee9-8154-4c59-a661-b5f45207fcbd

---
+++ org.amanzi.splash.neo4j/src/org/amanzi/splash/neo4j/database/services/Messages.java
@@ -0,0 +1,31 @@
+package org.amanzi.splash.neo4j.database.services;
+
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+import org.amanzi.neo.core.database.exception.SplashDatabaseExceptionMessages;
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+
+    private static final String BUNDLE_NAME = Messages.class.getName();
+    private static ResourceBundle resourceBundle = ResourceBundle.getBundle(BUNDLE_NAME);
+    
+    public static String Copy_Error_Title;
+    
+    private Messages() {
+    }
+
+    public static ResourceBundle getResourceBundle() {
+        return resourceBundle;
+    }
+    
+    public static String getFormattedString(String key, String ... args) {
+        return MessageFormat.format(key, (Object[])args);
+    }
+
+    static {
+        NLS.initializeMessages(BUNDLE_NAME, SplashDatabaseExceptionMessages.class);
+    }
+    
+}
