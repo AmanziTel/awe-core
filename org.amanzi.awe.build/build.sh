d2c47328b2f2b679c94ebbb2114da35eb3a110bd
amabdelsalam <amabdelsalam@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-09-27 15:45:35 +0000

    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@2149 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.awe.build/build.sh
+++ org.amanzi.awe.build/build.sh
@@ -3,6 +3,6 @@ targetDirectory=${HOME}/build/udig-sdk
 equinoxVersion=1.0.1.R33x_v20080118
 pdeBuildVersion=3.3.2.v20071019
 
-java -jar ${targetDirectory}/plugins/org.eclipse.equinox.launcher_${equinoxVersion}.jar -application org.eclipse.ant.core.antRunner -buildfile $HOME/.hudson/jobs/awe/workspace/awe/org.amanzi.awe.build/build.xml
+java -jar ${HOME}/build/udig-sdk/plugins/org.eclipse.equinox.launcher_1.0.1.R33x_v20080118.jar -application org.eclipse.ant.core.antRunner -buildfile $HOME/.hudson/jobs/awe/workspace/awe/org.amanzi.awe.build/build.xml
 
 
