878e498507a2514d7d09736c48bd99ecfcfcadaa
pechko_elena <pechko_elena@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-10-16 20:49:10 +0000

    Copyright headers have been added or corrected. 7 files have been skipped for manual editing.
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@2274 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.neo.core/src/org/amanzi/neo/core/enums/MeasurementRelationshipTypes.java
+++ org.amanzi.neo.core/src/org/amanzi/neo/core/enums/MeasurementRelationshipTypes.java
@@ -2,15 +2,13 @@
  * http://awe.amanzi.org
  * (C) 2008-2009, AmanziTel AB
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation;
- * version 3.0 of the License.
+ * This library is provided under the terms of the Eclipse Public License
+ * as described at http://www.eclipse.org/legal/epl-v10.html. Any use,
+ * reproduction or distribution of the library constitutes recipient's
+ * acceptance of this agreement.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This library is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 package org.amanzi.neo.core.enums;
 
