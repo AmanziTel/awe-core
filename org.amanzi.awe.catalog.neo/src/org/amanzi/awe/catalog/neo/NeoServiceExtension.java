4222371eb2cd791f547cd5a7ba837edffab7af6a
artemij <artemij@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-10-21 12:40:33 +0000

    Feature #739
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@2308 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.awe.catalog.neo/src/org/amanzi/awe/catalog/neo/NeoServiceExtension.java
+++ org.amanzi.awe.catalog.neo/src/org/amanzi/awe/catalog/neo/NeoServiceExtension.java
@@ -22,8 +22,8 @@ import net.refractions.udig.catalog.IService;
 import net.refractions.udig.catalog.ServiceExtension;
 import net.refractions.udig.catalog.URLUtils;
 
-/*import org.amanzi.awe.startup.splash.Activator;
- import org.eclipse.core.runtime.IProgressMonitor;*/
+import org.amanzi.awe.startup.splash.Activator;
+import org.eclipse.core.runtime.IProgressMonitor;
 
 public class NeoServiceExtension implements ServiceExtension {
     private static final String NEOSTORE_DIR = "neostore";
@@ -59,14 +59,18 @@ public class NeoServiceExtension implements ServiceExtension {
 
     
     public IService createService(URL id, Map<String, Serializable> params) {
+
+
+        // sets job name for splash monitor
+        try {
+            IProgressMonitor monitor = Activator.getDefault().getSpalhMonitor();
+            if (monitor != null) {
+                monitor.subTask("Initialize Neo Service");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         // good defensive programming
-
-        /*
-         * try { IProgressMonitor monitor = Activator.getDefault().getSpalhMonitor(); if (monitor !=
-         * null) { monitor.subTask("Initialize Neo Service"); } } catch (Exception e) {
-         * e.printStackTrace(); }
-         */
-
         if (params == null) {
             return null;
         }
