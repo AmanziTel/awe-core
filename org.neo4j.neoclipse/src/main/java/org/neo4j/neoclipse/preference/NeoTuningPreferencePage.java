3f6d286483f9db00c26e3789311f2473580407ff
artemij <artemij@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-11-04 12:34:02 +0000

    #feature 780
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@2383 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.neo4j.neoclipse/src/main/java/org/neo4j/neoclipse/preference/NeoTuningPreferencePage.java
+++ org.neo4j.neoclipse/src/main/java/org/neo4j/neoclipse/preference/NeoTuningPreferencePage.java
@@ -32,35 +32,36 @@ public class NeoTuningPreferencePage extends AbstractPreferencePage {
     private static final String LABEL_PROPERTIES_KEYS = "Properties Keys";
     private static final String LABEL_PROPERTIES_STRING = "String Properties";
     private static final String LABEL_PROPERTIES_ARRAY = "Array Properties";
+    private static final String PROPTERTY_NOTE = "stating that changes will only be effective after a restart";
 
     @Override
     protected void createFieldEditors() {
         IntegerFieldEditor nodes = new IntegerFieldEditor(NeoPreferences.NEOSTORE_NODES, LABEL_NODES, getFieldEditorParent());
         nodes.setValidRange(1, Integer.MAX_VALUE);
-        addField(nodes);
+        addField(nodes, PROPTERTY_NOTE);
         IntegerFieldEditor relationships = new IntegerFieldEditor(NeoPreferences.NEOSTORE_RELATIONSHIPS, LABEL_RELATIONSHIPS,
                 getFieldEditorParent());
         relationships.setValidRange(1, Integer.MAX_VALUE);
-        addField(relationships);
+        addField(relationships, PROPTERTY_NOTE);
         IntegerFieldEditor properties = new IntegerFieldEditor(NeoPreferences.NEOSTORE_PROPERTIES, LABEL_PROPERTIES,
                 getFieldEditorParent());
         properties.setValidRange(1, Integer.MAX_VALUE);
-        addField(properties);
+        addField(properties, PROPTERTY_NOTE);
         IntegerFieldEditor properties_ind = new IntegerFieldEditor(NeoPreferences.NEOSTORE_PROPERTIES_INDEX,
                 LABEL_PROPERTIES_INDEX, getFieldEditorParent());
         properties_ind.setValidRange(1, Integer.MAX_VALUE);
-        addField(properties_ind);
+        addField(properties_ind, PROPTERTY_NOTE);
         IntegerFieldEditor properties_keys = new IntegerFieldEditor(NeoPreferences.NEOSTORE_PROPERTIES_KEYS, LABEL_PROPERTIES_KEYS,
                 getFieldEditorParent());
         properties_keys.setValidRange(1, Integer.MAX_VALUE);
-        addField(properties_keys);
+        addField(properties_keys, PROPTERTY_NOTE);
         IntegerFieldEditor properties_string = new IntegerFieldEditor(NeoPreferences.NEOSTORE_PROPERTIES_STRING,
                 LABEL_PROPERTIES_STRING, getFieldEditorParent());
         properties_string.setValidRange(1, Integer.MAX_VALUE);
-        addField(properties_string);
+        addField(properties_string, PROPTERTY_NOTE);
         IntegerFieldEditor properties_arrays = new IntegerFieldEditor(NeoPreferences.NEOSTORE_PROPERTIES_ARRAYS,
                 LABEL_PROPERTIES_ARRAY, getFieldEditorParent());
         properties_arrays.setValidRange(1, Integer.MAX_VALUE);
-        addField(properties_arrays);
+        addField(properties_arrays, PROPTERTY_NOTE);
     }
 }
