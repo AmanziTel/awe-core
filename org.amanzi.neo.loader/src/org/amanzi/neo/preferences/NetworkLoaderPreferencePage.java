722a73ef9c4a37772aa80c23d72139a982ea8992
cat <cat@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-11-14 02:51:47 +0000

    removed inexplicable private static finals and added margins to group
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@2393 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.neo.loader/src/org/amanzi/neo/preferences/NetworkLoaderPreferencePage.java
+++ org.amanzi.neo.loader/src/org/amanzi/neo/preferences/NetworkLoaderPreferencePage.java
@@ -17,6 +17,7 @@ import org.amanzi.neo.loader.internal.NeoLoaderPlugin;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -26,46 +27,33 @@ import org.eclipse.ui.IWorkbenchPreferencePage;
 
 /**
  * Network Loader loading preference page
- * <p>
- * </p>
  * 
  * @author Cinkel_A
  * @since 1.0.0
  */
 public class NetworkLoaderPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
-    private static final String NH_CITY = "city";
-    private static final String NH_MSC = "msc";
-    private static final String NH_BSC = "bsc";
-    private static final String NH_SITE = "site";
-    private static final String NH_SECTOR = "sector";
-    private static final String NH_LATITUDE = "latitude";
-    private static final String NH_LONGITUDE = "longitude";
-
     @Override
     protected void createFieldEditors() {
         Composite attributePanel = getFieldEditorParent();
-        attributePanel.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL | GridData.FILL_BOTH));
+        attributePanel.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, true, true));
         attributePanel.setLayout(new GridLayout());
 
         Group attributeGroup = new Group(attributePanel, attributePanel.getStyle());
-        attributeGroup.setText("When loading network data find special types that match the following:");
+        attributeGroup.setText("When loading network data find special types that match:");
         attributeGroup.setLayout(new GridLayout());
+        Composite marginPanel = new Composite(attributeGroup, attributeGroup.getStyle());
+        GridLayout layout = new GridLayout();
+        layout.marginHeight = 15;
+        layout.marginWidth = 15;
+        marginPanel.setLayout(layout);
 
-        // group.setText("When loading network data find special types that match the following:");
-        StringFieldEditor strEd = new StringFieldEditor(DataLoadPreferences.NH_CITY, NH_CITY, attributeGroup);
-        addField(strEd);
-        strEd = new StringFieldEditor(DataLoadPreferences.NH_MSC, NH_MSC, attributeGroup);
-        addField(strEd);
-        strEd = new StringFieldEditor(DataLoadPreferences.NH_BSC, NH_BSC, attributeGroup);
-        addField(strEd);
-        strEd = new StringFieldEditor(DataLoadPreferences.NH_SITE, NH_SITE, attributeGroup);
-        addField(strEd);
-        strEd = new StringFieldEditor(DataLoadPreferences.NH_SECTOR, NH_SECTOR, attributeGroup);
-        addField(strEd);
-        strEd = new StringFieldEditor(DataLoadPreferences.NH_LATITUDE, NH_LATITUDE, attributeGroup);
-        addField(strEd);
-        strEd = new StringFieldEditor(DataLoadPreferences.NH_LONGITUDE, NH_LONGITUDE, attributeGroup);
-        addField(strEd);
+        addField(new StringFieldEditor(DataLoadPreferences.NH_CITY, "city", marginPanel));
+        addField(new StringFieldEditor(DataLoadPreferences.NH_MSC, "msc", marginPanel));
+        addField(new StringFieldEditor(DataLoadPreferences.NH_BSC, "bsc", marginPanel));
+        addField(new StringFieldEditor(DataLoadPreferences.NH_SITE, "site", marginPanel));
+        addField(new StringFieldEditor(DataLoadPreferences.NH_SECTOR, "sector", marginPanel));
+        addField(new StringFieldEditor(DataLoadPreferences.NH_LATITUDE, "latitude", marginPanel));
+        addField(new StringFieldEditor(DataLoadPreferences.NH_LONGITUDE, "longitude", marginPanel));
     }
 
     @Override
