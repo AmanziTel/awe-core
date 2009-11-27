5689dc2fe2e147990d1fe5232e520634e7e5ccd1
lagutko <lagutko@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-11-27 13:41:55 +0000

    Bug #838
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@2442 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.splash/src/org/amanzi/splash/importer/AbstractImporter.java
+++ org.amanzi.splash/src/org/amanzi/splash/importer/AbstractImporter.java
@@ -15,26 +15,11 @@ package org.amanzi.splash.importer;
 
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
-import java.net.MalformedURLException;
 
-import org.amanzi.integrator.awe.AWEProjectManager;
-import org.amanzi.neo.core.NeoCorePlugin;
-import org.amanzi.neo.core.database.nodes.AweProjectNode;
-import org.amanzi.neo.core.database.nodes.CellNode;
-import org.amanzi.neo.core.database.nodes.RubyProjectNode;
-import org.amanzi.neo.core.database.nodes.SpreadsheetNode;
-import org.amanzi.neo.core.database.services.AweProjectService;
-import org.amanzi.neo.core.service.NeoServiceProvider;
-import org.amanzi.splash.swing.Cell;
-import org.amanzi.splash.utilities.NeoSplashUtil;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
+import org.amanzi.splash.utilities.SpreadsheetCreator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.operation.IRunnableWithProgress;
-import org.neo4j.api.core.NeoService;
-import org.neo4j.api.core.Transaction;
-
 
 /**
  * Abstract class that provides basic functionality for importing data into Splash
@@ -42,37 +27,12 @@ import org.neo4j.api.core.Transaction;
  * @author Lagutko_N
  * @since 1.0.0
  */
-public abstract class AbstractImporter implements IRunnableWithProgress {
-    
-    /*
-     * Path to Project that will contain new Spreadsheet
-     */
-    protected IPath containerPath;
-    
-    /*
-     * FileName of file to Import
-     */
-    protected String fileName;
+public abstract class AbstractImporter extends SpreadsheetCreator implements IRunnableWithProgress {
     
     /*
      * Content of file to import
      */
     protected InputStream fileContent;
-    
-    /*
-     * Project Service
-     */
-    private AweProjectService projectService;
-    
-    /*
-     * Neo Service
-     */
-    private NeoService neoService;
-    
-    /*
-     * Node of created Spreadsheet
-     */
-    protected SpreadsheetNode spreadsheetNode;
     
     /*
      * Size of File
@@ -88,135 +48,12 @@ public abstract class AbstractImporter implements IRunnableWithProgress {
      * @param fileSize size of File to import
      */
     public AbstractImporter(IPath containerPath, String fileName, InputStream stream, long fileSize) {
-        this.containerPath = containerPath;
-        this.fileName = fileName;
+        super(containerPath, fileName);
         this.fileContent = stream;
         this.fileSize = fileSize;
-        
-        //initializing services
-        projectService = NeoCorePlugin.getDefault().getProjectService();
-        neoService = NeoServiceProvider.getProvider().getService();
     }
     
     @Override
     public abstract void run(IProgressMonitor monitor) throws InvocationTargetException;
     
-    /**
-     * Returns created Spreadsheet
-     *
-     * @return created spreadsheet
-     */
-    public SpreadsheetNode getSpreadsheet() {
-        return spreadsheetNode;
-    }
-    
-    /**
-     *  Creates a Spreadsheet 
-     */
-    protected void createSpreadsheet() {
-        if (spreadsheetNode != null) {
-            return;
-        }
-        //computing names of Ruby and AWE projects
-        IProject rubyProject = (IProject)ResourcesPlugin.getWorkspace().getRoot().findMember(containerPath);
-        
-        String rubyProjectName = rubyProject.getName();        
-        String aweProjectName = AWEProjectManager.getAWEprojectNameFromResource(rubyProject);
-        
-        //computing nodes for Ruby and AWE projects
-        AweProjectNode aweProjectNode = projectService.findOrCreateAweProject(aweProjectName);
-        RubyProjectNode rubyProjectNode = projectService.findOrCreateRubyProject(aweProjectNode, rubyProjectName);
-        
-        //computing name of Spreadsheet
-        String spreadsheetName = getSpreadsheetName();
-        
-        //validate name of Spreadsheet. If for example spreadsheet with name 'sheet' already exists
-        //than it will try name 'sheet1', 'sheet2' etc.
-        int i = 1;        
-        String newSpreadsheetName = spreadsheetName;
-        do {            
-            spreadsheetName = newSpreadsheetName;
-            spreadsheetNode = projectService.findSpreadsheet(rubyProjectNode, spreadsheetName);
-            newSpreadsheetName = spreadsheetName.concat(Integer.toString(i++));
-        } while (spreadsheetNode != null);
-        
-        //create a new Spreadsheet
-        spreadsheetNode = projectService.findOrCreateSpreadSheet(rubyProjectNode, spreadsheetName);
-        
-        //also create a Spreadsheet in AWE Project Structure
-        try {
-            AWEProjectManager.createNeoSpreadsheet(rubyProject, spreadsheetName, NeoSplashUtil.getSpeadsheetURL(spreadsheetName));
-        }
-        catch (MalformedURLException e) {
-            //can't happen
-        }
-    }
-    
-    /**
-     * Convert name of File to Import to name of Spreadsheet
-     *
-     * @return name of Spreadsheet
-     */
-    private String getSpreadsheetName() {
-        //if FileName contains extension than name of spreadsheet is a filename without extension
-        int pointIndex = fileName.indexOf(".");
-        if (pointIndex > 0) {
-            return fileName.substring(0, pointIndex);
-        }
-        else {
-            return fileName;
-        }
-    }
-    
-    /**
-     * Saves a Cell
-     *
-     * @param cell cell to save
-     */
-    protected void saveCell(Cell cell) {
-        //import Cell to database in given Row and Column
-        importCell(cell, cell.getRow() + 1, cell.getColumn() + 1);        
-    }
-    
-    
-    int count = 0;
-    /**
-     * Updates Transaction.
-     * To prevent JavaHeapSpace exception we can call this method for example after processing 1000 rows
-     *
-     * @param transaction transaction to update
-     * @return new transaction
-     */
-    protected Transaction updateTransaction(Transaction transaction) {
-        transaction.success();
-        transaction.finish();
-        
-        NeoServiceProvider.getProvider().commit();
-        Transaction result = neoService.beginTx();
-        
-        return result;
-    }
-    
-    /**
-     * Import Cell to database
-     *
-     * @param cell cell to import
-     * @param row row of Cell
-     * @param column column of Cell
-     * @return row that contain imported cell or null
-     */
-    private void importCell(Cell cell, int row, int column) {
-        createSpreadsheet();
-        
-        //create a new cell
-        CellNode cellNode = new CellNode(neoService.createNode());
-        //set a value to cell
-        cellNode.setValue(cell.getValue());
-        cellNode.setDefinition(cell.getDefinition());
-        
-        cellNode.setCellColumn(column);
-        cellNode.setCellRow(row);
-        
-        spreadsheetNode.addCell(cellNode);
-    }
 }
