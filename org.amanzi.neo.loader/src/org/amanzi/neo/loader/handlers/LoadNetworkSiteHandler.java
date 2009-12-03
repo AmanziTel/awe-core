a7151b6d99b02fa9460236bd675035165d162169
artemij <artemij@0edadee9-8154-4c59-a661-b5f45207fcbd>
2009-12-03 10:02:23 +0000

    feature #802 827
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@2494 0edadee9-8154-4c59-a661-b5f45207fcbd

---
+++ org.amanzi.neo.loader/src/org/amanzi/neo/loader/handlers/LoadNetworkSiteHandler.java
@@ -0,0 +1,48 @@
+/* AWE - Amanzi Wireless Explorer
+ * http://awe.amanzi.org
+ * (C) 2008-2009, AmanziTel AB
+ *
+ * This library is provided under the terms of the Eclipse Public License
+ * as described at http://www.eclipse.org/legal/epl-v10.html. Any use,
+ * reproduction or distribution of the library constitutes recipient's
+ * acceptance of this agreement.
+ *
+ * This library is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+package org.amanzi.neo.loader.handlers;
+
+import org.amanzi.neo.wizards.NetworkSiteImportWizard;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IImportWizard;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+/**
+ * <p>
+ * Load Network Site handler
+ * </p>
+ * 
+ * @author Cinkel_A
+ * @since 1.0.0
+ */
+public class LoadNetworkSiteHandler extends AbstractHandler {
+
+    @Override
+    public Object execute(ExecutionEvent arg0) throws ExecutionException {
+        IWorkbenchWindow workbenchWindow = HandlerUtil.getActiveWorkbenchWindowChecked(arg0);
+        IImportWizard wizard = new NetworkSiteImportWizard();
+        wizard.init(workbenchWindow.getWorkbench(), null);
+        Shell parent = workbenchWindow.getShell();
+        WizardDialog dialog = new WizardDialog(parent, wizard);
+        dialog.create();
+        dialog.open();
+        return null;
+    }
+
+}
