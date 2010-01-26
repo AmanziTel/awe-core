59a2885310e31fa921787623eaf2ad17c30a595f
artemij <artemij@0edadee9-8154-4c59-a661-b5f45207fcbd>
2010-01-26 11:22:47 +0000

    feature #903
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@2708 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.awe.views.tree.drive/src/org/amanzi/awe/views/tree/drive/views/DriveNeoNode.java
+++ org.amanzi.awe.views.tree.drive/src/org/amanzi/awe/views/tree/drive/views/DriveNeoNode.java
@@ -18,12 +18,12 @@ import org.amanzi.awe.views.network.proxy.NeoNode;
 import org.amanzi.neo.core.INeoConstants;
 import org.amanzi.neo.core.enums.GeoNeoRelationshipTypes;
 import org.amanzi.neo.core.enums.NetworkRelationshipTypes;
-import org.amanzi.neo.core.enums.ProbeCallRelationshipType;
 import org.amanzi.neo.core.utils.NeoUtils;
 import org.neo4j.api.core.Direction;
 import org.neo4j.api.core.Node;
 import org.neo4j.api.core.ReturnableEvaluator;
 import org.neo4j.api.core.StopEvaluator;
+import org.neo4j.api.core.TraversalPosition;
 import org.neo4j.api.core.Traverser;
 import org.neo4j.api.core.Traverser.Order;
 
@@ -53,7 +53,13 @@ public class DriveNeoNode extends NeoNode {
         ArrayList<NeoNode> children = new ArrayList<NeoNode>();
         Traverser traverse;
         if (isFileNode() || NeoUtils.isVirtualDataset(node)) {
-            traverse = node.traverse(Order.BREADTH_FIRST, StopEvaluator.END_OF_GRAPH, ReturnableEvaluator.ALL_BUT_START_NODE,
+            traverse = node.traverse(Order.BREADTH_FIRST, new StopEvaluator() {
+
+                @Override
+                public boolean isStopNode(TraversalPosition currentPos) {
+                    return !currentPos.isStartNode() && NeoUtils.isFileNode(currentPos.currentNode());
+                }
+            }, ReturnableEvaluator.ALL_BUT_START_NODE,
                     GeoNeoRelationshipTypes.NEXT, Direction.OUTGOING);
         } else if (isDatasetNode() || isDirectoryNode()) {        	
             traverse = node.traverse(Order.BREADTH_FIRST, StopEvaluator.DEPTH_ONE, ReturnableEvaluator.ALL_BUT_START_NODE,
