2ce46432856b495a3a7678466e5b2500bc43c35d
zaelenchits <zaelenchits@0edadee9-8154-4c59-a661-b5f45207fcbd>
2010-03-18 14:30:09 +0000

    Feature #1015
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@2969 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.awe.views.drive/src/org/amanzi/awe/views/drive/preferences/PropertyListPreferences.java
+++ org.amanzi.awe.views.drive/src/org/amanzi/awe/views/drive/preferences/PropertyListPreferences.java
@@ -16,6 +16,7 @@ package org.amanzi.awe.views.drive.preferences;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.amanzi.neo.index.MultiPropertyIndex;
 import org.amanzi.neo.loader.internal.NeoLoaderPlugin;
@@ -23,6 +24,7 @@ import org.amanzi.neo.preferences.DataLoadPreferences;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.EditingSupport;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.ITableLabelProvider;
@@ -32,6 +34,7 @@ import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -56,6 +59,7 @@ public class PropertyListPreferences extends PreferencePage implements IWorkbenc
 
     private static final int MIN_FIELD_WIDTH = 50;
     private static final int MAX_PROP_COUNT = 6;
+    public static final Color BAD_COLOR = new Color(null, 255, 0, 0);
     private final List<RowWr> propertyLists=new ArrayList<RowWr>();
     private Composite mainFrame;
     MultiPropertyIndex<Long> timestampIndex = null;
@@ -81,7 +85,7 @@ public class PropertyListPreferences extends PreferencePage implements IWorkbenc
         createTableColumn();
         
         viewer.setContentProvider(provider);
-        viewer.setLabelProvider(new TableLabelProvider());
+        // viewer.setLabelProvider(new TableLabelProvider());
         GridData layoutData = new GridData(SWT.FILL, SWT.FILL, false, false, 3, 1);
         viewer.getControl().setLayoutData(layoutData);
         layoutData.grabExcessVerticalSpace=true;
@@ -122,15 +126,18 @@ public class PropertyListPreferences extends PreferencePage implements IWorkbenc
     }
     @Override
     public boolean performOk() {
-        StringBuilder result=new StringBuilder();
+        StringBuilder result = new StringBuilder();
+        // checkEmpty();
        for (RowWr wr:propertyLists){
-           result.append(DataLoadPreferences.CRS_DELIMETERS);
+            if (!wr.isEmpty()) {
+                result.append(DataLoadPreferences.CRS_DELIMETERS);
                result.append(wr.getListName());
                result.append(DataLoadPreferences.CRS_DELIMETERS);
                result.append(wr.getProperties());
+            }
        }
        String string = result.length()>0?result.substring(DataLoadPreferences.CRS_DELIMETERS.length()):result.toString();
-    getPreferenceStore().setValue(DataLoadPreferences.PROPERY_LISTS,string);
+        getPreferenceStore().setValue(DataLoadPreferences.PROPERY_LISTS, string);
         return true;
         
     }
@@ -148,6 +155,7 @@ public class PropertyListPreferences extends PreferencePage implements IWorkbenc
         col.setText("List name");
         col.setWidth(100);
         col.setResizable(true);
+        column.setLabelProvider(new ColLabelProvider(0));
 
         column.setEditingSupport(new TableEditableSupport(viewer, true));
         
@@ -156,20 +164,51 @@ public class PropertyListPreferences extends PreferencePage implements IWorkbenc
         col.setText("Properties");
         col.setWidth(200);
         col.setResizable(true);
+        column.setLabelProvider(new ColLabelProvider(1));
 
         column.setEditingSupport(new TableEditableSupport(viewer, false));
         table.setHeaderVisible(true);
         table.setLinesVisible(true);
+
         viewer.refresh();
         
     }
-    
 
-    
+    private class ColLabelProvider extends ColumnLabelProvider {
+
+        private int i;
+
+        /**
+         * @param i
+         */
+        public ColLabelProvider(int i) {
+            super();
+            this.i = i;
+        }
+
+        @Override
+        public Color getForeground(Object element) {
+            RowWr wrapper = (RowWr)element;
+            boolean result;
+            if (i == 0) {
+                result = wrapper.isValidName();
+            } else {
+                result = wrapper.isValidProperties();
+            }
+            return result ? null : BAD_COLOR;
+        }
+
+        @Override
+        public String getText(Object element) {
+            RowWr wrapper = (RowWr)element;
+            return i == 0 ? wrapper.getListName() : wrapper.getProperties();
+        }
+    }
+
     /*
      * The content provider class is responsible for providing objects to the view. It can wrap
      * existing objects in adapters or simply return objects as-is. These objects may be sensitive
-     * to the current input of the view, or ignore it and always show the same content (like Task
+     * to the current input of the view, or ignore it and always show the same content (like Taskc
      * List, for example).
      */
 
@@ -202,6 +241,8 @@ public class PropertyListPreferences extends PreferencePage implements IWorkbenc
             RowWr wrapper=(RowWr)element;
             return columnIndex==0?wrapper.getListName():wrapper.getProperties();
         }
+
+
         
     }
     
@@ -236,13 +277,17 @@ public class PropertyListPreferences extends PreferencePage implements IWorkbenc
 
         @Override
         protected void setValue(Object element, Object value) {
+            String sValue = ((String)value).trim();
             if(isName){
-                ((RowWr)element).setListName((String)value);                
+                ((RowWr)element).setListName(sValue);
             }else{
-                ((RowWr)element).setProperties((String)value);
+                ((RowWr)element).setProperties(sValue);
             }
+            checkEmpty();
             validate();
-            getViewer().update(element, null);
+            // getViewer().update(element, null);
+            
+
         }        
         
     }
@@ -260,6 +305,14 @@ public class PropertyListPreferences extends PreferencePage implements IWorkbenc
             this.listName = listName;
             this.properties = properties;
         }
+
+        /**
+         * @return
+         */
+        public boolean isEmpty() {
+            return listName.isEmpty() && properties.isEmpty();
+        }
+
         /**
          * @return Returns the listName.
          */
@@ -284,6 +337,45 @@ public class PropertyListPreferences extends PreferencePage implements IWorkbenc
         public void setProperties(String properties) {
             this.properties = properties;
         }
+
+        public boolean isValidName() {
+            if (isEmpty()) {
+                return true;
+            }
+            if (listName.isEmpty() || listName.contains(DataLoadPreferences.CRS_DELIMETERS)) {
+                return false;
+            }
+
+            for(RowWr row : propertyLists){
+                if(row == this){
+                    continue;
+                }
+                if (row.getListName().equals(this.listName)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public boolean isValidProperties() {
+            if (isEmpty()) {
+                return true;
+            }
+            if (properties.isEmpty() || properties.contains(DataLoadPreferences.CRS_DELIMETERS)) {
+                return false;
+            }
+            if (Pattern.matches("(.*,( )*,.*)|(^,$)", properties)) {
+                return false;
+            }
+            return true;
+        }
+
+        /**
+         * @return
+         */
+        public boolean isValid() {
+            return isValidName() && isValidProperties();
+        }
         
     }
 
@@ -296,9 +388,36 @@ public class PropertyListPreferences extends PreferencePage implements IWorkbenc
 
     /**
      *
+     */
+    private void checkEmpty() {
+        ArrayList<RowWr> newPropertyLists = new ArrayList<RowWr>(propertyLists.size() + 1);
+        for (RowWr row : propertyLists) {
+            if (!row.getListName().isEmpty() || !row.getProperties().isEmpty()) {
+                newPropertyLists.add(row);
+            }
+        }
+        // Collections.sort(newPropertyLists, new Comparator<RowWr>() {
+        // @Override
+        // public int compare(RowWr arg0, RowWr arg1) {
+        // return arg0.getListName().compareTo(arg1.getListName());
+        // }
+        // });
+        propertyLists.clear();
+        propertyLists.addAll(newPropertyLists);
+        propertyLists.add(new RowWr("", ""));
+        viewer.setInput("");
+    }
+
+    /**
      * @return
      */
     private boolean isPageValid() {
+        for (RowWr row : propertyLists) {
+            if (!row.isValid()) {
+                return false;
+            }
+
+        }
         return true;
     }
 @Override
