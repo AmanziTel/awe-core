3c823768ddc74d2637a577d3f54568efea5d3086
lagutko <lagutko@0edadee9-8154-4c59-a661-b5f45207fcbd>
2010-04-19 19:39:28 +0000

    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@3132 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.neo.loader/src/org/amanzi/neo/loader/GPEHLoader.java
+++ org.amanzi.neo.loader/src/org/amanzi/neo/loader/GPEHLoader.java
@@ -38,17 +38,23 @@ import org.amanzi.awe.l3messages.rrc.EventResults;
 import org.amanzi.awe.l3messages.rrc.FrequencyInfo;
 import org.amanzi.awe.l3messages.rrc.FrequencyInfoFDD;
 import org.amanzi.awe.l3messages.rrc.FrequencyInfoTDD;
+import org.amanzi.awe.l3messages.rrc.GSM_MeasuredResults;
 import org.amanzi.awe.l3messages.rrc.IntegrityCheckInfo;
 import org.amanzi.awe.l3messages.rrc.InterFreqCellMeasuredResultsList;
 import org.amanzi.awe.l3messages.rrc.InterFreqMeasuredResults;
 import org.amanzi.awe.l3messages.rrc.InterFreqMeasuredResultsList;
+import org.amanzi.awe.l3messages.rrc.InterRATMeasuredResults;
+import org.amanzi.awe.l3messages.rrc.InterRATMeasuredResultsList;
+import org.amanzi.awe.l3messages.rrc.IntraFreqMeasuredResultsList;
 import org.amanzi.awe.l3messages.rrc.MeasuredResults;
 import org.amanzi.awe.l3messages.rrc.MeasuredResultsList;
 import org.amanzi.awe.l3messages.rrc.MeasuredResultsOnRACH;
 import org.amanzi.awe.l3messages.rrc.MeasurementIdentity;
 import org.amanzi.awe.l3messages.rrc.MeasurementReport;
+import org.amanzi.awe.l3messages.rrc.UE_InternalMeasuredResults;
 import org.amanzi.awe.l3messages.rrc.UL_DCCH_Message;
 import org.amanzi.awe.l3messages.rrc.UL_DCCH_MessageType;
+import org.amanzi.awe.l3messages.rrc.CellMeasuredResults.ModeSpecificInfoChoiceType.FddSequenceType;
 import org.amanzi.awe.l3messages.rrc.FrequencyInfo.ModeSpecificInfoChoiceType;
 import org.amanzi.neo.core.INeoConstants;
 import org.amanzi.neo.core.database.services.events.UpdateViewEventType;
@@ -80,7 +86,17 @@ import org.neo4j.graphdb.Transaction;
  * @since 1.0.0
  */
 public class GPEHLoader extends AbstractLoader {
-    /** int KEY_EVENT field */
+	
+	/**
+	 * Prefixes for Property Names of Measurement Report values
+	 */
+    private static final String GPEH_RRC_MR_BSIC_PREFIX = "MR_BSIC";
+	private static final String GPEH_RRC_MR_UE_TX_POWER_PREFIX = "MR_UE-TX-POWER";
+	private static final String GPEH_RRC_MR_RSCP_PREFIX = "MR_RSCP";
+	private static final String GPEH_RRC_MR_ECNO_PREFIX = "MR_ECNO";
+	
+	
+	/** int KEY_EVENT field */
     private static final int KEY_EVENT = 1;
     private final static Pattern mainFilePattern = Pattern.compile("(^.*)(_Mp0\\.)(.*$)");
     private static final int COUNT_LEN = 1000;
@@ -93,6 +109,16 @@ public class GPEHLoader extends AbstractLoader {
     private Node lastCellNode;
     private final LinkedHashMap<String, Header> headers;
     private int eventsCount;
+    
+    /*
+     * Index of RSCP Property in Event
+     */
+    private int rscpIndex;
+    
+    /*
+     * Index of Ec/N0 Property in Event
+     */
+    private int ecnoIndex;
 
     /**
      * Constructor
@@ -254,7 +280,8 @@ public void printStats(boolean verbose) {
                 if (entry.getKey() == Parameters.EVENT_PARAM_MESSAGE_CONTENTS) {
                     if (event.getId() == Events.RRC_MEASUREMENT_REPORT.getId()) {
                         byte[] messageContent = (byte[])entry.getValue();
-                        UL_DCCH_Message message = MessageDecoder.getInstance().parseRRCMeasurementReport2(messageContent);                        
+                        UL_DCCH_Message message = MessageDecoder.getInstance().parseRRCMeasurementReport(messageContent);
+                        saveMessageInfo(message, eventNode);
                     }
                 }
                 else {
@@ -272,6 +299,101 @@ public void printStats(boolean verbose) {
         } finally {
             tx.finish();
         }
+    }
+    
+    /**
+     * Save parsed result of RRC Measurement Report 
+     * 
+     * @param message parsed RRC Measurement Report 
+     * @param eventNode node of Event
+     */
+    private void saveMessageInfo(UL_DCCH_Message message, Node eventNode) {
+    	if (message == null) {
+    		//message is null than stop
+    		return;
+    	}
+    	
+    	UL_DCCH_MessageType messageType = message.getMessage();
+    	
+    	if (messageType == null) {
+    		//if message type is null than stop
+    		return;
+    	}
+    	
+    	//initialize indexes of fields
+    	rscpIndex = 1;
+    	ecnoIndex = 1;
+    	int bsicIndex = 1;
+    	
+    	if (messageType.isMeasurementReportSelected()) {
+    		//get a MeasurementReport
+    		MeasurementReport report = messageType.getMeasurementReport();
+    		
+    		if (report.isMeasuredResultsPresent()) {
+    			//get MeasuredResults
+    			MeasuredResults result = report.getMeasuredResults();
+    			
+    			if (result.isInterFreqMeasuredResultsListSelected()) {
+    				//process InterFreq Results
+    				InterFreqMeasuredResultsList interFreqResultList = result.getInterFreqMeasuredResultsList();
+    				
+    				for (InterFreqMeasuredResults singleInterFreqResult : interFreqResultList.getValue()) {
+    					if (singleInterFreqResult.isInterFreqCellMeasuredResultsListPresent()) {
+    						for (CellMeasuredResults singleCellMeasuredResult : singleInterFreqResult.getInterFreqCellMeasuredResultsList().getValue()) {
+    							saveCellMeasuredResults(singleCellMeasuredResult, eventNode);
+    						}
+    					}
+    				}
+    			}
+    			else if (result.isInterRATMeasuredResultsListSelected()) {
+    				//process InterRAT Results
+    				InterRATMeasuredResultsList interRATResultList = result.getInterRATMeasuredResultsList();
+    				for (InterRATMeasuredResults singleInterRatResults : interRATResultList.getValue()) {
+    					if (singleInterRatResults.isGsmSelected()) {
+    						for (GSM_MeasuredResults singleGSMResults : singleInterRatResults.getGsm().getValue()) {
+    							if (singleGSMResults.getBsicReported().isVerifiedBSICSelected()) {
+    								eventNode.setProperty(GPEH_RRC_MR_BSIC_PREFIX + bsicIndex++, singleGSMResults.getBsicReported().getVerifiedBSIC());
+    							}
+    						}
+    					}
+    				}
+    			}
+    			else if (result.isIntraFreqMeasuredResultsListSelected()) {
+    				//process IntraFreq Results
+    				IntraFreqMeasuredResultsList intraFreqResultList = result.getIntraFreqMeasuredResultsList();
+    				for (CellMeasuredResults singleCellMeasuredResults : intraFreqResultList.getValue()) {
+    					saveCellMeasuredResults(singleCellMeasuredResults, eventNode);
+    				}
+    			}
+    			else if (result.isUe_InternalMeasuredResultsSelected()) {
+    				//process UE Internal Results
+    				UE_InternalMeasuredResults ueInternalResults = result.getUe_InternalMeasuredResults();
+    				if (ueInternalResults.getModeSpecificInfo().isFddSelected()) {
+    					eventNode.setProperty(GPEH_RRC_MR_UE_TX_POWER_PREFIX, ueInternalResults.getModeSpecificInfo().getFdd().getUe_TransmittedPowerFDD().getValue());
+    				}
+    			}
+    			
+    		}
+    	}
+    }
+    
+    /**
+     * Saves CellMeasuredResults to Node
+     * 
+     * @param results Cell Measured Results to save
+     * @param eventNode node of Event
+     */
+    private void saveCellMeasuredResults(CellMeasuredResults results, Node eventNode) {
+    	if (results.getModeSpecificInfo().isFddSelected()) {
+    		FddSequenceType fdd = results.getModeSpecificInfo().getFdd();
+    		
+    		if (fdd.isCpich_RSCPPresent()) {
+    			eventNode.setProperty(GPEH_RRC_MR_RSCP_PREFIX + rscpIndex++, fdd.getCpich_RSCP().getValue());    			
+    		}
+    		if (fdd.isCpich_Ec_N0Present()) {
+    			eventNode.setProperty(GPEH_RRC_MR_ECNO_PREFIX + ecnoIndex++, fdd.getCpich_Ec_N0().getValue());
+    		}
+    	}
     }
 
     /**
