8ee354b2c2b7455bc18843f024f073b15d35336c
zaelenchits <zaelenchits@0edadee9-8154-4c59-a661-b5f45207fcbd>
2010-05-12 19:19:08 +0000

    Feature #1262
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@3298 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.neo.loader/src/org/amanzi/neo/loader/GSMLoader.java
+++ org.amanzi.neo.loader/src/org/amanzi/neo/loader/GSMLoader.java
@@ -13,11 +13,12 @@
 
 package org.amanzi.neo.loader;
 
-import java.io.File;
-import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -25,8 +26,6 @@ import org.amanzi.neo.core.INeoConstants;
 import org.amanzi.neo.core.enums.DriveTypes;
 import org.amanzi.neo.core.enums.GeoNeoRelationshipTypes;
 import org.amanzi.neo.core.enums.NodeTypes;
-import org.amanzi.neo.core.utils.NeoUtils;
-import org.amanzi.neo.core.utils.Pair;
 import org.eclipse.swt.widgets.Display;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
@@ -38,17 +37,27 @@ import org.neo4j.graphdb.Transaction;
  * @since 1.0.0
  */
 public class GSMLoader extends DriveLoader {
+    private static boolean needParceHeader = true;
+    private final LinkedHashMap<String, Header> headers;
+    private String time = null;
     private Float currentLatitude = null;
     private Float currentLongitude = null;
-    private String time = null;
-    
+    private Node mNode = null;
     private final ArrayList<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
-    
-    private Node mNode;
-    private Node gisNode;
-    // private Node dataSetNode;
-    private Node fileNode;
-    private Node lastChild;
+
+    /**
+     * Constructor
+     * 
+     * @param directory
+     * @param datasetName
+     * @param display
+     */
+    public GSMLoader(String directory, String datasetName, Display display) {
+        initialize("GSM", null, directory, display);
+        basename = datasetName;
+        headers = getHeaderMap(1).headers;
+        needParceHeader = true;
+    }
 
     /**
      * Constructor for loading data in AWE, with specified display and dataset, but no NeoService
@@ -58,13 +67,12 @@ public class GSMLoader extends DriveLoader {
      * @param dataset to add data to
      */
     public GSMLoader(Calendar workTime, String filename, Display display, String dataset) {
-        _workDate = workTime;
         driveType = DriveTypes.GSM;
-        initialize("Romes", null, filename, display, dataset);
-        // initData();
-        initializeLuceneIndex();
+        initialize("GSM", null, filename, display, dataset);
+        basename = dataset;
+        headers = getHeaderMap(1).headers;
+        needParceHeader = true;
         initializeKnownHeaders();
-        addDriveIndexes();
     }
 
     /**
@@ -72,85 +80,65 @@ public class GSMLoader extends DriveLoader {
      * in the algorithms later.
      */
     private void initializeKnownHeaders() {
+        addKnownHeader(1, "timestamp", ".*timestamp.*");
         addKnownHeader(1, "latitude", ".*latitude.*");
         addKnownHeader(1, "longitude", ".*longitude.*");
-        addKnownHeader(1, "timestamp", ".*timestamp.*");
         dropHeaderStats(1, new String[] {"timestamp", "latitude", "longitude"});
-    }
-
-    private void addDriveIndexes() {
-        try {
-            addIndex(NodeTypes.M.getId(), NeoUtils.getTimeIndexProperty(dataset));
-            addIndex(NodeTypes.MP.getId(), NeoUtils.getLocationIndexProperty(dataset));
-        } catch (IOException e) {
-            throw (RuntimeException)new RuntimeException().initCause(e);
-        }
     }
 
     @Override
     protected Node getStoringNode(Integer key) {
-//        GisProperties gisProperties = gisNodes.get(dataset);
-//        return gisProperties == null ? null : gisProperties.getGis();
-        return gisNode;
+        GisProperties gisProperties = gisNodes.get(dataset);
+        return gisProperties == null ? null : gisProperties.getGis();
+    }
+
+    @Override
+    protected boolean needParceHeaders() {
+        if (needParceHeader) {
+            needParceHeader = false;
+            return true;
+        }
+        return false;
     }
 
     @Override
     protected void parseLine(String line) {
-        if (fileNode == null) {
-
-            Transaction tx = neo.beginTx();
-            try {
-                gisNode = NeoUtils.findRootNode(NodeTypes.GIS, "GSM", neo);
-                if (gisNode == null) {
-                    gisNode = neo.createNode();
-                    gisNode.setProperty(INeoConstants.PROPERTY_TYPE_NAME, NodeTypes.GIS.getId());
-                    gisNode.setProperty(INeoConstants.PROPERTY_NAME_NAME, "gis_name");
-                    neo.getReferenceNode().createRelationshipTo(gisNode, GeoNeoRelationshipTypes.CHILD);
-                }
-                tx.success();
-            } finally {
-                tx.finish();
+        try {
+            // debug(line);
+            List<String> fields = splitLine(line);
+            if (fields.size() < 2)
+                return;
+            if (this.isOverLimit())
+                return;
+            this.incValidMessage(); // we have not filtered the message out on non-accepted content
+            this.incValidChanged(); // we have not filtered the message out on lack of data change
+            Map<String, Object> lineData = makeDataMap(fields);
+            this.time = (String)lineData.get("timestamp");
+            // Date nodeDate = (Date)lineData.get("timestamp");
+            // this.timestamp = getTimeStamp(nodeDate);
+            Float latitude = (Float)lineData.get("latitude");
+            Float longitude = (Float)lineData.get("longitude");
+            if (time == null || latitude == null || longitude == null) {
+                return;
             }
-            gisNode.createRelationshipTo(datasetNode, GeoNeoRelationshipTypes.CHILD);
-
-            Pair<Boolean, Node> fileNodePair = NeoUtils.findOrCreateFileNode(neo, datasetNode, new File(basename).getName(), new File(basename).getName());
-            fileNode = fileNodePair.getRight();
-            lastChild = null;
-
+            if ((latitude != null)
+                    && (longitude != null)
+                    && (((currentLatitude == null) && (currentLongitude == null)) || ((Math.abs(currentLatitude - latitude) > 10E-10) || (Math.abs(currentLongitude
+                            - longitude) > 10E-10)))) {
+                currentLatitude = latitude;
+                currentLongitude = longitude;
+                saveData(); // persist the current data to database
+            }
+            this.incValidLocation(); // we have not filtered the message out on lack of location
+            if (lineData.size() > 0) {
+                data.add(lineData);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
         }
-        List<String> fields = splitLine(line);
-        if (fields.size() < 2)
-            return;
-        if (this.isOverLimit())
-            return;
-        Map<String, Object> lineData = makeDataMap(fields);
-
-        this.time = (String)lineData.get("timestamp");
-        // Date nodeDate = (Date)lineData.get("timestamp");
-        // this.timestamp = getTimeStamp(nodeDate);
-        Float latitude = (Float)lineData.get("latitude");
-        Float longitude = (Float)lineData.get("longitude");
-        if (time == null || latitude == null || longitude == null) {
-            new Exception("Wrong time, latitude or longitude.").printStackTrace();
-            return;
-        }
-        if ((latitude != null)
-                && (longitude != null)
-                && (((currentLatitude == null) && (currentLongitude == null)) || ((Math.abs(currentLatitude - latitude) > 10E-10) || (Math.abs(currentLongitude
-                        - longitude) > 10E-10)))) {
-            currentLatitude = latitude;
-            currentLongitude = longitude;
-            saveData(); // persist the current data to database
-        }
-        this.incValidLocation(); // we have not filtered the message out on lack of location
-        if (lineData.size() > 0) {
-            data.add(lineData);
-        }
-        
-        
-        saveStatistic(lineData);
     }
-    
+
     /**
      * This method is called to dump the current cache of signals as one located point linked to a
      * number of signal strength measurements.
@@ -171,17 +159,26 @@ public class GSMLoader extends DriveLoader {
                 boolean haveEvents = false;
                 for (Map<String, Object> dataLine : data) {
                     Node m = neo.createNode();
-                    file.createRelationshipTo(m, GeoNeoRelationshipTypes.CHILD);
+                    findOrCreateFileNode(m);
                     m.setProperty(INeoConstants.PROPERTY_TYPE_NAME, NodeTypes.M.getId());
                     for (Map.Entry<String, Object> entry : dataLine.entrySet()) {
                         if (entry.getKey().equals(INeoConstants.SECTOR_ID_PROPERTIES)) {
                             mp.setProperty(INeoConstants.SECTOR_ID_PROPERTIES, entry.getValue());
                             // ms.setProperty(INeoConstants.SECTOR_ID_PROPERTIES, entry.getValue());
                         } else if ("timestamp".equals(entry.getKey())) {
-                            long timeStamp = getTimeStamp(1, ((Date)entry.getValue()));
-                            if (timeStamp != 0) {
-                                m.setProperty(entry.getKey(), timeStamp);
-                                mp.setProperty(entry.getKey(), timeStamp);
+
+                            try {
+                                String pattern = "dd.MM.yyyy hh:mm:ss";
+                                SimpleDateFormat sf = new SimpleDateFormat(pattern);
+                                Date date = sf.parse(entry.getValue().toString());
+                                long timeStamp = date.getTime();
+                                updateTimestampMinMax(1, timeStamp);
+                                if (timeStamp != 0) {
+                                    m.setProperty(entry.getKey(), timeStamp);
+                                    mp.setProperty(entry.getKey(), timeStamp);
+                                }
+                            } catch (ParseException e) {
+                                e.printStackTrace();
                             }
                         } else {
                             m.setProperty(entry.getKey(), entry.getValue());
@@ -212,33 +209,14 @@ public class GSMLoader extends DriveLoader {
     }
 
     /**
-     * @param lineData
-     */
-    private void saveStatistic(Map<String, Object> lineData) {
-        Transaction transaction = neo.beginTx();
-        try {
-            Node node = neo.createNode();
-            NodeTypes.M.setNodeType(node, neo);
-            for (Map.Entry<String, Object> entry : lineData.entrySet()) {
-                node.setProperty(entry.getKey(), entry.getValue());
-            }
-            node.setProperty(INeoConstants.PROPERTY_NAME_NAME, "GSM counter");
-            index(node);
-            NeoUtils.addChild(fileNode, node, lastChild, neo);
-            lastChild = node;
-        } finally {
-            transaction.finish();
-        }
-    }
-    /**
      * get name of m node
      * 
      * @param dataLine - node data
      * @return node name
      */
     private String getMNodeName(Map<String, Object> dataLine) {
+        // TODO check name of node
         Object timeNode = dataLine.get("time");
         return timeNode == null ? "ms node" : timeNode.toString();
     }
-
 }
