113caf62a2aae0e5c95ebdf0ad646c49dd78e8d6
zaelenchits <zaelenchits@0edadee9-8154-4c59-a661-b5f45207fcbd>
2010-06-14 07:34:25 +0000

    Feature #1078 (code cleanup)
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@3575 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.awe.views.drive/src/org/amanzi/awe/views/drive/views/DriveInquirerPropertyConfig.java
+++ org.amanzi.awe.views.drive/src/org/amanzi/awe/views/drive/views/DriveInquirerPropertyConfig.java
@@ -13,9 +13,7 @@
 
 package org.amanzi.awe.views.drive.views;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -28,9 +26,7 @@ import org.amanzi.neo.core.utils.AbstractDialog;
 import org.amanzi.neo.core.utils.NeoUtils;
 import org.amanzi.neo.core.utils.PropertyHeader;
 import org.apache.log4j.Logger;
-import org.eclipse.jface.viewers.CheckStateChangedEvent;
 import org.eclipse.jface.viewers.CheckboxTableViewer;
-import org.eclipse.jface.viewers.ICheckStateListener;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.LabelProvider;
@@ -69,7 +65,7 @@ import org.neo4j.graphdb.Traverser.Order;
  */
 public class DriveInquirerPropertyConfig extends AbstractDialog<Integer> {
     private static final Logger LOGGER = Logger.getLogger(DriveInquirerPropertyConfig.class);
-    // TODO add title icon
+
     private final Node dataset;
     private Shell shell;
     private CheckboxTableViewer propertyListTable;
@@ -81,12 +77,14 @@ public class DriveInquirerPropertyConfig extends AbstractDialog<Integer> {
     private Button bCancel;
     private Button bClear;
 
-    private List<String> propertyList = new ArrayList<String>();
+    private final Set<String> propertyList = new TreeSet<String>();
     private final Set<String> propertySlip = new TreeSet<String>();
 
     private final GraphDatabaseService service;
 
     /**
+     * Constructor
+     * 
      * @param parent
      * @param title
      */
@@ -101,19 +99,10 @@ public class DriveInquirerPropertyConfig extends AbstractDialog<Integer> {
     protected void createContents(final Shell shell) {
         this.shell = shell;
         shell.setImage(NodeTypes.DATASET.getImage());
-        // shell.setImage(IconManager.getIconManager().
         shell.setLayout(new GridLayout(2, false));
-        // Label label = new Label(shell, SWT.NONE);
-        // label.setText("Network:");
-        // cNetwork = new Combo(shell, SWT.DROP_DOWN | SWT.READ_ONLY);
-        // GridData layoutData = new GridData();
-        // layoutData.grabExcessHorizontalSpace = true;
-        // layoutData.minimumWidth = 200;
-        // cNetwork.setLayoutData(layoutData);
+
         propertyListTable = CheckboxTableViewer.newCheckList(shell, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER | SWT.CHECK);
         GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
-        // data.horizontalSpan = 2;
-        // data.verticalSpan = 1;
         data.heightHint = 300;
         data.widthHint = 200;
         createTable(propertyListTable, "Avaliable properties");
@@ -121,26 +110,14 @@ public class DriveInquirerPropertyConfig extends AbstractDialog<Integer> {
         propertyListTable.setContentProvider(new PropertyListContentProvider());
         propertyListTable.setLabelProvider(new PropertyListLabelProvider());
 
-        // GridData greedButtonData = new GridData();
-        // greedButtonData.horizontalAlignment = GridData.END;
-        // greedButtonData.widthHint = 80;
-
-        // propertySlipTable = CheckboxTableViewer.newCheckList(shell, SWT.MULTI | SWT.H_SCROLL |
-        // SWT.V_SCROLL | SWT.BORDER);
-
-
-
         Table table = new Table(shell, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER);
         propertySlipTable = new CheckboxTableViewer(table);
         data = new GridData(SWT.NO, SWT.FILL, true, true);
-        // data.horizontalSpan = 2;
-        // data.verticalSpan = 2;
         data.heightHint = 300;
         data.widthHint = 200;
         createTable(propertySlipTable, "Active properties");
         propertySlipTable.getControl().setLayoutData(data);
         propertySlipTable.setContentProvider(new PropertySlipContentProvider());
-
         propertySlipTable.setLabelProvider(new PropertyListLabelProvider());
 
         Group gr1 = new Group(shell, SWT.NULL);
@@ -153,8 +130,8 @@ public class DriveInquirerPropertyConfig extends AbstractDialog<Integer> {
         gr2.setLayout(new GridLayout(2, false));
         gr2.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
         new Label(shell, SWT.NULL);
+
         Composite gr3 = new Composite(shell, SWT.NULL);
-        // gr3.setText("Saving options");
         gr3.setLayout(new GridLayout(2, false));
         gr3.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 
@@ -175,7 +152,11 @@ public class DriveInquirerPropertyConfig extends AbstractDialog<Integer> {
     }
 
     /**
-     * @param gr1
+     * Create button
+     * 
+     * @param parent parent composite
+     * @param name visible name
+     * @return Button
      */
     private Button createButton(Composite parent, String name) {
         Button button = new Button(parent, SWT.PUSH);
@@ -185,7 +166,7 @@ public class DriveInquirerPropertyConfig extends AbstractDialog<Integer> {
     }
 
     /**
-     *
+     * Action perform deleting of selected properties
      */
     protected void deleteProperty() {
         IStructuredSelection selection = (IStructuredSelection)propertySlipTable.getSelection();
@@ -194,7 +175,7 @@ public class DriveInquirerPropertyConfig extends AbstractDialog<Integer> {
     }
 
     /**
-     *
+     * Action perform adding all selected properties to active list
      */
     protected void addSingle() {
         Object[] checkedElements = propertyListTable.getCheckedElements();
@@ -207,7 +188,7 @@ public class DriveInquirerPropertyConfig extends AbstractDialog<Integer> {
     }
 
     /**
-     *
+     * Action perform adding all selected properties as single composite property
      */
     protected void addComposite() {
         Object[] checkedElements = propertyListTable.getCheckedElements();
@@ -223,7 +204,7 @@ public class DriveInquirerPropertyConfig extends AbstractDialog<Integer> {
     }
 
     /**
-     *
+     * Loading saved selected properties from PROPERTY node
      */
     private void loadSavedData() {
         Object[] savedProperties = null;
@@ -254,7 +235,7 @@ public class DriveInquirerPropertyConfig extends AbstractDialog<Integer> {
     }
 
     /**
-     *
+     * Action perform saving
      */
     protected void perfomSave() {
         Transaction tx = NeoUtils.beginTransaction();
@@ -279,81 +260,61 @@ public class DriveInquirerPropertyConfig extends AbstractDialog<Integer> {
         }
     }
 
+    /**
+     * Init startup data
+     */
     private void init() {
         propertyList.clear();
-        propertyList = Arrays.asList(new PropertyHeader(dataset).getNumericFields());
-
-        // Collections.
+        propertyList.addAll(Arrays.asList(new PropertyHeader(dataset).getNumericFields()));
     }
 
+    /**
+     * Add listeners to UI components
+     */
     private void addListeners() {
-        propertyListTable.addCheckStateListener(new ICheckStateListener() {
-
-            @Override
-            public void checkStateChanged(CheckStateChangedEvent event) {
-                // updatePropertySlip();
-            }
-        });
-
         bAddComposite.addSelectionListener(new SelectionAdapter() {
-
             @Override
             public void widgetSelected(SelectionEvent e) {
                 addComposite();
             }
-
         });
-
         bAddSingle.addSelectionListener(new SelectionAdapter() {
-
             @Override
             public void widgetSelected(SelectionEvent e) {
                 addSingle();
             }
-
         });
-
         bDel.addSelectionListener(new SelectionAdapter() {
-
             @Override
             public void widgetSelected(SelectionEvent e) {
                 deleteProperty();
             }
-
         });
-
         bClear.addSelectionListener(new SelectionAdapter() {
-
             @Override
             public void widgetSelected(SelectionEvent e) {
                 clearList();
             }
-
         });
-
         bOk.addSelectionListener(new SelectionAdapter() {
-
             @Override
             public void widgetSelected(SelectionEvent e) {
                 status = SWT.OK;
                 perfomSave();
                 shell.close();
             }
-
         });
         bCancel.addSelectionListener(new SelectionAdapter() {
-
             @Override
             public void widgetSelected(SelectionEvent e) {
                 status = SWT.CANCEL;
                 shell.close();
             }
-
         });
     }
 
     /**
-     *
+     * Action perform cleaning list of selected properties
      */
     protected void clearList() {
         propertySlip.clear();
@@ -361,7 +322,10 @@ public class DriveInquirerPropertyConfig extends AbstractDialog<Integer> {
     }
 
     /**
-     * @param tableView2
+     * Create table
+     * 
+     * @param tableView table
+     * @param clumnName name of column
      */
     private void createTable(TableViewer tableView, String clumnName) {
         Table table = tableView.getTable();
@@ -372,6 +336,14 @@ public class DriveInquirerPropertyConfig extends AbstractDialog<Integer> {
         table.setLinesVisible(true);
     }
 
+    /**
+     * <p>
+     * PropertyListContentProvider
+     * </p>
+     * 
+     * @author Saelenchits_N
+     * @since 1.0.0
+     */
     private class PropertyListContentProvider implements IStructuredContentProvider {
 
         @Override
@@ -389,6 +361,14 @@ public class DriveInquirerPropertyConfig extends AbstractDialog<Integer> {
 
     }
 
+    /**
+     * <p>
+     * PropertySlipContentProvider
+     * </p>
+     * 
+     * @author Saelenchits_N
+     * @since 1.0.0
+     */
     private class PropertySlipContentProvider implements IStructuredContentProvider {
 
         @Override
@@ -406,6 +386,14 @@ public class DriveInquirerPropertyConfig extends AbstractDialog<Integer> {
 
     }
 
+    /**
+     * <p>
+     * PropertyListLabelProvider
+     * </p>
+     * 
+     * @author Saelenchits_N
+     * @since 1.0.0
+     */
     public class PropertyListLabelProvider extends LabelProvider {
         @Override
         public Image getImage(Object element) {
