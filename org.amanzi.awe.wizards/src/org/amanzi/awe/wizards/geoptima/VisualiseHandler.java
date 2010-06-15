51e46e8aafe41abb023f35ca66483ab9c64fadaf
artemij <artemij@0edadee9-8154-4c59-a661-b5f45207fcbd>
2010-06-15 12:01:34 +0000

    Geoptima gui logic
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@3592 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.awe.wizards/src/org/amanzi/awe/wizards/geoptima/VisualiseHandler.java
+++ org.amanzi.awe.wizards/src/org/amanzi/awe/wizards/geoptima/VisualiseHandler.java
@@ -13,16 +13,33 @@
 
 package org.amanzi.awe.wizards.geoptima;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.refractions.udig.catalog.CatalogPlugin;
+import net.refractions.udig.catalog.IGeoResource;
+import net.refractions.udig.catalog.IService;
+import net.refractions.udig.project.ILayer;
+import net.refractions.udig.project.ui.ApplicationGIS;
+import net.refractions.udig.project.ui.internal.actions.ZoomToLayer;
+
+import org.amanzi.neo.core.enums.GeoNeoRelationshipTypes;
+import org.amanzi.neo.core.service.NeoServiceProvider;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
 
 /**
- * TODO Purpose of 
  * <p>
- *
+ *Visualise handler
  * </p>
  * @author TsAr
  * @since 1.0.0
@@ -31,9 +48,54 @@ public class VisualiseHandler extends AbstractHandler {
 
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
-        IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
-        new ProcessDialog(window.getShell(), "Visualise", "Add on Map").open();
+        String id = event.getParameter("nodeId");
+        if (id==null){
+            return null;
+        }
+        GraphDatabaseService service = NeoServiceProvider.getProvider().getService();
+        Transaction tx = service.beginTx();
+        
+        try{
+            Node node=service.getNodeById(Long.parseLong(id));
+            Node gis = node.getSingleRelationship(GeoNeoRelationshipTypes.NEXT, Direction.INCOMING).getOtherNode(node);
+            ILayer layer = launchLayer(gis);
+            ZoomToLayer zoomCommand = new ZoomToLayer();
+            zoomCommand.selectionChanged(null, new StructuredSelection(layer));
+            zoomCommand.runWithEvent(null, null);
+        }finally{
+            tx.finish();
+        }
         return null;
     }
+    //TODO move to utility class
+    private ILayer launchLayer(Node gisNode) {
+        try {
+            String databaseLocation = NeoServiceProvider.getProvider().getDefaultDatabaseLocation();
+            URL url = new URL("file://" + databaseLocation);
+            List<ILayer> layers = ApplicationGIS.getActiveMap().getMapLayers();
+            for (ILayer iLayer : layers) {
+                if (iLayer.getGeoResource().canResolve(Node.class)) {
+                    if (iLayer.getGeoResource().resolve(Node.class, new NullProgressMonitor()).equals(gisNode)) {
+                        return iLayer;
+                    }
+                }
+            }
+            IService curService = CatalogPlugin.getDefault().getLocalCatalog().getById(IService.class, url, null);
+            List<IGeoResource> listGeoRes = new ArrayList<IGeoResource>();
+            for (IGeoResource iGeoResource : curService.resources(null)) {
+                if (iGeoResource.canResolve(Node.class)) {
+                    if (iGeoResource.resolve(Node.class, null).equals(gisNode)) {
+                        listGeoRes.add(iGeoResource);
+                        break;
+                    }
+                }
+            };
+            List< ? extends ILayer> result = ApplicationGIS.addLayersToMap(ApplicationGIS.getActiveMap(), listGeoRes, 0);
+            return result.iterator().next();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
 
+    }
 }
--- org.amanzi.awe.wizards/src/org/amanzi/awe/wizards/geoptima/VisualiseLayer.java
+++ org.amanzi.awe.wizards/src/org/amanzi/awe/wizards/geoptima/VisualiseLayer.java
@@ -54,37 +54,59 @@ import org.opengis.referencing.operation.MathTransform;
 
 import com.vividsolutions.jts.geom.Coordinate;
 
+// TODO: Auto-generated Javadoc
 /**
- * TODO Purpose of
  * <p>
  * </p>
+ * .
  * 
  * @author tsinkel_a
  * @since 1.0.0
  */
 public class VisualiseLayer implements MapGraphic, MapInterceptor {
+
     /**
-     * 
+     * Instantiates a new visualise layer.
      */
     public VisualiseLayer() {
         super();
 
     }
+
+    // TODO synchronyze!
+    public static ILayer datasetLayer = null;
+    public static VisualiseParam datasetParam=null;
+
+    /** The Constant LOGGER. */
     public static final Logger LOGGER = Logger.getLogger(VisualiseLayer.class);
-    public static final Long TIME_STEP = 10000l;
+
     // TODO check synchronize
     // TODO add listener on remove map!
+    /** The Constant animation. */
     private static final Set<VisualiseCommand> animation = new HashSet<VisualiseCommand>();
+
+    /** The Constant DATASET_LAYER. */
+    public static final String DATASET_LAYER = "DATASET_LAYER";
+    public static final String DATASET_PARAM = "DATASET_PARAM";
+
+    /** The service. */
     private final GraphDatabaseService service = NeoServiceProvider.getProvider().getService();
 
+    /**
+     * Draw.
+     * 
+     * @param context the context
+     */
     @Override
     public void draw(MapGraphicContext context) {
         ILayer datasetLayer = findDatasetLayer(context);
+        
         if (datasetLayer == null) {
             return;
         }
 
         ILayer layer = context.getLayer();
+        updateParam(layer);
         VisualiseCommand command;
         try {
             command = new VisualiseCommand(layer, datasetLayer, context);
@@ -101,46 +123,112 @@ public class VisualiseLayer implements MapGraphic, MapInterceptor {
     }
 
     /**
-     * @param context
-     * @return
+     *
+     * @param layer
+     */
+    private void updateParam(ILayer layer) {
+        Object param = layer.getBlackboard().get(DATASET_PARAM);
+        if (param!=null){
+            return;
+        }
+        layer.getBlackboard().put(DATASET_PARAM, datasetParam);
+        datasetParam=null;
+        return;
+    }
+
+    /**
+     * Find dataset layer.
+     * 
+     * @param context the context
+     * @return the i layer
      */
     private ILayer findDatasetLayer(MapGraphicContext context) {
+        ILayer datasetLayer = (ILayer)context.getLayer().getBlackboard().get(DATASET_LAYER);
+        if (datasetLayer != null) {
+            return datasetLayer;
+        }
+        if (this.datasetLayer != null) {
+            datasetLayer = this.datasetLayer;
+            context.getLayer().getBlackboard().put(DATASET_LAYER, this.datasetLayer);
+            this.datasetLayer = null;
+            return datasetLayer;
+        }
         Transaction tx = service.beginTx();
         ILayer result = null;
         try {
             for (ILayer layer : context.getMapLayers()) {
                 if (layer.getGeoResource().canResolve(Node.class)) {
-                    Node node = layer.getGeoResource().resolve(Node.class, null);
-                    node = NeoUtils.findRoot(node, service);
+                    Node nodeGis = layer.getGeoResource().resolve(Node.class, null);
+                    Node node = NeoUtils.findRoot(nodeGis, service);
                     if (NeoUtils.isDatasetNode(node) || NodeTypes.OSS.checkNode(node)) {
+                        if (datasetParam==null){
+                            Pair<Long, Long> minMax = NeoUtils.getMinMaxTimeOfDataset(node, service);
+                            datasetParam=new VisualiseParam(minMax.getLeft(), minMax.getRight(), true,1000);
+                        }
                         result = layer;
                         break;
                     }
                 }
             }
+
+            context.getLayer().getBlackboard().put(DATASET_LAYER, result);
             return result;
         } catch (IOException e) {
             e.printStackTrace();
             return null;
-        }finally{
+        } finally {
             tx.finish();
         }
     }
 
+    /**
+     * The Class VisualiseCommand.
+     */
     private class VisualiseCommand extends AbstractDrawCommand implements IAnimation {
-        private final ILayer datasetLayer;
-        private final ILayer layer;
-        private final MapGraphicContext context;
-        private boolean cansel;
-        private IProgressMonitor monitor = new NullProgressMonitor();
-        private Node rootNode;
-        private Pair<Long, Long> minMax;
-        private MultiPropertyIndex<Long> timestampIndex;
-        private Long currentTime;
-        private MathTransform transform_d2w;
-        private MathTransform transform_w2d;
-        private CoordinateReferenceSystem cRSDataset;
 
+        /** The dataset layer. */
+        private final ILayer datasetLayer;
+
+        /** The layer. */
+        private final ILayer layer;
+
+        /** The context. */
+        private final MapGraphicContext context;
+
+        /** The cansel. */
+        private boolean cansel;
+
+        /** The monitor. */
+        private IProgressMonitor monitor = new NullProgressMonitor();
+
+        /** The root node. */
+        private Node rootNode;
+
+
+        /** The timestamp index. */
+        private MultiPropertyIndex<Long> timestampIndex;
+
+        /** The current time. */
+        private Long currentTime;
+
+        /** The transform_d2w. */
+        private MathTransform transform_d2w;
+
+        /** The transform_w2d. */
+        private MathTransform transform_w2d;
+
+        /** The c rs dataset. */
+        private CoordinateReferenceSystem cRSDataset;
+        private VisualiseParam  param;
+
+        /**
+         * Instantiates a new visualise command.
+         * 
+         * @param layer the layer
+         * @param datasetLayer the dataset layer
+         * @param context the context
+         * @throws IllegalArgumentException the illegal argument exception
+         */
         public VisualiseCommand(ILayer layer, ILayer datasetLayer, MapGraphicContext context) throws IllegalArgumentException {
             this.layer = layer;
             this.datasetLayer = datasetLayer;
@@ -150,20 +238,20 @@ public class VisualiseLayer implements MapGraphic, MapInterceptor {
 
         }
 
+        /**
+         * Inits the.
+         */
         private void init() {
             Transaction tx = service.beginTx();
             try {
                 Node node = datasetLayer.getGeoResource().resolve(Node.class, null);
                 cRSDataset = datasetLayer.getGeoResource().getInfo(new NullProgressMonitor()).getCRS();
                 rootNode = NeoUtils.findRoot(node, service);
-                minMax = NeoUtils.getMinMaxTimeOfDataset(rootNode, service);
-                if (minMax == null || minMax.getLeft() == null || minMax.getRight() == null) {
-                    throw new IllegalArgumentException("incorrect init parameters: minmax time");
-                }
                 String datasetName = NeoUtils.getNodeName(node, service);
                 timestampIndex = NeoUtils.getTimeIndexProperty(datasetName);
                 timestampIndex.initialize(NeoServiceProvider.getProvider().getService(), null);
-                currentTime = minMax.getRight();
+                param=(VisualiseParam)layer.getBlackboard().get(DATASET_PARAM);
+                currentTime = param.getBeginTime();
             } catch (Exception e) {
                 throw new IllegalArgumentException("incorrect init parameters", e);
             } finally {
@@ -171,24 +259,50 @@ public class VisualiseLayer implements MapGraphic, MapInterceptor {
             }
         }
 
+        /**
+         * Gets the frame interval.
+         * 
+         * @return the frame interval
+         */
         public short getFrameInterval() {
             return 100;
         }
 
+        /**
+         * Checks for next.
+         * 
+         * @return true, if successful
+         */
         public boolean hasNext() {
             List<ILayer> layers = context.getMapLayers();
             boolean result = layers.contains(layer) && layers.contains(datasetLayer);
+            if (result){
+                param=(VisualiseParam)layer.getBlackboard().get(DATASET_PARAM);
+                result=param!=null&&(param.isRepeat()||param.getEndTime()>=currentTime);
+            }
+            if(!result){
+                layer.getMap().sendCommandASync(new DeleteLayerCommand((Layer)layer));
+            }
             return result;
         }
 
+        /**
+         * Next frame.
+         */
         public void nextFrame() {
-            currentTime += TIME_STEP;
-            if (currentTime > minMax.getRight()) {
-                currentTime = minMax.getLeft();
+            currentTime += param.timeWindow;
+            if (currentTime > param.getEndTime()) {
+                currentTime = param.getBeginTime();
             }
 
         }
 
+        /**
+         * Run.
+         * 
+         * @param monitor the monitor
+         * @throws Exception the exception
+         */
         public void run(IProgressMonitor monitor) throws Exception {
             // long time = System.currentTimeMillis();
             // try {
@@ -200,7 +314,7 @@ public class VisualiseLayer implements MapGraphic, MapInterceptor {
                 try {
                     Node location = findLocation(currentTime);
                     if (location == null) {
-                        LOGGER.warn(String.format("not found location on time [%s;%s)", currentTime, currentTime + TIME_STEP));
+                        LOGGER.warn(String.format("not found location on time [%s;%s)", currentTime, currentTime + param.timeWindow));
                         return;
                     }
                     Pair<MathTransform, MathTransform> driveTransform = setCrsTransforms(cRSDataset);// TODO
@@ -228,11 +342,13 @@ public class VisualiseLayer implements MapGraphic, MapInterceptor {
         }
 
         /**
-         * @param currentTime2
-         * @return
+         * Find location.
+         * 
+         * @param currentTime the current time
+         * @return the node
          */
         private Node findLocation(Long currentTime) {
-            Traverser traverse = timestampIndex.searchTraverser(new Long[] {currentTime}, new Long[] {currentTime + TIME_STEP});
+            Traverser traverse = timestampIndex.searchTraverser(new Long[] {currentTime}, new Long[] {currentTime + param.timeWindow});
             Node location = null;
             for (Node node : traverse) {
                 if (node.hasRelationship(GeoNeoRelationshipTypes.LOCATION, Direction.OUTGOING)) {
@@ -243,6 +359,11 @@ public class VisualiseLayer implements MapGraphic, MapInterceptor {
             return location;
         }
 
+        /**
+         * Gets the valid area.
+         * 
+         * @return the valid area
+         */
         public Rectangle getValidArea() {
             // to be more efficient we could set the area to draw so only the
             // area affected by this animation would be updated in the map
@@ -253,6 +374,9 @@ public class VisualiseLayer implements MapGraphic, MapInterceptor {
             return null;
         }
 
+        /**
+         * Dispose.
+         */
         @Override
         public void dispose() {
             if (context.getMapLayers().contains(layer)) {
@@ -265,6 +389,11 @@ public class VisualiseLayer implements MapGraphic, MapInterceptor {
             // AnimationUpdater.runTimer(context.getMapDisplay(), this);
         }
 
+        /**
+         * Hash code.
+         * 
+         * @return the int
+         */
         @Override
         public int hashCode() {
             final int prime = 31;
@@ -275,6 +404,12 @@ public class VisualiseLayer implements MapGraphic, MapInterceptor {
             return result;
         }
 
+        /**
+         * Equals.
+         * 
+         * @param obj the obj
+         * @return true, if successful
+         */
         @Override
         public boolean equals(Object obj) {
             if (this == obj)
@@ -299,13 +434,25 @@ public class VisualiseLayer implements MapGraphic, MapInterceptor {
             return true;
         }
 
+        /**
+         * Gets the outer type.
+         * 
+         * @return the outer type
+         */
         private VisualiseLayer getOuterType() {
             return VisualiseLayer.this;
         }
 
+        /**
+         * Sets the crs transforms.
+         * 
+         * @param dataCrs the data crs
+         * @return the pair
+         * @throws FactoryException the factory exception
+         */
         private Pair<MathTransform, MathTransform> setCrsTransforms(CoordinateReferenceSystem dataCrs) throws FactoryException {
             boolean lenient = true; // needs to be lenient to work on uDIG 1.1 (otherwise we get
-                                    // error:
+            // error:
             // bursa wolf parameters required
             CoordinateReferenceSystem worldCrs = context.getCRS();
             Pair<MathTransform, MathTransform> oldTransform = new Pair<MathTransform, MathTransform>(transform_d2w, transform_w2d);
@@ -316,15 +463,131 @@ public class VisualiseLayer implements MapGraphic, MapInterceptor {
     }
 
     // mapclose
+    /**
+     * Run.
+     * 
+     * @param map the map
+     */
     @Override
     public void run(Map map) {
         Iterator<VisualiseCommand> it = animation.iterator();
         while (it.hasNext()) {
             VisualiseCommand comand = it.next();
             if (comand.layer.getMap() == null || comand.layer.getMap().equals(map)) {
+                map.sendCommandASync(new DeleteLayerCommand((Layer)comand.layer));
                 it.remove();
             }
         }
     }
 
+    /**
+     * <p>
+     * Container for mapgraphic properties
+     * </p>
+     * 
+     * @author tsinkel_a
+     * @since 1.0.0
+     */
+    public static class VisualiseParam {
+
+        /** The begin time. */
+        long beginTime;
+
+        /** The end time. */
+        long endTime;
+        
+        /** The time window. */
+        long timeWindow;
+
+        /** The is repeat. */
+        boolean isRepeat;
+
+        /**
+         * Gets the begin time.
+         * 
+         * @return the begin time
+         */
+        public long getBeginTime() {
+            return beginTime;
+        }
+
+        /**
+         * Instantiates a new visualise param.
+         */
+        public VisualiseParam() {
+            super();
+        }
+
+        public VisualiseParam(long beginTime, long endTime, boolean isRepeat,long timewindow) {
+            this();
+            this.beginTime = beginTime;
+            this.endTime = endTime;
+            this.isRepeat = isRepeat;
+            timeWindow = timewindow;
+        }
+
+        /**
+         * Sets the begin time.
+         * 
+         * @param beginTime the new begin time
+         */
+        public void setBeginTime(long beginTime) {
+            this.beginTime = beginTime;
+        }
+
+        /**
+         * Gets the end time.
+         * 
+         * @return the end time
+         */
+        public long getEndTime() {
+            return endTime;
+        }
+
+        /**
+         * Sets the end time.
+         * 
+         * @param endTime the new end time
+         */
+        public void setEndTime(long endTime) {
+            this.endTime = endTime;
+        }
+
+        /**
+         * Checks if is repeat.
+         * 
+         * @return true, if is repeat
+         */
+        public boolean isRepeat() {
+            return isRepeat;
+        }
+
+        /**
+         * Sets the repeat.
+         * 
+         * @param isRepeat the new repeat
+         */
+        public void setRepeat(boolean isRepeat) {
+            this.isRepeat = isRepeat;
+        }
+
+        /**
+         * Gets the time window.
+         *
+         * @return the time window
+         */
+        public long getTimeWindow() {
+            return timeWindow;
+        }
+
+        /**
+         * Sets the time window.
+         *
+         * @param timeWindow the new time window
+         */
+        public void setTimeWindow(long timeWindow) {
+            this.timeWindow = timeWindow;
+        }
+
+    }
 }
