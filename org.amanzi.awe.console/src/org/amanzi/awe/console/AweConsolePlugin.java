c5f88d6873d06f197bef885ad2af719f9af8327a
artemij <artemij@0edadee9-8154-4c59-a661-b5f45207fcbd>
2010-06-18 14:08:36 +0000

    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@3640 0edadee9-8154-4c59-a661-b5f45207fcbd

---
+++ org.amanzi.awe.console/src/org/amanzi/awe/console/AweConsolePlugin.java
@@ -0,0 +1,227 @@
+package org.amanzi.awe.console;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+import org.apache.log4j.Logger;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class AweConsolePlugin extends AbstractUIPlugin {
+    private static final Logger LOGGER = Logger.getLogger(AweConsolePlugin.class);
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.amanzi.awe.console";
+    /** String DEFAULT_CHARSET field */
+    public static final String DEFAULT_CHARSET = "UTF-8";
+    /*
+     * Name of console
+     */
+    
+    private static String CONSOLE_NAME = "AWE Console";
+    
+    
+    /*
+     * Is logging possible
+     */
+    
+    private static boolean loggingPossible = false;
+    
+    /*
+     * Is console visible
+     */
+    
+    private static boolean isVisible = false;
+    
+    /*
+     * Console for NeoLoaderPlugin
+     */
+    
+    private MessageConsole pluginConsole;
+    
+    /*
+     * Console's output
+     */
+    
+    private MessageConsoleStream consoleStream;
+
+    
+    /*
+     * Logging properties
+     */
+    public static boolean debug = false;
+    private static boolean verbose = true;  
+    
+
+	// The shared instance
+	private static AweConsolePlugin plugin;
+	
+
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+    public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		initializeConsole();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+    public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		removeConsole();
+		isVisible = false;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static AweConsolePlugin getDefault() {
+		return plugin;
+	}
+    /**
+     * Initialize console for output from NeoLoaderPlugin
+     */
+    
+    private void initializeConsole() {
+        pluginConsole = new MessageConsole(CONSOLE_NAME, null, true);
+        pluginConsole.initialize();
+        
+        consoleStream = pluginConsole.newMessageStream();       
+        
+        ConsolePlugin.getDefault().getConsoleManager().addConsoles(new IConsole[] {pluginConsole});
+        
+        loggingPossible = (plugin != null) && (pluginConsole != null);
+    }
+    
+    /**
+     * Destroys console for output
+     */
+    
+    private void removeConsole() {
+        ConsolePlugin.getDefault().getConsoleManager().removeConsoles(new IConsole[] {pluginConsole});
+        
+        try {
+            consoleStream.close();
+        }
+        catch (IOException e) {
+            LOGGER.error(e.getLocalizedMessage(), e);
+        }
+        
+        pluginConsole.destroy();
+    }
+    
+    /**
+     * Print debug message
+     * 
+     * @param line
+     */
+    
+    public static void debug(String line) {
+        if (loggingPossible) {
+            if (debug) {
+                getDefault().printToStream(line);
+            }
+        } else if(debug) {
+            LOGGER.debug(line);
+        }
+    }
+    
+    /**
+     * Print info message
+     * 
+     * @param line
+     */
+    
+    public static void info(String line) {
+        if (loggingPossible) {
+            if (verbose || debug) {
+                getDefault().printToStream(line);
+            }
+        } else {
+            LOGGER.debug(line);
+        }
+    }
+    
+    /**
+     * Print a notification message
+     * 
+     * @param line
+     */
+    
+    public static void notify(String line) {
+        if (loggingPossible) {
+            getDefault().printToStream(line);
+        } else {
+            LOGGER.debug(line);
+        }
+    }
+    
+    /**
+     * Print an error message
+     * 
+     * @param line
+     */
+    
+    public static void error(String line) {
+        if (loggingPossible) {
+            getDefault().printToStream(line);
+        } else {
+            System.err.println(line);
+        }
+    }
+    
+    /**
+     * Print an exception
+     * 
+     * @param line
+     */
+    
+    public static void exception(Exception e) {
+        if (loggingPossible) {
+            getDefault().printException(e);
+        } else {
+            e.printStackTrace(System.out);
+        }
+    }
+    
+    /** Print a message to Console */
+    private void printToStream(final String line) {
+        if (!isVisible) {           
+            pluginConsole.activate();           
+            ConsolePlugin.getDefault().getConsoleManager().showConsoleView(pluginConsole);
+            isVisible = true;
+        }       
+        consoleStream.println(line);
+    }
+    
+    /** Print a exception to Console */ 
+    private void printException(Exception e) {
+        if (!isVisible) {           
+            pluginConsole.activate();           
+            ConsolePlugin.getDefault().getConsoleManager().showConsoleView(pluginConsole);
+            isVisible = true;
+        }
+                
+        PrintStream stream = new PrintStream(consoleStream);        
+        e.printStackTrace(stream);      
+    }
+
+
+}
