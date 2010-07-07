b2aa0caca8f90717162d74fa571cd7df62343662
zaelenchits <zaelenchits@0edadee9-8154-4c59-a661-b5f45207fcbd>
2010-07-07 13:08:46 +0000

    Feature #780
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@3794 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.neo4j.neoclipse/src/main/java/org/neo4j/neoclipse/preference/NeoTuningPreferencePage.java
+++ org.neo4j.neoclipse/src/main/java/org/neo4j/neoclipse/preference/NeoTuningPreferencePage.java
@@ -14,6 +14,18 @@
 package org.neo4j.neoclipse.preference;
 
 import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Dialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * <p>
@@ -39,29 +51,143 @@ public class NeoTuningPreferencePage extends AbstractPreferencePage {
         IntegerFieldEditor nodes = new IntegerFieldEditor(Preferences.NEOSTORE_NODES, LABEL_NODES, getFieldEditorParent());
         nodes.setValidRange(1, Integer.MAX_VALUE);
         addField(nodes, PROPTERTY_NOTE);
-        IntegerFieldEditor relationships = new IntegerFieldEditor(Preferences.NEOSTORE_RELATIONSHIPS, LABEL_RELATIONSHIPS,
-                getFieldEditorParent());
+        IntegerFieldEditor relationships = new IntegerFieldEditor(Preferences.NEOSTORE_RELATIONSHIPS, LABEL_RELATIONSHIPS, getFieldEditorParent());
         relationships.setValidRange(1, Integer.MAX_VALUE);
         addField(relationships, PROPTERTY_NOTE);
-        IntegerFieldEditor properties = new IntegerFieldEditor(Preferences.NEOSTORE_PROPERTIES, LABEL_PROPERTIES,
-                getFieldEditorParent());
+        IntegerFieldEditor properties = new IntegerFieldEditor(Preferences.NEOSTORE_PROPERTIES, LABEL_PROPERTIES, getFieldEditorParent());
         properties.setValidRange(1, Integer.MAX_VALUE);
         addField(properties, PROPTERTY_NOTE);
-        IntegerFieldEditor properties_ind = new IntegerFieldEditor(Preferences.NEOSTORE_PROPERTIES_INDEX,
-                LABEL_PROPERTIES_INDEX, getFieldEditorParent());
+        IntegerFieldEditor properties_ind = new IntegerFieldEditor(Preferences.NEOSTORE_PROPERTIES_INDEX, LABEL_PROPERTIES_INDEX, getFieldEditorParent());
         properties_ind.setValidRange(1, Integer.MAX_VALUE);
         addField(properties_ind, PROPTERTY_NOTE);
-        IntegerFieldEditor properties_keys = new IntegerFieldEditor(Preferences.NEOSTORE_PROPERTIES_KEYS, LABEL_PROPERTIES_KEYS,
-                getFieldEditorParent());
+        IntegerFieldEditor properties_keys = new IntegerFieldEditor(Preferences.NEOSTORE_PROPERTIES_KEYS, LABEL_PROPERTIES_KEYS, getFieldEditorParent());
         properties_keys.setValidRange(1, Integer.MAX_VALUE);
         addField(properties_keys, PROPTERTY_NOTE);
-        IntegerFieldEditor properties_string = new IntegerFieldEditor(Preferences.NEOSTORE_PROPERTIES_STRING,
-                LABEL_PROPERTIES_STRING, getFieldEditorParent());
+        IntegerFieldEditor properties_string = new IntegerFieldEditor(Preferences.NEOSTORE_PROPERTIES_STRING, LABEL_PROPERTIES_STRING, getFieldEditorParent());
         properties_string.setValidRange(1, Integer.MAX_VALUE);
         addField(properties_string, PROPTERTY_NOTE);
-        IntegerFieldEditor properties_arrays = new IntegerFieldEditor(Preferences.NEOSTORE_PROPERTIES_ARRAYS,
-                LABEL_PROPERTIES_ARRAY, getFieldEditorParent());
+        IntegerFieldEditor properties_arrays = new IntegerFieldEditor(Preferences.NEOSTORE_PROPERTIES_ARRAYS, LABEL_PROPERTIES_ARRAY, getFieldEditorParent());
         properties_arrays.setValidRange(1, Integer.MAX_VALUE);
         addField(properties_arrays, PROPTERTY_NOTE);
     }
+
+    @Override
+    protected void performApply() {
+        super.performApply();
+        
+        Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+        Promt pdialog = new Promt(shell, "title");;
+        if (pdialog.open()) {
+            PlatformUI.getWorkbench().restart();
+        }
+        
+        
+    }
+
+    @Override
+    public boolean performOk() {
+        return super.performOk();
+    }
+    // @Override
+    // protected Control createContents(Composite parent) {
+    // Composite control = (Composite)super.createContents(parent);
+    // Button b = new Button(control, SWT.PUSH);
+    // b.setText("Restart");
+    // GridData layoutData = new GridData(SWT.DEFAULT, SWT.DEFAULT, false, false);
+    // b.setLayoutData(layoutData);
+    // return control;
+    // }
+    private class Promt extends Dialog{
+
+        private final String title;
+        protected boolean status;
+        private Shell shell;
+        private Button bOk;
+        private Button bCancel;
+
+        public Promt(Shell parent, String title) {
+            this(parent, title, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL | SWT.CENTER);
+        }
+
+        public Promt(Shell parent, String title, int style) {
+            super(parent, style);
+            this.title = title;
+        }
+
+        public boolean open() {
+            Shell parentShell = getParent();
+            Shell shell = new Shell(parentShell, getStyle());
+            shell.setText(getTitle());
+
+            createContents(shell);
+            shell.pack();
+
+            // calculate location
+            Point size = parentShell.getSize();
+            int dlgWidth = shell.getSize().x;
+            int dlgHeight = shell.getSize().y;
+            shell.setLocation((size.x - dlgWidth) / 2, (size.y - dlgHeight) / 2);
+            beforeOpen();
+            shell.open();
+            // wait
+            Display display = getParent().getDisplay();
+            while (!shell.isDisposed()) {
+                if (!display.readAndDispatch()) {
+                    display.sleep();
+                }
+            }
+            dispose();
+            return status;
+        }
+
+        protected void dispose() {
+        }
+
+        protected void beforeOpen() {
+        }
+
+        protected String getTitle() {
+            return title;
+        }
+
+        protected void createContents(final Shell shell){
+            this.shell = shell;
+//            shell.setImage(NodeTypes.DATASET.getImage());
+            shell.setLayout(new GridLayout(2, false));
+            
+            bOk = createButton(shell, "Restart now");
+            bOk.addSelectionListener(new SelectionAdapter() {
+                @Override
+                public void widgetSelected(SelectionEvent e) {
+                    status = true;
+                    shell.close();
+                }
+            });
+            
+            bCancel = createButton(shell, "Restart later");
+            bCancel.addSelectionListener(new SelectionAdapter() {
+                @Override
+                public void widgetSelected(SelectionEvent e) {
+                    status = false;
+                    shell.close();
+                }
+            });
+            
+        };
+        
+        /**
+         * Create button
+         * 
+         * @param parent parent composite
+         * @param name visible name
+         * @return Button
+         */
+        private Button createButton(Composite parent, String name) {
+            Button button = new Button(parent, SWT.PUSH);
+            button.setText(name);
+            button.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
+            return button;
+        }
+        
+    } 
 }
