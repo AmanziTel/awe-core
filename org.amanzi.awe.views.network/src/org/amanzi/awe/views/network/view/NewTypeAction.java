57ef277806bb45d4384718f5a08d14f3d7bcdbdc
zaelenchits <zaelenchits@0edadee9-8154-4c59-a661-b5f45207fcbd>
2010-09-15 19:09:59 +0000

    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@4262 0edadee9-8154-4c59-a661-b5f45207fcbd

---
+++ org.amanzi.awe.views.network/src/org/amanzi/awe/views/network/view/NewTypeAction.java
@@ -0,0 +1,45 @@
+/* AWE - Amanzi Wireless Explorer
+ * http://awe.amanzi.org
+ * (C) 2008-2009, AmanziTel AB
+ *
+ * This library is provided under the terms of the Eclipse Public License
+ * as described at http://www.eclipse.org/legal/epl-v10.html. Any use,
+ * reproduction or distribution of the library constitutes recipient's
+ * acceptance of this agreement.
+ *
+ * This library is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+package org.amanzi.awe.views.network.view;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+
+/**
+ * TODO Purpose of 
+ * <p>
+ *
+ * </p>
+ * @author Saelenchits_N
+ * @since 1.0.0
+ */
+public class NewTypeAction extends Action implements IViewActionDelegate{
+
+    @Override
+    public void run(IAction action) {
+        System.out.println("\norg.amanzi.awe.views.network.view.NewTypeAction 2\n");
+    }
+
+    @Override
+    public void selectionChanged(IAction action, ISelection selection) {
+    }
+
+    @Override
+    public void init(IViewPart view) {
+    }
+
+}
