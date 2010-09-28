04243bcfe2e418fff54d8935e3f57e2260f26ccd
artemij <artemij@0edadee9-8154-4c59-a661-b5f45207fcbd>
2010-09-28 07:26:08 +0000

    refactor ceating of gis node
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@4346 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.neo.services/src/org/amanzi/neo/services/DatasetService.java
+++ org.amanzi.neo.services/src/org/amanzi/neo/services/DatasetService.java
@@ -951,6 +951,8 @@ public class DatasetService extends AbstractService {
                 INodeType type = getNodeType(root);
                 GisTypes gisType = GisTypes.NETWORK.getGisTypeFromRootType(type.getId());
                 gis.setProperty(INeoConstants.PROPERTY_GIS_TYPE_NAME, gisType.getHeader());
+                databaseService.getReferenceNode().createRelationshipTo(gis, GeoNeoRelationshipTypes.CHILD);
+                gis.createRelationshipTo(root, GeoNeoRelationshipTypes.NEXT);
                 tx.success();
             } finally {
                 tx.finish();
