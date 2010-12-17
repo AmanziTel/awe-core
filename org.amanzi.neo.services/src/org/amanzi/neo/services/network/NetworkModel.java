c95ab3ec47fd2a42ecc8c7228b00533fbf263f96
zaelenchits <zaelenchits@0edadee9-8154-4c59-a661-b5f45207fcbd>
2010-12-17 12:52:11 +0000

    Bug #2128
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@4886 0edadee9-8154-4c59-a661-b5f45207fcbd

---
+++ org.amanzi.neo.services/src/org/amanzi/neo/services/network/NetworkModel.java
@@ -0,0 +1,47 @@
+/* AWE - Amanzi Wireless Explorer
+ * http://awe.amanzi.org
+ * (C) 2008-2009, AmanziTel AB
+ *
+ * This library is provided under the terms of the Eclipse Public License
+ * as described at http://www.eclipse.org/legal/epl-v10.html. Any use,
+ * reproduction or distribution of the library constitutes recipient's
+ * acceptance of this agreement.
+ *
+ * This library is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+package org.amanzi.neo.services.network;
+
+import org.amanzi.neo.services.DatasetService;
+import org.amanzi.neo.services.NeoServiceFactory;
+import org.neo4j.graphdb.Node;
+
+/**
+ * TODO Purpose of
+ * <p>
+ * 
+ * </p>
+ * 
+ * @author Saelenchits_N
+ * @since 1.0.0
+ */
+public class NetworkModel {
+	private final Node rootNode;
+	private final DatasetService ds;
+
+	public NetworkModel(Node rootNode) {
+		this.rootNode = rootNode;
+		ds = NeoServiceFactory.getInstance().getDatasetService();
+	}
+
+//	public List<NetworkSelectionModel> getSelectionModels() {
+//		List<NetworkSelectionModel> models = ds.getSelectionModels(rootNode);
+//		return models;
+//	}
+//
+//	public NetworkSelectionModel getSelectionModel(String selectionName) {
+//		return null;
+//	}
+
+}
