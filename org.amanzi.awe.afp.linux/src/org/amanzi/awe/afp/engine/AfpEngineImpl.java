f917d1895594496047cb2a2d9406181073f02632
cat <cat@0edadee9-8154-4c59-a661-b5f45207fcbd>
2010-12-27 17:12:04 +0000

    better fix for path, should work on both windows and linux
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@4928 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.awe.afp.linux/src/org/amanzi/awe/afp/engine/AfpEngineImpl.java
+++ org.amanzi.awe.afp.linux/src/org/amanzi/awe/afp/engine/AfpEngineImpl.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.URL;
 
 import org.amanzi.awe.afp.Activator;
-import org.amanzi.awe.afp.engine.*;
 import org.eclipse.core.runtime.Path;
 
 public class AfpEngineImpl extends org.amanzi.awe.afp.AfpEngine{
@@ -15,8 +14,7 @@ public class AfpEngineImpl extends org.amanzi.awe.afp.AfpEngine{
 		try {
 			URL url = org.eclipse.core.runtime.FileLocator.find(Activator.getDefault().getBundle(), 
 				new Path("exe/japa_awe.exe"), null);
-			path = org.eclipse.core.runtime.FileLocator.toFileURL(url).toString();
-			path = path.substring(5);
+            path = org.eclipse.core.runtime.FileLocator.toFileURL(url).getPath();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
