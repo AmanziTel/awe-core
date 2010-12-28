47b950dac566c02f41dc7a221f2bb36f2f47da2d
rahul <rahul@0edadee9-8154-4c59-a661-b5f45207fcbd>
2010-12-28 07:54:12 +0000

    AFP: populating the filter table on separations page
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@4937 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.awe.afp/src/org/amanzi/awe/afp/wizards/AfpSeparationRulesPage.java
+++ org.amanzi.awe.afp/src/org/amanzi/awe/afp/wizards/AfpSeparationRulesPage.java
@@ -8,6 +8,8 @@ import java.util.Vector;
 import org.amanzi.awe.afp.models.AfpHoppingMALDomainModel;
 import org.amanzi.awe.afp.models.AfpModel;
 import org.amanzi.awe.afp.models.AfpSeparationDomainModel;
+import org.amanzi.neo.services.INeoConstants;
+import org.amanzi.neo.services.enums.NetworkRelationshipTypes;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
@@ -20,6 +22,7 @@ import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
+import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Traverser;
 
@@ -31,7 +34,7 @@ public class AfpSeparationRulesPage extends AfpWizardPage {
 	protected HashMap<String, Label[]> siteDomainLabels;
 	private AfpModel model;
 	private final String[] headers = { "BSC", "Site", "Sector", "Layer"};
-	private final String[] prop_name = { "BSC", "Site", "name", "Layer"};
+	private final String[] prop_name = { "bsc", "Site", INeoConstants.PROPERTY_NAME_NAME, "Layer"};
 	private Table filterTableSector;
 	private Table filterTableSite;
 
@@ -174,7 +177,8 @@ public class AfpSeparationRulesPage extends AfpWizardPage {
 			defaultSitesLabel.setText("0");
 			siteDomainLabels.put(sectorDomainModel.getName(), new Label[]{defaultSiteDomainLabel, defaultSitesLabel});
 		}
-		//loadData();
+		loadData(filterTableSite);
+		loadData(filterTableSector);
 		siteDomainsGroup.layout();
 	}
 	public void loadData(Table table) {
@@ -189,11 +193,21 @@ public class AfpSeparationRulesPage extends AfpWizardPage {
 		    		break;
 		    	TableItem item = new TableItem(table, SWT.NONE);
 		    	for (int j = 0; j < headers.length; j++){
+		    		String val = "";
 		    		try {
-		    			String val = (String)node.getProperty(prop_name[j]);
+		    			if (prop_name[j].equals("bsc")){
+		    				val = (String)node.getProperty(prop_name[j], "bsc");
+		    			}
+		    			else if (prop_name[j].equals("Site")){
+		    				Node siteNode = node.getSingleRelationship(NetworkRelationshipTypes.CHILD, Direction.INCOMING).getStartNode();
+		    				if (siteNode.getProperty(INeoConstants.PROPERTY_TYPE_NAME).equals("site"))
+		    					val = (String)siteNode.getProperty(INeoConstants.PROPERTY_NAME_NAME, "");
+		    			}	
+		    			else 
+		    				val = (String)node.getProperty(prop_name[j], "");
 		    			item.setText(j, val);
 		    		} catch(Exception e) {
-		    			item.setText(j, "NA");
+		    			item.setText(j, "");
 		    		}
 		    	}
 		    	cnt++;
