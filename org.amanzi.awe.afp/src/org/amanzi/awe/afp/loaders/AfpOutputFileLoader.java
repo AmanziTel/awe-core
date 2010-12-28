e4e21777e4d66dc8bc98a247bd69ca21f32bf1eb
rahul <rahul@0edadee9-8154-4c59-a661-b5f45207fcbd>
2010-12-28 22:00:30 +0000

    2095: AFP - loading back result
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@4950 0edadee9-8154-4c59-a661-b5f45207fcbd

---
+++ org.amanzi.awe.afp/src/org/amanzi/awe/afp/loaders/AfpOutputFileLoader.java
@@ -0,0 +1,55 @@
+package org.amanzi.awe.afp.loaders;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.amanzi.awe.afp.files.ControlFile;
+import org.amanzi.neo.services.ui.NeoUtils;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+
+public class AfpOutputFileLoader extends AfpLoader {
+	
+	String outputFileName;
+
+	public AfpOutputFileLoader(Node networkRoot,String outputFileName,Node afpDataset,  GraphDatabaseService service) {
+		super("", null,service);
+		
+		this.afpRoot = networkRoot;
+		this.afpDataset = afpDataset;
+		this.outputFileName = outputFileName;
+		
+	}
+	
+    /**
+     * Run.
+     * 
+     * @param monitor the monitor
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    @Override
+    public void run(IProgressMonitor monitor) throws IOException {
+        //monitor.beginTask("Load AFP data", 7);
+        runAfpLoader(monitor,null);
+    }
+    
+    public void runAfpLoader(IProgressMonitor monitor, String projectName) {
+
+    	mainTx = neo.beginTx();
+        NeoUtils.addTransactionLog(mainTx, Thread.currentThread(), "AfpLoader");
+        try {
+        	File outputFile = new File(this.outputFileName);
+        	
+            //defineRoot(projectName);
+            if (outputFile != null) {
+                loadCellFile(outputFile);
+            }
+            commit(true);
+           
+            saveProperties();
+        } finally {
+            commit(false);
+        }
+    }
+}
