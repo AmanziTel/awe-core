7f98120213ad232cbf79f3c40278397c51395495
lagutko <lagutko@0edadee9-8154-4c59-a661-b5f45207fcbd>
2011-02-22 15:48:18 +0000

    #2298
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@5200 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.neo.loader.ui/src/org/amanzi/neo/loader/ui/handlers/LaunchLoader.java
+++ org.amanzi.neo.loader.ui/src/org/amanzi/neo/loader/ui/handlers/LaunchLoader.java
@@ -142,7 +142,7 @@ public class LaunchLoader extends AbstractHandler {
             ISaver< ? extends IDataElement> saver = defineSaver(element);
             if (parser != null && saver != null) {
                 loader.setDescription(element.getAttribute("description"));
-                loader.setRenderable(Boolean.getBoolean(element.getAttribute("renderable")));
+                loader.setRenderable(Boolean.parseBoolean(element.getAttribute("renderable")));
                 // TODO define additional pages for loaders!
                 loader.setParser(parser);
                 loader.setSaver(saver);
