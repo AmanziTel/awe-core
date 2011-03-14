68b31b5f4b4f2a7cdb7776d42191b4f06b82d79c
pechko_elena <pechko_elena@0edadee9-8154-4c59-a661-b5f45207fcbd>
2011-03-14 11:44:37 +0000

    typo in formulas file name fixed
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@5276 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.awe.statistics/ruby/kpi/tems_formulas.rb
+++ org.amanzi.awe.statistics/ruby/kpi/tems_formulas.rb
@@ -5,55 +5,55 @@ module KPI
     annotation :name=>'Call Attempt'
     def Tems.call_attempt(data)
       event=data[:event]
-      !event.nil? and event=="Call Attempt"? 1:0
+      !event.nil? and event=="Call Attempt"? 1:nil
     end
     annotation :name=>'Call End'
 
     def Tems.call_end(data)
       event=data[:event]
-      !event.nil? and event=="Call End"? 1:0
+      !event.nil? and event=="Call End"? 1:nil
     end
     annotation :name=>'Call Setup'
 
     def Tems.call_setup(data)
       event=data[:event]
-      !event.nil? and event=="Call Setup"? 1:0
+      !event.nil? and event=="Call Setup"? 1:nil
     end
     annotation :name=>'Call Established'
 
     def Tems.call_established(data)
       event=data[:event]
-      !event.nil? and event=="Call Established"? 1:0
+      !event.nil? and event=="Call Established"? 1:nil
     end
     annotation :name=>'Dedicated Mode'
 
     def Tems.dedicated_mode(data)
       event=data[:event]
-      !event.nil? and event=="Dedicated Mode"? 1:0
+      !event.nil? and event=="Dedicated Mode"? 1:nil
     end
     annotation :name=>'Handover'
 
     def Tems.handover(data)
       event=data[:event]
-      !event.nil? and event=="Handover"? 1:0
+      !event.nil? and event=="Handover"? 1:nil
     end
     annotation :name=>'Handover Failure'
 
     def Tems.handover_failure(data)
       event=data[:event]
-      !event.nil? and event=="Handover Failure"? 1:0
+      !event.nil? and event=="Handover Failure"? 1:nil
     end
     annotation :name=>'Handover Intracell'
 
     def Tems.handover_intracell(data)
       event=data[:event]
-      !event.nil? and event=="Handover Intracell"? 1:0
+      !event.nil? and event=="Handover Intracell"? 1:nil
     end
     annotation :name=>'Idle Mode'
 
     def Tems.idle_mode(data)
       event=data[:event]
-      !event.nil? and event=="Idle Mode"? 1:0
+      !event.nil? and event=="Idle Mode"? 1:nil
     end
 
   end
