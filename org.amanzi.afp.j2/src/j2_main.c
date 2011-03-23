6c724ba8eca86c1eaeffe306c5244afc3e052313
cat <cat@0edadee9-8154-4c59-a661-b5f45207fcbd>
2011-03-23 12:19:50 +0000

    Added text cases from Jan
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@5340 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.afp.j2/src/j2_main.c
+++ org.amanzi.afp.j2/src/j2_main.c
@@ -3,7 +3,7 @@
 
 #define MAXNAMELENGTH 256
 
-int  japa_awe(char * pctrfile);
+int japa_awe(char * pctrfile);
 
 int main(int argc, char *argv[])
 
@@ -14,20 +14,16 @@ int main(int argc, char *argv[])
 		printf("Syntax Error :: %s <input control file name>\n", argv[0]);
 		return -1;
 	}
-	
-	strncpy(TrialName,argv[1], MAXNAMELENGTH - 1);
-	TrialName[MAXNAMELENGTH - 1] =0;
 
+	strncpy(TrialName, argv[1], MAXNAMELENGTH - 1);
+	TrialName[MAXNAMELENGTH - 1] =0;
 
 	awe_afp(TrialName);
 
-    printf("\n\nj2 for %s finished\n\n", TrialName);
+	printf("\n\nj2 for %s finished\n\n", TrialName);
 
 	fflush(stdin);
 	fflush(stdout);
 
-    return EXIT_SUCCESS;
-
-
-
+	return EXIT_SUCCESS;
 }
