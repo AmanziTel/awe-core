2c284d308c5df00a868a3e3c0221af65550c5953
artemij <artemij@0edadee9-8154-4c59-a661-b5f45207fcbd>
2011-05-14 17:18:26 +0000

    BSIC/HSN optimizer
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@5539 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.awe.afp/src/org/amanzi/awe/afp/exporters/HsnOptimizer.java
+++ org.amanzi.awe.afp/src/org/amanzi/awe/afp/exporters/HsnOptimizer.java
@@ -13,13 +13,17 @@
 
 package org.amanzi.awe.afp.exporters;
 
+import java.util.Random;
+
 import org.amanzi.neo.services.NeoServiceFactory;
 import org.amanzi.neo.services.NetworkService;
 import org.amanzi.neo.services.network.FrequencyPlanModel;
 import org.amanzi.neo.services.network.NetworkModel;
 import org.amanzi.neo.services.statistic.IStatistic;
 import org.amanzi.neo.services.statistic.StatisticManager;
+import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 /**
  * TODO Purpose of 
@@ -30,13 +34,17 @@ import org.eclipse.core.runtime.IProgressMonitor;
  * @since 1.0.0
  */
 public class HsnOptimizer {
+    static final Logger LOG=Logger.getLogger(HsnOptimizer.class);
     private NetworkModel network;
     private FrequencyPlanModel freq;
     private NetworkService ns;
     private IStatistic statistic;
+    private CoordinateReferenceSystem crs;
+    private Random random=new Random();
 
     public HsnOptimizer(NetworkModel network, FrequencyPlanModel freq) {
         this.network = network;
+        crs=network.getCrs();
         this.freq = freq;
         ns = NeoServiceFactory.getInstance().getNetworkService();
         statistic = StatisticManager.getStatistic(network.getRootNode());
@@ -53,6 +61,46 @@ public class HsnOptimizer {
      * @param monitor
      */
     private void optimize(HSNImpactMatrix matrix, IProgressMonitor monitor) {
-        //TODO implemented, but chrckin after debug
+        double violation=Double.POSITIVE_INFINITY;
+        //TODO only random, check for more correctly mechanism
+        int k=0;
+        int maxK=100;
+        do{
+            k++;
+            matrix.clearDefine();
+            double viol=defineMatrix(matrix);
+            if (viol<violation){
+                k=0;
+                maxK=maxK/2;
+                violation=viol;
+                matrix.storeDefine();
+            }
+        }while (violation>0||k<maxK);
+        matrix.saveStored(freq,statistic);
+        LOG.info(String.format("Violation by hsn is %s ",violation));
     }
+    /**
+     *
+     * @param matrix
+     * @return
+     */
+    private double defineMatrix(HSNImpactMatrix matrix) {
+        int hsn=1;
+        for (int i=0;i<matrix.getSize();i++){
+            matrix.definePlanToRandomNode(hsn);
+            hsn++;
+            if (hsn>63){
+                hsn=1;
+            }
+        }
+        return matrix.calculateViolation(crs);
+    }
+    /**
+     *
+     * @return
+     */
+//    private int getHsn() {
+//        //1..63
+//        return random.nextInt(63)+1;
+//    }
 }
