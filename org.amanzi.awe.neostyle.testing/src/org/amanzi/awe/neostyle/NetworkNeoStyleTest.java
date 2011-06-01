4df17d018b46c0596adadc148651c518385318ed
artemij <artemij@0edadee9-8154-4c59-a661-b5f45207fcbd>
2011-06-01 11:42:35 +0000

    feature #2375
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@5673 0edadee9-8154-4c59-a661-b5f45207fcbd

---
+++ org.amanzi.awe.neostyle.testing/src/org/amanzi/awe/neostyle/NetworkNeoStyleTest.java
@@ -0,0 +1,58 @@
+/* AWE - Amanzi Wireless Explorer
+ * http://awe.amanzi.org
+ * (C) 2008-2009, AmanziTel AB
+ *
+ * This library is provided under the terms of the Eclipse Public License
+ * as described at http://www.eclipse.org/legal/epl-v10.html. Any use,
+ * reproduction or distribution of the library constitutes recipient's
+ * acceptance of this agreement.
+ *
+ * This library is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+package org.amanzi.awe.neostyle;
+
+import org.amanzi.neo.services.enums.NodeTypes;
+import org.junit.Test;
+
+/**
+ * <p>
+ *Test Network style
+ * </p>
+ * @author tsinkel_a
+ * @since 1.0.0
+ */
+public class NetworkNeoStyleTest {
+//    private Mockery context=new JUnit4Mockery();
+    @Test
+    public  final void  ckeckSectorLabelTextOnEmptyOrNullPrperty(){
+        NetworkNeoStyle style=createDefaultNetworkStyle();
+        style.setSectorLabelTypeId(NodeTypes.SECTOR.getId());
+        style.setSectorLabelProperty("property1");
+    }
+
+    /**
+     *
+     * @return
+     */
+    private  NetworkNeoStyle createDefaultNetworkStyle() {
+            NetworkNeoStyle result = new NetworkNeoStyle(NetworkNeoStyleContent.DEF_COLOR_LINE, NetworkNeoStyleContent.DEF_COLOR_FILL, NetworkNeoStyleContent.DEF_COLOR_LABEL);
+            result.setSmallestSymb(NetworkNeoStyleContent.DEF_SMALLEST_SYMB);
+            result.setSmallSymb(NetworkNeoStyleContent.DEF_SMALL_SYMB);
+            result.setLabeling(NetworkNeoStyleContent.DEF_LABELING);
+            result.setFixSymbolSize(NetworkNeoStyleContent.DEF_FIX_SYMB_SIZE);
+            result.setSymbolSize(NetworkNeoStyleContent.DEF_SYMB_SIZE);
+            result.setSymbolTransparency(NetworkNeoStyleContent.DEF_TRANSPARENCY);
+            result.setSiteFill(NetworkNeoStyleContent.DEF_COLOR_SITE);
+            result.setMaximumSymbolSize(NetworkNeoStyleContent.DEF_MAXIMUM_SYMBOL_SIZE);
+            result.setDefaultBeamwidth(NetworkNeoStyleContent.DEF_DEF_BEAMWIDTH);
+            result.setFontSize(NetworkNeoStyleContent.DEF_FONT_SIZE);
+            result.setSectorFontSize(NetworkNeoStyleContent.DEF_FONT_SIZE_SECTOR);
+            result.setMainProperty(NetworkNeoStyleContent.DEF_MAIN_PROPERTY);
+            result.setSectorLabelProperty(NetworkNeoStyleContent.DEF_SECONDARY_PROPERTY);
+            result.setSectorLabelTypeId(NetworkNeoStyleContent.DEF_SECTOR_LABEL_TYPE_ID);
+            result.setIgnoreTransparency(NetworkNeoStyleContent.IGNORE_TRANSPARENCY);
+            return result;
+        }
+}
