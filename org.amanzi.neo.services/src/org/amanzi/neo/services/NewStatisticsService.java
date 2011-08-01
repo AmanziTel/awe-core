2eef2e23b6af18bf17ffd460ec7da070e6eb57a2
Kruglik_A <Kruglik_A@0edadee9-8154-4c59-a661-b5f45207fcbd>
2011-08-01 06:36:14 +0000

    refactor savaVault
    git-svn-id: svn+ssh://svn.amanzi.org/var/lib/svn/trunk/awe@6052 0edadee9-8154-4c59-a661-b5f45207fcbd

--- org.amanzi.neo.services/src/org/amanzi/neo/services/NewStatisticsService.java
+++ org.amanzi.neo.services/src/org/amanzi/neo/services/NewStatisticsService.java
@@ -17,8 +17,11 @@ import java.util.ArrayList;
 
 import org.amanzi.neo.services.enums.INodeType;
 import org.amanzi.neo.services.exceptions.DatabaseException;
+import org.amanzi.neo.services.exceptions.DuplicateStatisticsException;
+import org.amanzi.neo.services.exceptions.InvalidStatisticsParameterException;
 import org.amanzi.neo.services.statistic.IVault;
 import org.apache.log4j.Logger;
+import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
@@ -33,14 +36,12 @@ import org.neo4j.graphdb.Transaction;
  */
 public class NewStatisticsService extends NewAbstractService {
 
-    
     public static final String CLASS = "class";
     public static final String COUNT = "count";
 
-    
     private static Logger LOGGER = Logger.getLogger(NewAbstractService.class);
     private Transaction tx;
-    
+
     public enum StatisticsRelationships implements RelationshipType {
         STATISTICS, CHILD;
     }
@@ -54,8 +55,18 @@ public class NewStatisticsService extends NewAbstractService {
         }
     }
 
-    public void saveVault(Node rootNode, IVault vault) throws DatabaseException {
+    public void saveVault(Node rootNode, IVault vault) throws DatabaseException, InvalidStatisticsParameterException,
+            DuplicateStatisticsException {
         LOGGER.debug("start method saveVault(Node rootNode, IVault vault)");
+        if (rootNode == null) {
+            throw new InvalidStatisticsParameterException("rootNode", rootNode);
+        }
+        if (vault == null) {
+            throw new InvalidStatisticsParameterException("vault", vault);
+        }
+        if (rootNode.getRelationships(StatisticsRelationships.STATISTICS, Direction.OUTGOING).iterator().hasNext()) {
+            throw new DuplicateStatisticsException("for this rootNode already exists statistics");
+        }
         Node vaultNode = createNode(StatisticsNodeTypes.VAULT);
         tx = graphDb.beginTx();
         try {
@@ -64,7 +75,7 @@ public class NewStatisticsService extends NewAbstractService {
             } else {
                 rootNode.createRelationshipTo(vaultNode, StatisticsRelationships.STATISTICS);
             }
-            vaultNode.setProperty(PROPERTY_NAME_NAME, vault.getType());          
+            vaultNode.setProperty(PROPERTY_NAME_NAME, vault.getType());
             vaultNode.setProperty(COUNT, vault.getCount());
             vaultNode.setProperty(CLASS, vault.getClass().toString());
             for (IVault subVault : vault.getSubVaults()) {
@@ -72,7 +83,7 @@ public class NewStatisticsService extends NewAbstractService {
             }
             tx.success();
         } catch (Exception e) {
-            LOGGER.error("Could not create vault node in database",e);
+            LOGGER.error("Could not create vault node in database", e);
             tx.failure();
             throw new DatabaseException(e);
 
