83db80fc16df7d779b87501760b59a0e0e54f96e
gerzog <gerzog@gmx.com>
2011-08-23 12:28:29 +0300

    Remove GPEH plugins

--- org.amanzi.awe.gpeh.console/ConvertGPEH.bat
+++
@@ -1,5 +0,0 @@
-set CP=gt2-main.jar;jflac-1.3.jar;org.eclipse.equinox.common_3.3.0.v20070426.jar;log4j-1.2.8.jar;geoapi.jar
-cd bin
-java -cp %CP%;. org.amanzi.awe.gpeh.console.test.ProjectRun %1 %2
-cd ..
-pause
