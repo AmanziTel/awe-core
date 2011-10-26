bb4e031e158750760a3b3b94ae3f55e09ed4179f
kostyukovich_n <nikolai.kostyukovich@gersis-software.com>
2011-10-26 16:07:45 +0300

    Number Distribution

--- org.amanzi.neo.services/src/org/amanzi/neo/model/distribution/types/impl/AbstractDistribution.java
+++ org.amanzi.neo.services/src/org/amanzi/neo/model/distribution/types/impl/AbstractDistribution.java
@@ -25,43 +25,46 @@ import org.apache.log4j.Logger;
 
 /**
  * Abstract class for Distributions
+ * 
  * @author gerzog
  * @since 1.0.0
  */
 public abstract class AbstractDistribution<T extends IRange> implements IDistribution<T> {
-    
+
     private static final Logger LOGGER = Logger.getLogger(AbstractDistribution.class);
-    
+    private static final int DOUBLE_SCALE = 4;
+    static final double EPS = 0.00001;
+
     /*
-     * Analyzed Model 
+     * Analyzed Model
      */
     protected IDistributionalModel model;
-    
+
     /*
      * Type of Node to Analyze
      */
     protected INodeType nodeType;
-    
+
     /*
      * Name of Property to Analyze
      */
     protected String propertyName;
-    
+
     /*
      * Were Ranges already created
      */
     private boolean isInitialized = false;
-    
+
     /*
      * List of ranges
      */
     protected List<T> ranges = new ArrayList<T>();
-    
+
     /*
      * Type of Select
      */
     protected Select select;
-    
+
     /**
      * Default constructor
      * 
@@ -71,8 +74,8 @@ public abstract class AbstractDistribution<T extends IRange> implements IDistrib
      */
     protected AbstractDistribution(IDistributionalModel model, INodeType nodeType, String propertyName) {
         LOGGER.debug("start new AbstractDistribution(<" + model + ">, <" + nodeType + ">, <" + propertyName + ">)");
-        
-        //check input
+
+        // check input
         if (model == null) {
             LOGGER.error("Analyzed model cannot be null");
             throw new IllegalArgumentException("Analyzed model cannot be null");
@@ -85,11 +88,11 @@ public abstract class AbstractDistribution<T extends IRange> implements IDistrib
             LOGGER.error("PropertyName to Analyze cannot be null or empty");
             throw new IllegalArgumentException("PropertyName to Analyze cannot be null or empty");
         }
-        
+
         this.model = model;
         this.nodeType = nodeType;
         this.propertyName = propertyName;
-        
+
         LOGGER.debug("finish new AbstractDistribution()");
     }
 
@@ -97,40 +100,38 @@ public abstract class AbstractDistribution<T extends IRange> implements IDistrib
     public String toString() {
         return getName();
     }
-    
+
     @Override
     public void init() {
-        //distribution can came from Cache, so initialize it only once
+        // distribution can came from Cache, so initialize it only once
         if (!isInitialized) {
             isInitialized = true;
             createRanges();
         }
     }
-    
+
     @Override
     public List<T> getRanges() {
         return ranges;
     }
-    
+
     /**
      * Method to Create Ranges of Distribution
      */
     protected abstract void createRanges();
-    
-
 
     @Override
     public INodeType getNodeType() {
         return nodeType;
     }
-    
+
     /**
      * Returns Default Select for this Distribution
-     *
+     * 
      * @return
      */
     protected abstract Select getDefaultSelect();
-    
+
     @Override
     public void setSelect(Select select) {
         if (ArrayUtils.contains(getPossibleSelects(), select)) {
@@ -142,10 +143,40 @@ public abstract class AbstractDistribution<T extends IRange> implements IDistrib
             throw new IllegalArgumentException("Cannot set Select <" + select + "> for Distribution <" + this + ">.");
         }
     }
-    
+
     @Override
     public String getPropertyName() {
         return propertyName;
     }
 
+    static String getNumberDistributionRangeName(double min, double max) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(min).append(" - ").append(max);
+        return sb.toString();
+    }
+
+    static double getMinValue(Number[] values) {
+        double res = Double.MAX_VALUE;
+        for (Number v : values) {
+            if (v.doubleValue() < res)
+                res = v.doubleValue();
+        }
+        return res;
+    }
+
+    static double getMaxValue(Number[] values) {
+        double res = -Double.MAX_VALUE;
+        for (Number v : values) {
+            if (v.doubleValue() > res)
+                res = v.doubleValue();
+        }
+        return res;
+    }
+
+    static double getStep(double min, double max, int delta) {
+        double res = (max - min) / delta;
+        double scaleNum = Math.pow(10, DOUBLE_SCALE);
+        res = Math.round(res * scaleNum) / scaleNum;
+        return res;
+    }
 }
