828063f6d1db1eb5a064a53df3f981b3e73ab0ff
Nikolay Lagutko <nikolay.lagutko@amanzitel.com>
2011-11-24 01:29:10 +0300

    Configuring OpenSource Test Suite Cleaning up plugin dependencies

--- org.amanzi.awe.core.testing.suite/src/org/amanzi/awe/core/testing/suite/OpenSourceCoreTestSuite.java
+++
@@ -1,8 +0,0 @@
-package org.amanzi.awe.core.testing.suite;
-
-import org.junit.runners.Suite.SuiteClasses;
-
-@SuiteClasses({ org.amanzi.neo.db.manager.impl.Neo4jDatabaseManagerTest.class })
-public class OpenSourceCoreTestSuite {
-
-}
