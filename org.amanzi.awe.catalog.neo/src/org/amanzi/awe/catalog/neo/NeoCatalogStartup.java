d6119be11f295a468b150bda37f3a025e2598650
Vladislav Kondratenko <k.volad@gmail.com>
2011-12-07 19:57:39 +0300

    refactored

--- org.amanzi.awe.catalog.neo/src/org/amanzi/awe/catalog/neo/NeoCatalogStartup.java
+++ org.amanzi.awe.catalog.neo/src/org/amanzi/awe/catalog/neo/NeoCatalogStartup.java
@@ -13,29 +13,19 @@
 
 package org.amanzi.awe.catalog.neo;
 
-import java.io.IOException;
-import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.UnknownHostException;
-import java.util.LinkedList;
 import java.util.List;
 
 import net.refractions.udig.catalog.CatalogPlugin;
 import net.refractions.udig.catalog.ICatalog;
-import net.refractions.udig.catalog.ID;
-import net.refractions.udig.catalog.IGeoResource;
 import net.refractions.udig.catalog.IService;
-import net.refractions.udig.core.internal.CorePlugin;
-import net.refractions.udig.project.IMap;
-import net.refractions.udig.project.ui.ApplicationGIS;
 
 import org.amanzi.neo.db.manager.DatabaseManagerFactory;
 import org.amanzi.neo.services.ui.enums.EventsType;
 import org.amanzi.neo.services.ui.events.EventManager;
 import org.amanzi.neo.services.ui.events.IEventsListener;
 import org.amanzi.neo.services.ui.events.UpdateDataEvent;
-import org.amanzi.neo.services.ui.events.UpdateLayerEvent;
 import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.ui.IStartup;
@@ -50,50 +40,11 @@ import org.eclipse.ui.IStartup;
  */
 public class NeoCatalogStartup implements IStartup {
     private static Logger LOGGER = Logger.getLogger(NeoCatalogStartup.class);
-    private static String OSM_WMS_ID = "wmt://localhost/wmt/net.refractions.udig.catalog.internal.wmt.wmtsource.OSMCloudMadeSource/1155";
-    private static String URL_TO_CHECK_CONNETCTION = "www.openstreetmap.org";
     public static final String FILE_PREFIX = "file://";
 
     @SuppressWarnings("unchecked")
     public NeoCatalogStartup() {
         EventManager.getInstance().addListener(EventsType.UPDATE_DATA, new UpdateDataHandling());
-        EventManager.getInstance().addListener(EventsType.UPDATE_LAYER, new UpdateLayerHandling());
-    }
-
-    /**
-     * <p>
-     * describe handling of update layer event. and response for update catalog layer
-     * </p>
-     * TODO should implement
-     * 
-     * @author Vladislav_Kondratenko
-     * @since 1.0.0
-     */
-    private class UpdateLayerHandling implements IEventsListener<UpdateLayerEvent> {
-
-        @Override
-        public void handleEvent(UpdateLayerEvent data) {
-            try {
-                String databaseLocation = DatabaseManagerFactory.getDatabaseManager().getLocation();
-                ICatalog catalog = CatalogPlugin.getDefault().getLocalCatalog();
-                URL url = new URL("file://" + databaseLocation);
-                List<IService> services = CatalogPlugin.getDefault().getServiceFactory().createService(url);
-                for (IService service : services) {
-                    if (catalog.getById(IService.class, service.getID(), new NullProgressMonitor()) != null) {
-                    } else {
-                        catalog.add(service);
-                    }
-                }
-            } catch (MalformedURLException e) {
-                LOGGER.error("Could not create database location URL.", e);
-            }
-        }
-
-        @Override
-        public Object getSource() {
-            return null;
-        }
-
     }
 
     /**
@@ -135,66 +86,6 @@ public class NeoCatalogStartup implements IStartup {
     @Override
     public void earlyStartup() {
         EventManager.getInstance().fireEvent(new UpdateDataEvent());
-        addDefaultLayer();
-
     }
 
-    /**
-     * added default OpenStreetMaps layer
-     */
-    private void addDefaultLayer() {
-        try {
-            if (!isConnected()) {
-                return;
-            }
-            IService curService;
-            curService = getMapService();
-            List<IGeoResource> resMap = new LinkedList<IGeoResource>();
-            IMap map = ApplicationGIS.getActiveMap();
-
-            if (map.getMapLayers().isEmpty()) {
-                for (IGeoResource iGeoResource : curService.resources(null)) {
-                    resMap.add(iGeoResource);
-                }
-                ApplicationGIS.addLayersToMap(map, resMap, 0);
-            }
-        } catch (MalformedURLException e) {
-            LOGGER.error("Could not create database location URL.", e);
-        } catch (IOException e) {
-            LOGGER.error("Error while try to add default layer on map", e);
-        }
-    }
-
-    /**
-     * try to check connection to Internet
-     * 
-     * @return
-     */
-    private Boolean isConnected() {
-        try {
-            InetAddress.getByName(URL_TO_CHECK_CONNETCTION);
-            return true;
-        } catch (UnknownHostException e) {
-            return false;
-        }
-
-    }
-
-    /**
-     * Get OSM service
-     * 
-     * @return IService
-     * @throws MalformedURLException
-     */
-    private IService getMapService() throws MalformedURLException {
-        ICatalog catalog = CatalogPlugin.getDefault().getLocalCatalog();
-        URL url = new URL(null, OSM_WMS_ID, CorePlugin.RELAXED_HANDLER);
-        ID id = new ID(url);
-        IService curService = catalog.getById(IService.class, id, null);
-        if (curService == null) {
-            curService = CatalogPlugin.getDefault().getServiceFactory().createService(url).get(0);
-            new UpdateDataHandling().handleEvent(null);
-        }
-        return curService;
-    }
 }
