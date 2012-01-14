55e6d4e691b2ac03f7a7f4acd86c2556bebe4e77
LadornayaAnna <anka.ladornaya@gmail.com>
2012-01-14 23:31:19 +0200

    Added class AntennaPattern

---
+++ org.amanzi.neo.loader.core/src/org/amanzi/neo/loader/core/saver/antenna/AntennaPattern.java
@@ -0,0 +1,70 @@
+/* AWE - Amanzi Wireless Explorer
+ * http://awe.amanzi.org
+ * (C) 2008-2009, AmanziTel AB
+ *
+ * This library is provided under the terms of the Eclipse Public License
+ * as described at http://www.eclipse.org/legal/epl-v10.html. Any use,
+ * reproduction or distribution of the library constitutes recipient's
+ * acceptance of this agreement.
+ *
+ * This library is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+package org.amanzi.neo.loader.core.saver.antenna;
+
+/**
+ * TODO Purpose of
+ * <p>
+ * Antenna values for saver
+ * </p>
+ * 
+ * @author Ladornaya_A
+ * @since 1.0.0
+ */
+public class AntennaPattern {
+
+    // angle must be from 0 to 360 degrees
+    private double angle;
+
+    // loss values must be positive
+    private double loss;
+
+    // type of radiation pattern
+    private TypeRadiationPattern type;
+
+    /*
+     * getters and setters
+     */
+    public double getAngle() {
+        return angle;
+    }
+
+    public void setAngle(double angle) {
+        this.angle = angle;
+    }
+
+    public double getLoss() {
+        return loss;
+    }
+
+    public void setLoss(double loss) {
+        this.loss = loss;
+    }
+
+    public TypeRadiationPattern getType() {
+        return type;
+    }
+
+    public void setType(TypeRadiationPattern type) {
+        this.type = type;
+    }
+
+    /*
+     * radiation pattern type
+     */
+    private enum TypeRadiationPattern {
+        HORISONTAL, VERTICAL
+    }
+
+}
