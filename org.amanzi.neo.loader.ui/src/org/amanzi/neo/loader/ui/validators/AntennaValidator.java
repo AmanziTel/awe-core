67f4036bb1682477e10e933fb9cb509df30dd9d0
LadornayaAnna <anka.ladornaya@gmail.com>
2012-01-26 12:14:09 +0200

    Added antenna validator

--- org.amanzi.neo.loader.ui/src/org/amanzi/neo/loader/ui/validators/AntennaValidator.java
+++ org.amanzi.neo.loader.ui/src/org/amanzi/neo/loader/ui/validators/AntennaValidator.java
@@ -12,31 +12,116 @@
  */
 package org.amanzi.neo.loader.ui.validators;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.util.List;
 
+import org.amanzi.neo.loader.core.CountingFileInputStream;
 import org.amanzi.neo.loader.core.config.NetworkConfiguration;
 import org.amanzi.neo.loader.ui.validators.IValidateResult.Result;
+import org.amanzi.neo.services.exceptions.AWEException;
+import org.amanzi.neo.services.model.INetworkModel;
+import org.amanzi.neo.services.model.IProjectModel;
+import org.amanzi.neo.services.model.impl.ProjectModel;
 
 /**
- * 
- * TODO Purpose of 
+ * TODO Purpose of
  * <p>
  * Antenna patterns validator
  * </p>
+ * 
  * @author Ladornaya_A
  * @since 1.0.0
  */
-public class AntennaValidator implements IValidator<NetworkConfiguration>{
+public class AntennaValidator implements IValidator<NetworkConfiguration> {
+
+    //file reader
+    private CountingFileInputStream is;
+    private BufferedReader reader;
+
+    private String charSetName = Charset.defaultCharset().name();
+
+    // separator
+    private static final String SEPARATOR = " ";
+    
+    //constants
+    private static final Object NAME = "NAME";
+    private static final Object FREQUENCY = "FREQUENCY";
+    private static final Object GAIN = "GAIN";
 
     @Override
     public Result appropriate(List<File> filesToLoad) {
-        return null;
+        for (File file : filesToLoad) {
+
+            // checking for file expansion
+            String name = file.getName();
+            String[] part = name.split("\\.");
+            int size = part.length;
+            if (!part[size - 1].equals("msi")) {
+                return Result.FAIL;
+            }
+
+            // checking for file headers
+            try {
+                is = new CountingFileInputStream(file);
+                reader = new BufferedReader(new InputStreamReader(is, charSetName));
+
+                String line1 = reader.readLine();
+                if(!line1.split(SEPARATOR)[1].equals(NAME)){
+                    return Result.FAIL;
+                }
+                
+                String line2 = reader.readLine();
+                if(!line2.split(SEPARATOR)[1].equals(FREQUENCY)){
+                    return Result.FAIL;
+                }
+                
+                String line3 = reader.readLine();
+                if(!line3.split(SEPARATOR)[1].equals(GAIN)){
+                    return Result.FAIL;
+                }
+            } catch (FileNotFoundException e) {
+                // TODO Handle FileNotFoundException
+                throw (RuntimeException)new RuntimeException().initCause(e);
+            } catch (UnsupportedEncodingException e) {
+                // TODO Handle UnsupportedEncodingException
+                throw (RuntimeException)new RuntimeException().initCause(e);
+            } catch (IOException e) {
+                // TODO Handle IOException
+                throw (RuntimeException) new RuntimeException( ).initCause( e );
+            }
+
+        }
+
+        return Result.SUCCESS;
     }
 
     @Override
-    public IValidateResult validate(NetworkConfiguration configuration) {
-        return null;
+    public IValidateResult validate(NetworkConfiguration filesToLoad) {
+        if (filesToLoad.getDatasetName() == null) {
+            return new ValidateResultImpl(Result.FAIL, "There is no project name");
+        }
+        try {
+            IProjectModel projectModel = ProjectModel.getCurrentProjectModel();
+            String networkName = filesToLoad.getDatasetName();
+            INetworkModel network = projectModel.findNetwork(networkName);
+            if (network == null || networkName == null) {
+                return new ValidateResultImpl(Result.FAIL, "Network is not exist in database");
+            }
+            Result result = appropriate(filesToLoad.getFilesToLoad());
+            if (result == Result.FAIL || result == Result.UNKNOWN) {
+                return new ValidateResultImpl(Result.FAIL, "The file no antenna patterns data");
+            }
+        } catch (AWEException e) {
+            return new ValidateResultImpl(Result.FAIL, "Error while antenna patterns data validate");
+        }
+
+        return new ValidateResultImpl(Result.SUCCESS, "");
     }
 
 }
