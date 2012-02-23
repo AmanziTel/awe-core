ac8ca33e1feb16311319afb48f3fedb6b68b4947
ladornaya_a <ladornaya_a@asd-ladornaya.gersis.local>
2012-02-23 15:07:29 +0300

    Updated CopyOfElementDialog

--- org.amanzi.awe.views.network/src/org/amanzi/awe/views/network/view/CopyOfElementDialog.java
+++ org.amanzi.awe.views.network/src/org/amanzi/awe/views/network/view/CopyOfElementDialog.java
@@ -3,6 +3,7 @@ package org.amanzi.awe.views.network.view;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.amanzi.neo.services.AbstractService;
 import org.amanzi.neo.services.NodeTypeManager;
 import org.amanzi.neo.services.NetworkService.NetworkElementNodeType;
 import org.amanzi.neo.services.model.IDataElement;
@@ -34,6 +35,9 @@ public class CopyOfElementDialog extends AbstractDialog<Integer> {
      */
     private TableViewer tableViewer;
 
+    /*
+     * list content properties of selected element
+     */
     private List<String> properties;
 
     // sector
@@ -52,10 +56,13 @@ public class CopyOfElementDialog extends AbstractDialog<Integer> {
         updateProperies();
     }
 
+    /**
+     * Update properties from selected element
+     */
     private void updateProperies() {
-        
+
         properties = new ArrayList<String>();
-        
+
         String type = NodeTypeManager.getType(element).getId();
 
         // if sector
@@ -65,38 +72,41 @@ public class CopyOfElementDialog extends AbstractDialog<Integer> {
             for (int i = 0; i < SECTOR_PROPERTIES.length; i++) {
                 properties.add(SECTOR_PROPERTIES[i]);
             }
-
-            // add others properties
-            for (String property : element.keySet()) {
-                if (!properties.contains(property)) {
-                    properties.add(property);
-                }
-            }
         }
 
         // if site
         else if (type.equals(NetworkElementNodeType.SITE.getId())) {
 
+            // add to properties list unique properties for site
+            for (int j = 0; j < SITE_PROPERTIES.length; j++) {
+                properties.add(SITE_PROPERTIES[j]);
+            }
         }
 
-        // else
+        // other elements
         else {
+            properties.add(AbstractService.NAME);
+        }
 
+        // add other properties
+        for (String property : element.keySet()) {
+            if (!properties.contains(property)) {
+                properties.add(property);
+            }
         }
     }
 
     @Override
     protected void createContents(Shell shell) {
-        tableViewer = new TableViewer(shell, SWT.MULTI | SWT.H_SCROLL
-                | SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
-        //createColumns(shell, tableViewer);
+        tableViewer = new TableViewer(shell, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
+        // createColumns(shell, tableViewer);
         final Table table = tableViewer.getTable();
         table.setHeaderVisible(true);
         table.setLinesVisible(true);
 
         tableViewer.setContentProvider(new TableContentProvider());
         tableViewer.setLabelProvider(new TableLabelProvider());
-        tableViewer.setInput(StringUtils.EMPTY);     
+        tableViewer.setInput(StringUtils.EMPTY);
 
         // Layout the viewer
         GridData gridData = new GridData();
@@ -107,10 +117,8 @@ public class CopyOfElementDialog extends AbstractDialog<Integer> {
         gridData.horizontalAlignment = GridData.FILL;
         tableViewer.getControl().setLayoutData(gridData);
     }
-    
- 
-    
-    private class TableContentProvider implements IStructuredContentProvider{
+
+    private class TableContentProvider implements IStructuredContentProvider {
 
         @Override
         public void dispose() {
@@ -124,10 +132,10 @@ public class CopyOfElementDialog extends AbstractDialog<Integer> {
         public Object[] getElements(Object inputElement) {
             return null;
         }
-        
+
     }
-    
-    private class TableLabelProvider extends LabelProvider implements ITableLabelProvider{
+
+    private class TableLabelProvider extends LabelProvider implements ITableLabelProvider {
 
         @Override
         public Image getColumnImage(Object element, int columnIndex) {
@@ -139,9 +147,6 @@ public class CopyOfElementDialog extends AbstractDialog<Integer> {
             return null;
         }
 
-        
-        
     }
-    
 
 }
