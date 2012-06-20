cfe6f8a03dfc687600f17e7065e7417e84dad192
Nikolay Lagutko <nikolay.lagutko@amanzitel.com>
2012-06-20 17:57:33 +0300

    Tycho build Running tests Fixing buildpaths

---
+++ org.amanzi.awe.scripting.testing/src/org/amanzi/awe/scripting/ScriptingTests.java
@@ -0,0 +1,12 @@
+package org.amanzi.awe.scripting;
+
+import org.amanzi.awe.scripting.testing.AbstractScriptingPluginTesting;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AbstractScriptingPluginTesting.class })
+public class ScriptingTests {
+
+}
