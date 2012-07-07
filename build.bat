102dce824b9f382c68d079c1a63b85be16d4a47b
Nikolay Lagutko <nikolay.lagutko@amanzitel.com>
2012-07-07 16:04:54 +0300

    fixing and cleaning build.bat and build.sh

--- build.bat
+++ build.bat
@@ -1,20 +1,12 @@
 @echo off
 echo "%1"
-set lws=libs_without_sources
+set lws=development
 IF "%1" == %lws%( 
-        ECHO ------->install org.amanzi.awe.libs without sources
 	mvn clean install -f   org.amanzi.awe.libs/pom-libs.xml -P %lws%
-  	ECHO -------> install org.neo4j without sources
 	mvn clean install -f   org.neo4j/pom-libs.xml/ -P %lws%
-	ECHO -------> org.jfree without sources
-	mvn clean install -f   org.jfree/pom-libs.xml -P %lws%
 ) ELSE (
- 	ECHO ------->install org.amanzi.awe.libs with sources
 	mvn clean install -f   org.amanzi.awe.libs/pom-libs.xml 
-  	ECHO -------> install org.neo4j with sources
 	mvn clean install -f   org.neo4j/pom-libs.xml
-	ECHO -------> org.jfree with sources
-	mvn clean install -f   org.jfree/pom-libs.xml 
 )
 mvn clean install
 
--- build.sh
+++ build.sh
@@ -1,21 +1,13 @@
 #!/bin/sh
 echo $1
 libs_without_sources="libs_without_sources"
-if [ "$1" == libs_without_sources ]
+if [ "$1" == development ]
  then
-        echo '------->install org.amanzi.awe.libs without sources'
-	mvn clean install -f   org.amanzi.awe.libs/pom-libs.xml -P libs_without_sources
-  	echo '-------> install org.neo4j without sources'
-	mvn clean install -f   org.neo4j/pom-libs.xml/ -P libs_without_sources
-	echo '-------> org.jfree without sources'
-	mvn clean install -f   org.jfree/pom-libs.xml -P libs_without_sources
+	mvn clean install -f   org.amanzi.awe.libs/pom-libs.xml -P development
+	mvn clean install -f   org.neo4j/pom-libs.xml/ -P development
 else
- echo '------->install org.amanzi.awe.libs with sources'
 	mvn clean install -f   org.amanzi.awe.libs/pom-libs.xml 
-  	echo '-------> install org.neo4j with sources'
 	mvn clean install -f   org.neo4j/pom-libs.xml
-	echo '-------> org.jfree with sources'
-	mvn clean install -f   org.jfree/pom-libs.xml 
 fi
 mvn clean install
 
