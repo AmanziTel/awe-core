50453ecd5837af0d5cbdd6c032adfe2c26190810
Nikolay Lagutko <nikolay.lagutko@amanzitel.com>
2012-07-07 17:56:53 +0300

    Self-updatable plugin for tests

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 echo $1
 libs_without_sources="libs_without_sources"
 if [ "$1" == development ]
  then
-	mvn clean install -f   org.amanzi.awe.libs/pom-libs.xml -P development
-	mvn clean install -f   org.neo4j/pom-libs.xml/ -P development
+	mvn clean install -f org.amanzi.awe.libs/pom-libs.xml -P development
+	mvn clean install -f org.neo4j/pom-libs.xml/ -P development
+	mvn clean install -f org.amanzi.testing.libs/pom-libs.xml -P development
 else
-	mvn clean install -f   org.amanzi.awe.libs/pom-libs.xml 
-	mvn clean install -f   org.neo4j/pom-libs.xml
+	mvn clean install -f org.amanzi.awe.libs/pom-libs.xml 
+	mvn clean install -f org.neo4j/pom-libs.xml
+	mvn clean install -f org.amanzi.testing.libs/pom-libs.xml
+
+	mvn clean install
 fi
-mvn clean install
 
