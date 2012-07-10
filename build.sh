f746cca7e422a110a516149eec1bbee4fdfb3dec
Nikolay Lagutko <nikolay.lagutko@amanzitel.com>
2012-07-10 11:50:36 +0300

    Configuring test build

--- build.sh
+++ build.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-echo $1
-libs_without_sources="libs_without_sources"
+development="--development"
+testing="--testing"
 if [ "$1" == development ]
  then
 	mvn clean install -f org.amanzi.awe.libs/pom-libs.xml -P development
 	mvn clean install -f org.neo4j/pom-libs.xml/ -P development
 	mvn clean install -f org.amanzi.testing.libs/pom-libs.xml -P development
 else
-	mvn clean install -f org.amanzi.awe.libs/pom-libs.xml 
-	mvn clean install -f org.neo4j/pom-libs.xml
-	mvn clean install -f org.amanzi.testing.libs/pom-libs.xml
-
-	mvn clean install
+	if [ "$1" == testing ]
+	 then 
+		mvn clean install -f org.amanzi.awe.libs/pom-libs.xml
+		mvn clean install -f org.neo4j/pom-libs.xml
+		mvn clean install -Ptest
+	else
+		mvn clean install -f org.amanzi.awe.libs/pom-libs.xml 
+		mvn clean install -f org.neo4j/pom-libs.xml
+		mvn clean install -f org.amanzi.testing.libs/pom-libs.xml
+		mvn clean install
+	fi
 fi
 
