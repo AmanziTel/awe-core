90c624f8eb87228d1cdfc2d35270f3d23586fb51
Nikolay Lagutko <nikolay.lagutko@amanzitel.com>
2012-07-20 16:50:51 +0300

    General structure of refactored org.amanzi.neo.loader.ui plugin Schemas of extensions for Wizard and Page Extensions for Network CSV Loader Tests on LoaderWizardHandler

---
+++ org.amanzi.neo.loader.core/src/org/amanzi/neo/loader/core/validator/impl/network/NetworkValidator.java
@@ -0,0 +1,45 @@
+/* AWE - Amanzi Wireless Explorer
+ * http://awe.amanzi.org
+ * (C) 2008-2009, AmanziTel AB
+ *
+ * This library is provided under the terms of the Eclipse Public License
+ * as described at http://www.eclipse.org/legal/epl-v10.html. Any use,
+ * reproduction or distribution of the library constitutes recipient's
+ * acceptance of this agreement.
+ *
+ * This library is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+package org.amanzi.neo.loader.core.validator.impl.network;
+
+import java.io.File;
+import java.util.List;
+
+import org.amanzi.neo.loader.core.internal.IConfiguration;
+import org.amanzi.neo.loader.core.validator.IValidationResult;
+import org.amanzi.neo.loader.core.validator.IValidator;
+
+/**
+ * TODO Purpose of
+ * <p>
+ * </p>
+ * 
+ * @author Nikolay Lagutko (nikolay.lagutko@amanzitel.com)
+ * @since 1.0.0
+ */
+public class NetworkValidator implements IValidator<IConfiguration> {
+
+    @Override
+    public IValidationResult appropriate(final List<File> filesToLoad) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public IValidationResult validate(final IConfiguration configuration) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
