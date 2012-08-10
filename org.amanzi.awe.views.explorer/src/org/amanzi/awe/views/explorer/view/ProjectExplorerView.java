aa1ffcce8b949ba656ba3591082fdfc3b2ba8603
Volad <k.volad@gmail.com>
2012-08-10 14:39:32 +0300

    fix fir project explorer menu

--- org.amanzi.awe.views.explorer/src/org/amanzi/awe/views/explorer/view/ProjectExplorerView.java
+++ org.amanzi.awe.views.explorer/src/org/amanzi/awe/views/explorer/view/ProjectExplorerView.java
@@ -11,9 +11,13 @@ import org.amanzi.awe.views.explorer.providers.ProjectTreeContentProvider;
 import org.amanzi.awe.views.treeview.AbstractTreeView;
 import org.amanzi.awe.views.treeview.provider.ITreeItem;
 import org.amanzi.neo.models.IModel;
+import org.amanzi.neo.models.project.IProjectModel;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.viewers.IElementComparer;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.events.MenuEvent;
+import org.eclipse.swt.events.MenuListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Menu;
 
@@ -23,12 +27,12 @@ import org.eclipse.swt.widgets.Menu;
  * @author Vladislav_Kondratenko
  * @since 0.3
  */
-public class ProjectExplorerView extends AbstractTreeView {
+public class ProjectExplorerView extends AbstractTreeView implements MenuListener {
     /*
      * ID of this View
      */
     public static final String PROJECT_EXPLORER_ID = "org.amanzi.awe.views.explorer.view.ProjectExplorer";
-
+    private static final String SHOW_IN_VIEW_ITEM = "Show in View";
     private static final IElementComparer TREE_ITEMS_COMPARATOR = new IElementComparer() {
 
         @Override
@@ -48,6 +52,8 @@ public class ProjectExplorerView extends AbstractTreeView {
         }
     };
 
+    private Menu menu;
+
     /**
      * The constructor.
      */
@@ -63,10 +69,41 @@ public class ProjectExplorerView extends AbstractTreeView {
     public void createPartControl(Composite parent) {
         super.createPartControl(parent);
         MenuManager menuManager = new MenuManager();
-        Menu menu = menuManager.createContextMenu(getTreeViewer().getControl());
+        menu = menuManager.createContextMenu(getTreeViewer().getControl());
+        menu.addMenuListener(this);
         getTreeViewer().getControl().setMenu(menu);
         getTreeViewer().setComparer(TREE_ITEMS_COMPARATOR);
         getSite().registerContextMenu(menuManager, getTreeViewer());
     }
 
+    @Override
+    public void menuHidden(MenuEvent e) {
+
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void menuShown(MenuEvent e) {
+        IStructuredSelection selection = (IStructuredSelection)getTreeViewer().getSelection();
+        ITreeItem<IProjectModel> item = (ITreeItem<IProjectModel>)selection.getFirstElement();
+        if (item.getParent().asDataElement().equals(item.getDataElement())) {
+            menu.getItems()[getMenuItemIndexByName(SHOW_IN_VIEW_ITEM)].setEnabled(false);
+        } else {
+            menu.getItems()[getMenuItemIndexByName(SHOW_IN_VIEW_ITEM)].setEnabled(true);
+        }
+
+    }
+
+    /**
+     * @param showInViewItem
+     * @return
+     */
+    private int getMenuItemIndexByName(String showInViewItem) {
+        for (int i = 0; i < menu.getItemCount(); i++) {
+            if (menu.getItem(i).getText().equalsIgnoreCase(showInViewItem)) {
+                return i;
+            }
+        }
+        return 0;
+    }
 }
