4610fc9cd0a3d979301fe80c16f670ad080b2417
Nikolay Lagutko <nikolay.lagutko@amanzitel.com>
2012-08-24 13:19:11 +0300

    Started Property Sheet

---
+++ org.amanzi.awe.views.property/src/org/amanzi/awe/views/property/views/internal/DataElementPropertySource.java
@@ -0,0 +1,72 @@
+/* AWE - Amanzi Wireless Explorer
+ * http://awe.amanzi.org
+ * (C) 2008-2009, AmanziTel AB
+ *
+ * This library is provided under the terms of the Eclipse Public License
+ * as described at http://www.eclipse.org/legal/epl-v10.html. Any use,
+ * reproduction or distribution of the library constitutes recipient's
+ * acceptance of this agreement.
+ *
+ * This library is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+package org.amanzi.awe.views.property.views.internal;
+
+import org.amanzi.neo.dto.IDataElement;
+import org.eclipse.ui.views.properties.IPropertyDescriptor;
+import org.eclipse.ui.views.properties.IPropertySource;
+
+/**
+ * TODO Purpose of
+ * <p>
+ *
+ * </p>
+ * @author Nikolay Lagutko (nikolay.lagutko@amanzitel.com)
+ * @since 1.0.0
+ */
+public class DataElementPropertySource implements IPropertySource {
+
+    private final IDataElement dataElement;
+
+    public DataElementPropertySource(final IDataElement dataElement) {
+        this.dataElement = dataElement;
+    }
+
+    @Override
+    public Object getEditableValue() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public IPropertyDescriptor[] getPropertyDescriptors() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object getPropertyValue(final Object id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isPropertySet(final Object id) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void resetPropertyValue(final Object id) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setPropertyValue(final Object id, final Object value) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ org.amanzi.awe.views.property/src/org/amanzi/awe/views/property/views/internal/DataElementPropertySourceProvider.java
@@ -0,0 +1,41 @@
+/* AWE - Amanzi Wireless Explorer
+ * http://awe.amanzi.org
+ * (C) 2008-2009, AmanziTel AB
+ *
+ * This library is provided under the terms of the Eclipse Public License
+ * as described at http://www.eclipse.org/legal/epl-v10.html. Any use,
+ * reproduction or distribution of the library constitutes recipient's
+ * acceptance of this agreement.
+ *
+ * This library is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+package org.amanzi.awe.views.property.views.internal;
+
+import org.amanzi.neo.dto.IDataElement;
+import org.amanzi.neo.models.IModel;
+import org.eclipse.ui.views.properties.IPropertySource;
+import org.eclipse.ui.views.properties.IPropertySourceProvider;
+
+/**
+ * TODO Purpose of
+ * <p>
+ *
+ * </p>
+ * @author Nikolay Lagutko (nikolay.lagutko@amanzitel.com)
+ * @since 1.0.0
+ */
+public class DataElementPropertySourceProvider implements IPropertySourceProvider {
+
+    @Override
+    public IPropertySource getPropertySource(final Object object) {
+        if (object instanceof IDataElement) {
+            return new DataElementPropertySource((IDataElement)object);
+        } else if (object instanceof IModel) {
+            return new DataElementPropertySource(((IModel)object).asDataElement());
+        }
+        return null;
+    }
+
+}
