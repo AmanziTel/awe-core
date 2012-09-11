4399dfc5c508a5d7960c7554c9a8c99da0f10568
Nikolay Lagutko <nikolay.lagutko@amanzitel.com>
2012-09-11 16:44:34 +0300

    #4865 prototype of commands for drill-down using Eclipse Command and Eclipse Expressions Frameworks

---
+++ org.amanzi.awe.render.core/src/org/amanzi/awe/render/core/testers/RenderingTester.java
@@ -0,0 +1,37 @@
+/* AWE - Amanzi Wireless Explorer
+ * http://awe.amanzi.org
+ * (C) 2008-2009, AmanziTel AB
+ *
+ * This library is provided under the terms of the Eclipse Public License
+ * as described at http://www.eclipse.org/legal/epl-v10.html. Any use,
+ * reproduction or distribution of the library constitutes recipient's
+ * acceptance of this agreement.
+ *
+ * This library is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+package org.amanzi.awe.render.core.testers;
+
+import org.amanzi.neo.models.render.IRenderableModel;
+import org.eclipse.core.expressions.PropertyTester;
+
+/**
+ * TODO Purpose of
+ * <p>
+ *
+ * </p>
+ * @author Nikolay Lagutko (nikolay.lagutko@amanzitel.com)
+ * @since 1.0.0
+ */
+public class RenderingTester extends PropertyTester {
+
+    @Override
+    public boolean test(final Object receiver, final String property, final Object[] args, final Object expectedValue) {
+        if (receiver instanceof IRenderableModel) {
+            return ((IRenderableModel)receiver).isRenderable();
+        }
+        return false;
+    }
+
+}
