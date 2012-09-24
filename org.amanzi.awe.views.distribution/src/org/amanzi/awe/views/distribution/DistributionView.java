c564391e97d579e6f991f99bfd3a24e4a654c98e
Nikolay Lagutko <nikolay.lagutko@amanzitel.com>
2012-09-24 18:24:15 +0300

    Added Distribution View plugin Distribution feature added to Project

---
+++ org.amanzi.awe.views.distribution/src/org/amanzi/awe/views/distribution/DistributionView.java
@@ -0,0 +1,48 @@
+/* AWE - Amanzi Wireless Explorer
+ * http://awe.amanzi.org
+ * (C) 2008-2009, AmanziTel AB
+ *
+ * This library is provided under the terms of the Eclipse Public License
+ * as described at http://www.eclipse.org/legal/epl-v10.html. Any use,
+ * reproduction or distribution of the library constitutes recipient's
+ * acceptance of this agreement.
+ *
+ * This library is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+package org.amanzi.awe.views.distribution;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+/**
+ * TODO Purpose of 
+ * <p>
+ *
+ * </p>
+ * @author Nikolay Lagutko (nikolay.lagutko@amanzitel.com)
+ * @since 1.0.0
+ */
+public class DistributionView extends ViewPart {
+
+    /**
+     * 
+     */
+    public DistributionView() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void createPartControl(Composite parent) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setFocus() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
