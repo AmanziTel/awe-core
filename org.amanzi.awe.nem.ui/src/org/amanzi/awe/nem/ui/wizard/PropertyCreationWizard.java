de60a06d22908751efb766f77b16b6fd7e2c35f8
Volad <k.volad@gmail.com>
2012-10-10 15:13:08 +0300

    some refactoring

---
+++ org.amanzi.awe.nem.ui/src/org/amanzi/awe/nem/ui/wizard/PropertyCreationWizard.java
@@ -0,0 +1,56 @@
+/* AWE - Amanzi Wireless Explorer
+ * http://awe.amanzi.org
+ * (C) 2008-2009, AmanziTel AB
+ *
+ * This library is provided under the terms of the Eclipse Public License
+ * as described at http://www.eclipse.org/legal/epl-v10.html. Any use,
+ * reproduction or distribution of the library constitutes recipient's
+ * acceptance of this agreement.
+ *
+ * This library is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+package org.amanzi.awe.nem.ui.wizard;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.amanzi.awe.nem.ui.wizard.pages.PropertyEditorPage;
+import org.amanzi.neo.models.network.INetworkModel;
+import org.eclipse.jface.wizard.IWizardPage;
+
+/**
+ * TODO Purpose of
+ * <p>
+ * </p>
+ * 
+ * @author Vladislav_Kondratenko
+ * @since 1.0.0
+ */
+public class PropertyCreationWizard extends NetworkCreationWizard {
+
+    private INetworkModel model;
+
+    public PropertyCreationWizard(INetworkModel model) {
+        assert model != null;
+
+        this.model = model;
+
+        List<String> structure = Arrays.asList(model.getNetworkStructure());
+        getDataContainer().setName(model.getName());
+        getDataContainer().setStructure(structure);
+
+        setForcePreviousAndNextButtons(false);
+    }
+
+    @Override
+    protected void handleFirstPageOnFinish(IWizardPage page) {
+        handlePropertyPage((PropertyEditorPage)page);
+    }
+
+    // TODO KV: not implemented yet
+    protected void handleModelRefreshing() {
+
+    }
+}
---
+++ org.amanzi.awe.nem/src/org/amanzi/awe/nem/exceptions/NemManagerOperationException.java
@@ -0,0 +1,31 @@
+/* AWE - Amanzi Wireless Explorer
+ * http://awe.amanzi.org
+ * (C) 2008-2009, AmanziTel AB
+ *
+ * This library is provided under the terms of the Eclipse Public License
+ * as described at http://www.eclipse.org/legal/epl-v10.html. Any use,
+ * reproduction or distribution of the library constitutes recipient's
+ * acceptance of this agreement.
+ *
+ * This library is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+package org.amanzi.awe.nem.exceptions;
+
+/**
+ * TODO Purpose of
+ * <p>
+ * </p>
+ * 
+ * @author Vladislav_Kondratenko
+ * @since 1.0.0
+ */
+public class NemManagerOperationException extends Exception {
+    /** long serialVersionUID field */
+    private static final long serialVersionUID = -6086738960836742360L;
+
+    public NemManagerOperationException(String message, Exception e) {
+        super(message, e);
+    }
+}
