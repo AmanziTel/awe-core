06e99969257b816b62c3bc582ae53e32264fd435
Nikolay Lagutko <nikolay.lagutko@mail.com>
2012-10-18 15:23:41 +0300

    Remove bad code from Rendering Core plugin

--- org.amanzi.awe.render.core/src/org/amanzi/awe/render/core/handlers/RenderHandler.java
+++ org.amanzi.awe.render.core/src/org/amanzi/awe/render/core/handlers/RenderHandler.java
@@ -13,31 +13,9 @@
 
 package org.amanzi.awe.render.core.handlers;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-
-import org.amanzi.awe.ui.events.impl.ShowGISOnMap;
-import org.amanzi.awe.ui.manager.AWEEventManager;
-import org.amanzi.awe.ui.manager.EventChain;
-import org.amanzi.awe.views.treeview.provider.IPeriodTreeItem;
-import org.amanzi.awe.views.treeview.provider.ITreeItem;
-import org.amanzi.neo.dto.IDataElement;
-import org.amanzi.neo.dto.ISourcedElement;
-import org.amanzi.neo.models.IAnalyzisModel;
-import org.amanzi.neo.models.IModel;
-import org.amanzi.neo.models.exceptions.ModelException;
-import org.amanzi.neo.models.measurement.IMeasurementModel;
-import org.amanzi.neo.models.render.IGISModel;
-import org.amanzi.neo.models.render.IRenderableModel;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.handlers.HandlerUtil;
-
-import com.google.common.collect.Iterables;
 
 /**
  * TODO Purpose of
@@ -49,106 +27,8 @@ import com.google.common.collect.Iterables;
  */
 public class RenderHandler extends AbstractHandler {
 
-    @SuppressWarnings("unchecked")
     @Override
     public Object execute(final ExecutionEvent event) throws ExecutionException {
-        ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().getSelection();
-
-        // TODO: LN, 12.09.2012, a lot of duplications with RenderingTester
-
-        if (selection instanceof IStructuredSelection) {
-            Iterator<Object> selectionIterator = ((IStructuredSelection)selection).iterator();
-
-            boolean isModel = false;
-
-            Set<IDataElement> elements = new HashSet<IDataElement>();
-            IRenderableModel model = null;
-
-            while (selectionIterator.hasNext()) {
-                Object selectedObject = selectionIterator.next();
-                if (selectedObject instanceof IPeriodTreeItem) {
-                    IPeriodTreeItem< ? , ? > periodItem = (IPeriodTreeItem< ? , ? >)selectedObject;
-
-                    if (periodItem.getPeriod() != null) {
-                        IMeasurementModel parentModel = getParentModel(periodItem, IMeasurementModel.class);
-
-                        if (parentModel != null) {
-                            try {
-                                Iterables.addAll(elements,
-                                        parentModel.getElements(periodItem.getStartDate(), periodItem.getEndDate()));
-                            } catch (ModelException e) {
-                                // TODO: handle
-                            }
-                            model = parentModel;
-                            continue;
-                        }
-                    }
-                }
-                if (selectedObject instanceof ITreeItem) {
-                    ITreeItem< ? , ? > treeItem = (ITreeItem< ? , ? >)selectedObject;
-
-                    if (treeItem.getChild() instanceof ISourcedElement) {
-                        IRenderableModel elementParent = getParentModel(treeItem, IRenderableModel.class);
-
-                        if (elementParent != null) {
-                            Iterables.addAll(elements, ((ISourcedElement)treeItem.getChild()).getSources());
-
-                            model = elementParent;
-                            continue;
-                        }
-
-                    } else if (treeItem.getChild() instanceof IRenderableModel) {
-                        model = (IRenderableModel)treeItem.getChild();
-                        isModel = true;
-                        break;
-                    } else if (treeItem.getChild() instanceof IDataElement) {
-                        IDataElement element = (IDataElement)treeItem.getChild();
-
-                        elements.add(element);
-
-                        model = model == null ? getParentModel(treeItem, IRenderableModel.class) : model;
-                    }
-                }
-            }
-
-            if (isModel) {
-                showOnMap(model);
-            } else {
-                showOnMap(model, elements);
-            }
-        }
-
-        return null;
-    }
-
-    private void showOnMap(final IRenderableModel model, final Set<IDataElement> elements) {
-        AWEEventManager.getManager().fireShowOnMapEvent(model, elements, this);
-    }
-
-    private void showOnMap(final IRenderableModel model) {
-        EventChain eventChain = new EventChain(true);
-
-        for (IGISModel gis : model.getAllGIS()) {
-            eventChain.addEvent(new ShowGISOnMap(gis, this));
-        }
-
-        AWEEventManager.getManager().fireEventChain(eventChain);
-    }
-
-    private <T extends IModel> T getParentModel(final ITreeItem< ? , ? > treeItem, final Class<T> clazz) {
-        IModel model = treeItem.getParent();
-
-        if (model != null) {
-            if (model instanceof IAnalyzisModel) {
-                model = ((IAnalyzisModel< ? >)model).getSourceModel();
-            }
-
-            if (model != null) {
-                if (clazz.isAssignableFrom(model.getClass())) {
-                    return clazz.cast(model);
-                }
-            }
-        }
 
         return null;
     }
--- org.amanzi.awe.render.core/src/org/amanzi/awe/render/core/testers/RenderingTester.java
+++ org.amanzi.awe.render.core/src/org/amanzi/awe/render/core/testers/RenderingTester.java
@@ -13,21 +13,7 @@
 
 package org.amanzi.awe.render.core.testers;
 
-import java.util.Iterator;
-
-import org.amanzi.awe.views.treeview.provider.IPeriodTreeItem;
-import org.amanzi.awe.views.treeview.provider.ITreeItem;
-import org.amanzi.neo.dto.IDataElement;
-import org.amanzi.neo.dto.ISourcedElement;
-import org.amanzi.neo.models.IAnalyzisModel;
-import org.amanzi.neo.models.IModel;
-import org.amanzi.neo.models.exceptions.ModelException;
-import org.amanzi.neo.models.measurement.IMeasurementModel;
-import org.amanzi.neo.models.render.IRenderableModel;
-import org.apache.commons.collections.iterators.IteratorChain;
-import org.apache.commons.collections.iterators.SingletonIterator;
 import org.eclipse.core.expressions.PropertyTester;
-import org.eclipse.jface.viewers.IStructuredSelection;
 
 /**
  * TODO Purpose of
@@ -42,120 +28,7 @@ public class RenderingTester extends PropertyTester {
     @SuppressWarnings("unchecked")
     @Override
     public boolean test(final Object receiver, final String property, final Object[] args, final Object expectedValue) {
-        if (receiver instanceof IStructuredSelection) {
-            IStructuredSelection selection = (IStructuredSelection)receiver;
-
-            Iterator<Object> selectedElements = selection.iterator();
-
-            IRenderableModel parent = null;
-
-            boolean testElements = false;
-            final IteratorChain elementChain = new IteratorChain();
-
-            while (selectedElements.hasNext()) {
-                Object selectedElement = selectedElements.next();
-
-                if (selectedElement instanceof IPeriodTreeItem) {
-                    IPeriodTreeItem< ? , ? > periodItem = (IPeriodTreeItem< ? , ? >)selectedElement;
-
-                    if (periodItem.getPeriod() != null) {
-
-                        if (periodItem.getParent() instanceof IMeasurementModel) {
-                            IMeasurementModel parentModel = getParentModel(periodItem, IMeasurementModel.class);
-
-                            if (parentModel != null) {
-                                try {
-                                    elementChain.addIterator(parentModel.getElements(periodItem.getStartDate(),
-                                            periodItem.getEndDate()).iterator());
-                                } catch (ModelException e) {
-                                    // TODO: handle
-                                }
-                                parent = parentModel;
-                                testElements = true;
-
-                                continue;
-                            }
-                        }
-                    }
-                }
-                if (selectedElement instanceof ITreeItem) {
-                    ITreeItem< ? , ? > treeItem = (ITreeItem< ? , ? >)selectedElement;
-
-                    if (treeItem.getChild() instanceof ISourcedElement) {
-                        IRenderableModel elementParent = getParentModel(treeItem, IRenderableModel.class);
-
-                        if (elementParent != null) {
-                            elementChain.addIterator(((ISourcedElement)treeItem.getChild()).getSources().iterator());
-
-                            parent = elementParent;
-                            testElements = true;
-
-                            continue;
-                        }
-
-                    } else if (treeItem.getChild() instanceof IDataElement) {
-                        IRenderableModel elementParent = getParentModel(treeItem, IRenderableModel.class);
-                        if (elementParent != null) {
-                            if (parent == null) {
-                                parent = elementParent;
-                            } else {
-                                if (!parent.equals(elementParent)) {
-                                    return false;
-                                }
-                            }
-
-                            elementChain.addIterator(new SingletonIterator(treeItem.getChild()));
-                            testElements = true;
-                        }
-                    } else if (treeItem.getChild() instanceof IRenderableModel) {
-                        IRenderableModel renderableModel = (IRenderableModel)treeItem.getChild();
-
-                        if (!renderableModel.isRenderable()) {
-                            return false;
-                        }
-                        if (selectedElements.hasNext()) {
-                            return false;
-                        }
-                    } else {
-                        return false;
-                    }
-                } else {
-                    return false;
-                }
-            }
-
-            if (testElements) {
-                if (!parent.getElementsLocations(new Iterable<IDataElement>() {
-
-                    @Override
-                    public Iterator<IDataElement> iterator() {
-                        return elementChain;
-                    }
-                }).iterator().hasNext()) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    private <T extends IModel> T getParentModel(final ITreeItem< ? , ? > treeItem, final Class<T> clazz) {
-        IModel model = treeItem.getParent();
-
-        if (model != null) {
-            if (model instanceof IAnalyzisModel) {
-                model = ((IAnalyzisModel< ? >)model).getSourceModel();
-            }
-
-            if (model != null) {
-                if (clazz.isAssignableFrom(model.getClass())) {
-                    return clazz.cast(model);
-                }
-            }
-        }
-
-        return null;
+        return false;
     }
 
 }
